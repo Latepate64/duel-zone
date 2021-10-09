df94278eacd34a55264cac83a1270cf0329fc0b2
Latepate64 <lauri.koriseva@gmail.com>
2021-10-09 14:03:06 +0300

    Implemented Corile.

---
+++ DuelMastersCards/Resolvables/CorileResolvable.cs
@@ -0,0 +1,51 @@
+﻿using DuelMastersModels;
+using DuelMastersModels.Abilities;
+using DuelMastersModels.Choices;
+using System;
+using System.Linq;
+
+namespace DuelMastersCards.Resolvables
+{
+    public class CorileResolvable : Resolvable
+    {
+        public CorileResolvable()
+        {
+        }
+
+        public CorileResolvable(Resolvable resolvable) : base(resolvable)
+        {
+        }
+
+        public override Resolvable Copy()
+        {
+            return new CorileResolvable(this);
+        }
+
+        public override Choice Resolve(Duel duel, Decision decision)
+        {
+            // Choose one of your opponent's creatures in the battle zone and put it on top of his deck.
+            var opponent = duel.GetOpponent(duel.GetPlayer(Controller));
+            if (decision == null)
+            {
+                if (opponent.BattleZone.Permanents.Count > 1)
+                {
+                    return new GuidSelection(Controller, opponent.BattleZone.Permanents, 1, 1);
+                }
+                else if (opponent.BattleZone.Permanents.Any())
+                {
+                    opponent.PutFromBattleZoneOnTopOfDeck(opponent.BattleZone.Permanents.Single(), duel);
+                    return null;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                opponent.PutFromBattleZoneOnTopOfDeck(duel.GetPermanent((decision as GuidDecision).Decision.Single()), duel);
+                return null;
+            }
+        }
+    }
+}
