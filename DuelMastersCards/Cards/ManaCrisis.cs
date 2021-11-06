27b0802f8a5f9003ecf0a3f2d184491d33d28f61
Latepate64 <lauri.koriseva@gmail.com>
2021-11-06 08:31:06 +0200

    Implemented Mana Crisis.

---
+++ DuelMastersCards/Cards/ManaCrisis.cs
@@ -0,0 +1,15 @@
+﻿using DuelMastersCards.Resolvables;
+using DuelMastersModels;
+using DuelMastersModels.Abilities;
+
+namespace DuelMastersCards.Cards
+{
+    public class ManaCrisis : Spell
+    {
+        public ManaCrisis() : base("Mana Crisis", 4, Civilization.Nature)
+        {
+            ShieldTrigger = true;
+            Abilities.Add(new SpellAbility(new PutCardsFromManaZoneIntoGraveyardResolvable(1, 1, ZoneOwner.Opponent)));
+        }
+    }
+}
