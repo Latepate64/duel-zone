58621d2bab34f93709703e90cbedccdf4d141e36
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-20 21:29:27 +0200

    Removed ControllerPutsCardsFromGraveyardIntoManaZoneEffect.

--- DuelMastersCards/Cards/ThornyMandra.cs
+++ DuelMastersCards/Cards/ThornyMandra.cs
@@ -2,6 +2,7 @@
 using DuelMastersCards.OneShotEffects;
 using DuelMastersCards.TriggeredAbilities;
 using DuelMastersModels;
+using DuelMastersModels.Zones;
 
 namespace DuelMastersCards.Cards
 {
@@ -9,7 +10,8 @@ namespace DuelMastersCards.Cards
     {
         public ThornyMandra() : base("Thorny Mandra", 5, Civilization.Nature, 4000, Subtype.TreeFolk)
         {
-            Abilities.Add(new WhenYouPutThisCreatureIntoTheBattleZoneAbility(new ControllerPutsCardsFromGraveyardIntoManaZoneEffect(new CardTypeFilter(CardType.Creature))));
+            // When you put this creature into the battle zone, you may put 1 creature from your graveyard into your mana zone.
+            Abilities.Add(new WhenYouPutThisCreatureIntoTheBattleZoneAbility(new CardMovingEffect(ZoneType.Graveyard, ZoneType.ManaZone, 0, 1, true, new CardTypeFilter(CardType.Creature), new PlayerFilter(true), new ZoneFilter(ZoneType.Graveyard))));
         }
     }
 }
