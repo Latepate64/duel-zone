d31498ea646b4cee8efa058f668b4614adb88bbd
Latepate64 <lauri.koriseva@gmail.com>
2022-01-22 13:41:34 +0200

    Removed CreaturesWithMaxPowerFilter.

--- DuelMastersCards/Cards/SaucerHeadShark.cs
+++ DuelMastersCards/Cards/SaucerHeadShark.cs
@@ -2,6 +2,7 @@
 using DuelMastersCards.OneShotEffects;
 using DuelMastersCards.TriggeredAbilities;
 using DuelMastersModels;
+using DuelMastersModels.Zones;
 
 namespace DuelMastersCards.Cards
 {
@@ -10,7 +11,7 @@ namespace DuelMastersCards.Cards
         public SaucerHeadShark() : base("Saucer-Head Shark", 5, Civilization.Water, 3000, Subtype.GelFish)
         {
             // When you put this creature into the battle zone, return each creature in the battle zone that has power 2000 or less to its owner's hand.
-            Abilities.Add(new WhenYouPutThisCreatureIntoTheBattleZoneAbility(new ReturnCreaturesToTheirOwnersHandsEffect(new CreaturesWithMaxPowerFilter(2000))));
+            Abilities.Add(new WhenYouPutThisCreatureIntoTheBattleZoneAbility(new CardMovingAreaOfEffect(ZoneType.BattleZone, ZoneType.Hand, new BattleZoneMaxPowerCreatureFilter(2000))));
         }
     }
 }
