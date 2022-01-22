a51076beca66f0a58eb457b9482fd2fd1887f9ab
Latepate64 <lauri.koriseva@gmail.com>
2022-01-22 18:56:02 +0200

    Added more filters.

---
+++ DuelMastersCards/CardFilters/OwnersBattleZoneCreatureFilter.cs
@@ -0,0 +1,26 @@
+﻿
+using DuelMastersModels;
+
+namespace DuelMastersCards.CardFilters
+{
+    class OwnersBattleZoneCreatureFilter : OwnersBattleZoneCardFilter
+    {
+        public OwnersBattleZoneCreatureFilter()
+        {
+        }
+
+        public OwnersBattleZoneCreatureFilter(OwnersBattleZoneCreatureFilter filter) : base(filter)
+        {
+        }
+
+        public override bool Applies(Card card, Game game, Player player)
+        {
+            return base.Applies(card, game, player) && card.CardType == CardType.Creature;
+        }
+
+        public override CardFilter Copy()
+        {
+            return new OwnersBattleZoneCreatureFilter(this);
+        }
+    }
+}
---
+++ DuelMastersModels/ContinuousEffects/AbilityGrantingEffect.cs
@@ -0,0 +1,25 @@
+﻿using DuelMastersModels.Abilities;
+using DuelMastersModels.Durations;
+
+namespace DuelMastersModels.ContinuousEffects
+{
+    public class AbilityGrantingEffect : ContinuousEffect
+    {
+        public Ability Ability { get; }
+
+        public AbilityGrantingEffect(AbilityGrantingEffect effect) : base(effect)
+        {
+            Ability = effect.Ability;
+        }
+
+        public AbilityGrantingEffect(CardFilter filter, Duration duration, Ability ability) : base(filter, duration)
+        {
+            Ability = ability;
+        }
+
+        public override ContinuousEffect Copy()
+        {
+            return new AbilityGrantingEffect(this);
+        }
+    }
+}
