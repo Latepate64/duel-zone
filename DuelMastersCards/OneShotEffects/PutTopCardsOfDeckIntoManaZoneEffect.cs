e8e29cf60e1940e8e78466649ed7273cfd65caa3
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 20:28:20 +0200

    Ultimate Force

---
+++ DuelMastersCards/OneShotEffects/PutTopCardsOfDeckIntoManaZoneEffect.cs
@@ -0,0 +1,30 @@
+﻿using DuelMastersModels;
+using DuelMastersModels.Abilities;
+
+namespace DuelMastersCards.OneShotEffects
+{
+    public class PutTopCardsOfDeckIntoManaZoneEffect : OneShotEffect
+    {
+        public int Amount { get; }
+
+        public PutTopCardsOfDeckIntoManaZoneEffect(int amount) : base()
+        {
+            Amount = amount;
+        }
+
+        public PutTopCardsOfDeckIntoManaZoneEffect(PutTopCardsOfDeckIntoManaZoneEffect effect)
+        {
+            Amount = effect.Amount;
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new PutTopCardsOfDeckIntoManaZoneEffect(this);
+        }
+
+        public override void Apply(Game game, Ability source)
+        {
+            game.GetPlayer(source.Owner).PutFromTopOfDeckIntoManaZone(game, Amount);
+        }
+    }
+}
