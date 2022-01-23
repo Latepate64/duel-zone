7a22a11050cd5c3c38b64f024cf52defe1756bc3
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 20:38:53 +0200

    Miele, Vizier of Lightning

---
+++ DuelMastersCards/Cards/MieleVizierOfLightning.cs
@@ -0,0 +1,16 @@
+﻿using DuelMastersCards.CardFilters;
+using DuelMastersCards.OneShotEffects;
+using DuelMastersCards.TriggeredAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class MieleVizierOfLightning : Creature
+    {
+        public MieleVizierOfLightning() : base("Miele, Vizier of Lightning", 3, Civilization.Light, 1000, Subtype.Initiate)
+        {
+            // When you put this creature into the battle zone, you may choose 1 of your opponent's creatures in the battle zone and tap it.
+            Abilities.Add(new WhenYouPutThisCreatureIntoTheBattleZoneAbility(new TapChoiceEffect(new OpponentsBattleZoneChoosableCreatureFilter(), 0, 1)));
+        }
+    }
+}
---
+++ DuelMastersCards/OneShotEffects/TapChoiceEffect.cs
@@ -0,0 +1,64 @@
+﻿using DuelMastersModels;
+using DuelMastersModels.Abilities;
+using DuelMastersModels.Choices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DuelMastersCards.OneShotEffects
+{
+    class TapChoiceEffect : OneShotEffect
+    {
+        public CardFilter Filter { get; }
+        public int Minimum { get; }
+        public int Maximum { get; }
+
+        public TapChoiceEffect(CardFilter filter, int minimum, int maximum)
+        {
+            Filter = filter;
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+
+        public TapChoiceEffect(TapChoiceEffect effect)
+        {
+            Filter = effect.Filter;
+            Minimum = effect.Minimum;
+            Maximum = effect.Maximum;
+        }
+
+        public override void Apply(Game game, Ability source)
+        {
+            var cards = game.GetAllCards().Where(card => Filter.Applies(card, game, game.GetPlayer(source.Owner)));
+            if (cards.Any())
+            {
+                if (Minimum >= cards.Count())
+                {
+                    Tap(cards);
+                }
+                else
+                {
+                    var player = game.GetPlayer(source.Owner);
+                    if (player != null)
+                    {
+                        Tap(player.Choose(new GuidSelection(player.Id, cards.Select(x => x.Id), Minimum, Math.Min(Maximum, cards.Count()))).Decision.Select(x => game.GetCard(x)));
+                    }
+                }
+
+            }
+        }
+
+        private static void Tap(IEnumerable<Card> cards)
+        {
+            foreach (var card in cards)
+            {
+                card.Tapped = true;
+            }
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new TapChoiceEffect(this);
+        }
+    }
+}
