cad361294e8a667c559bfd6eaa60b14431116a8f
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:15:02 +0200

    Frei, Vizier of Air

---
+++ DuelMastersCards/Cards/FreiVizierOfAir.cs
@@ -0,0 +1,15 @@
+﻿using DuelMastersCards.OneShotEffects;
+using DuelMastersCards.TriggeredAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class FreiVizierOfAir : Creature
+    {
+        public FreiVizierOfAir() : base("Frei, Vizier of Air", 4, Civilization.Light, 3000, Subtype.Initiate)
+        {
+            // At the end of each of your turns, you may untap this creature.
+            Abilities.Add(new AtTheEndOfYourTurnAbility(new ControllerMayUntapCreatureEffect()));
+        }
+    }
+}
