ec54cd60b4b1347785fb027c9a204940e7c4bcdf
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:00:16 +0200

    Death Smoke

---
+++ DuelMastersCards/CardFilters/OpponentsBattleZoneChoosableUntappedCreatureFilter.cs
@@ -0,0 +1,25 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.CardFilters
+{
+    class OpponentsBattleZoneChoosableUntappedCreatureFilter : OpponentsBattleZoneChoosableCreatureFilter
+    {
+        public OpponentsBattleZoneChoosableUntappedCreatureFilter()
+        {
+        }
+
+        public OpponentsBattleZoneChoosableUntappedCreatureFilter(OpponentsBattleZoneCardFilter filter) : base(filter)
+        {
+        }
+
+        public override bool Applies(Card card, Game game, Player player)
+        {
+            return base.Applies(card, game, player) && !card.Tapped;
+        }
+
+        public override CardFilter Copy()
+        {
+            return new OpponentsBattleZoneChoosableUntappedCreatureFilter(this);
+        }
+    }
+}
---
+++ DuelMastersCards/Cards/DeathSmoke.cs
@@ -0,0 +1,17 @@
+﻿using DuelMastersCards.CardFilters;
+using DuelMastersCards.OneShotEffects;
+using DuelMastersModels;
+using DuelMastersModels.Abilities;
+using DuelMastersModels.Zones;
+
+namespace DuelMastersCards.Cards
+{
+    class DeathSmoke : Spell
+    {
+        public DeathSmoke() : base("Death Smoke", 4, Civilization.Darkness)
+        {
+            // Destroy 1 of your opponent's untapped creatures.
+            Abilities.Add(new SpellAbility(new CardMovingChoiceEffect(ZoneType.BattleZone, ZoneType.Graveyard, new OpponentsBattleZoneChoosableUntappedCreatureFilter(), 1, 1, true)));
+        }
+    }
+}
