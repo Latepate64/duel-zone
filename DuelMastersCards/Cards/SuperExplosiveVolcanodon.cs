ffc18481143960c7c660791bc58a6939617b476a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 20:14:05 +0200

    Super Explosive Volcanodon

---
+++ DuelMastersCards/Cards/SuperExplosiveVolcanodon.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersCards.StaticAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class SuperExplosiveVolcanodon : Creature
+    {
+        public SuperExplosiveVolcanodon() : base("Super Explosive Volcanodon", 4, Civilization.Fire, 2000, Subtype.Dragonoid)
+        {
+            Abilities.Add(new PowerAttackerAbility(4000));
+        }
+    }
+}
