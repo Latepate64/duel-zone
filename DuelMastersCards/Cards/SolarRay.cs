d1cb4e305b3714438396d67d0e336748ad3d3c7d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 20:42:11 +0200

    Solar Ray

---
+++ DuelMastersCards/Cards/SolarRay.cs
@@ -0,0 +1,18 @@
+﻿using DuelMastersCards.CardFilters;
+using DuelMastersCards.OneShotEffects;
+using DuelMastersModels;
+using DuelMastersModels.Abilities;
+
+namespace DuelMastersCards.Cards
+{
+    class SolarRay : Spell
+    {
+        public SolarRay() : base("Solar Ray", 2, Civilization.Light)
+        {
+            ShieldTrigger = true;
+
+            // Choose 1 of your opponent's creatures in the battle zone and tap it.
+            Abilities.Add(new SpellAbility(new TapChoiceEffect(new OpponentsBattleZoneChoosableCreatureFilter(), 1, 1)));
+        }
+    }
+}
