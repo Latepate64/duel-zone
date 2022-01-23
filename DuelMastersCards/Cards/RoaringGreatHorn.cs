bf25958385c658629a645246c528fa824bf4fad6
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:58:46 +0200

    Stampeding Longhorn

--- DuelMastersCards/Cards/RoaringGreatHorn.cs
+++ DuelMastersCards/Cards/RoaringGreatHorn.cs
@@ -5,7 +5,7 @@ namespace DuelMastersCards.Cards
 {
     public class RoaringGreatHorn : Creature
     {
-        public RoaringGreatHorn() : base("Roaring Great-Horn", 7, Civilization.Nature, 8000, Subtype.BeastFolk)
+        public RoaringGreatHorn() : base("Roaring Great-Horn", 7, Civilization.Nature, 8000, Subtype.HornedBeast)
         {
             Abilities.Add(new PowerAttackerAbility(2000));
             Abilities.Add(new DoubleBreakerAbility());
