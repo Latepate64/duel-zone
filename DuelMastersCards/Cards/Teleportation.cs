e59bc4bbb49f84826d83d43b5589836c5a6768c1
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:54:43 +0200

    Spiral Gate

--- DuelMastersCards/Cards/Teleportation.cs
+++ DuelMastersCards/Cards/Teleportation.cs
@@ -9,7 +9,7 @@ namespace DuelMastersCards.Cards
         public Teleportation() : base("Teleportation", 5, Civilization.Water)
         {
             // Choose up to 2 creatures in the battle zone and return them to their owners' hands.
-            Abilities.Add(new SpellAbility(new BounceEffect(2)));
+            Abilities.Add(new SpellAbility(new BounceEffect(0, 2)));
         }
     }
 }
