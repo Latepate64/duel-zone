e47b02df59e733936be60a2c8d01355f66e1d4ea
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:39:11 +0200

    Pangaea's Song

---
+++ DuelMastersCards/Cards/PangaeasSong.cs
@@ -0,0 +1,17 @@
+﻿using DuelMastersCards.CardFilters;
+using DuelMastersCards.OneShotEffects;
+using DuelMastersModels;
+using DuelMastersModels.Abilities;
+using DuelMastersModels.Zones;
+
+namespace DuelMastersCards.Cards
+{
+    class PangaeasSong : Spell
+    {
+        public PangaeasSong() : base("Pangaea's Song", 1, Civilization.Nature)
+        {
+            // Put 1 of your creatures from the battle zone into your mana zone.
+            Abilities.Add(new SpellAbility(new CardMovingChoiceEffect(ZoneType.BattleZone, ZoneType.ManaZone, new OwnersBattleZoneCreatureFilter(), 1, 1, true)));
+        }
+    }
+}
