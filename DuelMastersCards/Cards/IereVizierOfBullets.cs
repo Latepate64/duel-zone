7dafe10af777446fe085f812cdd995ee36949e85
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:19:41 +0200

    Iere, Vizier of Bullets

---
+++ DuelMastersCards/Cards/IereVizierOfBullets.cs
@@ -0,0 +1,11 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class IereVizierOfBullets : Creature
+    {
+        public IereVizierOfBullets() : base("Iere, Vizier of Bullets", 3, Civilization.Light, 3000, Subtype.Initiate)
+        {
+        }
+    }
+}
