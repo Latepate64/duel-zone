e7811796ee37938a96a5c936281421f654e3fa27
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:47:08 +0200

    Ruby Grass

---
+++ DuelMastersCards/Cards/RubyGrass.cs
@@ -0,0 +1,19 @@
+﻿using DuelMastersCards.OneShotEffects;
+using DuelMastersCards.StaticAbilities;
+using DuelMastersCards.TriggeredAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class RubyGrass : Creature
+    {
+        public RubyGrass() : base("Ruby Grass", 3, Civilization.Light, 3000, Subtype.StarlightTree)
+        {
+            Abilities.Add(new BlockerAbility());
+            Abilities.Add(new CannotAttackPlayersAbility());
+
+            // At the end of each of your turns, you may untap this creature.
+            Abilities.Add(new AtTheEndOfYourTurnAbility(new ControllerMayUntapCreatureEffect()));
+        }
+    }
+}
