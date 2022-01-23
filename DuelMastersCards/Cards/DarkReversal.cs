20d770c1203c88a68e257423307e77fc57b3b7f2
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 18:56:34 +0200

    Dark Reversal.

---
+++ DuelMastersCards/Cards/DarkReversal.cs
@@ -0,0 +1,19 @@
+﻿using DuelMastersCards.CardFilters;
+using DuelMastersCards.OneShotEffects;
+using DuelMastersModels;
+using DuelMastersModels.Abilities;
+using DuelMastersModels.Zones;
+
+namespace DuelMastersCards.Cards
+{
+    class DarkReversal : Spell
+    {
+        public DarkReversal() : base("Dark Reversal", 2, Civilization.Darkness)
+        {
+            ShieldTrigger = true;
+
+            // Return a creature from your graveyard to your hand.
+            Abilities.Add(new SpellAbility(new CardMovingChoiceEffect(ZoneType.Graveyard, ZoneType.Hand, new OwnersGraveyardCreatureFilter(), 1, 1, true)));
+        }
+    }
+}
