2a20152da369f404e8ebdb851d0defa722a60e32
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 20:07:01 +0200

    Steel Smasher

---
+++ DuelMastersCards/Cards/SteelSmasher.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersCards.StaticAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class SteelSmasher : Creature
+    {
+        public SteelSmasher() : base("Steel Smasher", 2, Civilization.Nature, 3000, Subtype.BeastFolk)
+        {
+            Abilities.Add(new CannotAttackPlayersAbility());
+        }
+    }
+}
