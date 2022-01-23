54663dd24a1408388938d3b55369d60baecf7124
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 19:12:16 +0200

    Fire Sweeper Burning Hellion

---
+++ DuelMastersCards/Cards/FireSweeperBurningHellion.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersCards.StaticAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class FireSweeperBurningHellion : Creature
+    {
+        public FireSweeperBurningHellion() : base("Fire Sweeper Burning Hellion", 4, Civilization.Fire, 3000, Subtype.Dragonoid)
+        {
+            Abilities.Add(new PowerAttackerAbility(2000));
+        }
+    }
+}
