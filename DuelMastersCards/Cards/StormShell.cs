1a773b92625ac3b0f2cb832bfe5cd6db8b221d67
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-23 20:12:36 +0200

    Storm Shell

---
+++ DuelMastersCards/Cards/StormShell.cs
@@ -0,0 +1,17 @@
+﻿using DuelMastersCards.CardFilters;
+using DuelMastersCards.OneShotEffects;
+using DuelMastersCards.TriggeredAbilities;
+using DuelMastersModels;
+using DuelMastersModels.Zones;
+
+namespace DuelMastersCards.Cards
+{
+    class StormShell : Creature
+    {
+        public StormShell() : base("Storm Shell", 7, Civilization.Nature, 2000, Subtype.ColonyBeetle)
+        {
+            // When you put this creature into the battle zone, your opponent chooses 1 of his creatures in the battle zone and puts it into his mana zone.
+            Abilities.Add(new WhenYouPutThisCreatureIntoTheBattleZoneAbility(new CardMovingChoiceEffect(ZoneType.BattleZone, ZoneType.ManaZone, new OpponentsBattleZoneCreatureFilter(), 1, 1, false)));
+        }
+    }
+}
