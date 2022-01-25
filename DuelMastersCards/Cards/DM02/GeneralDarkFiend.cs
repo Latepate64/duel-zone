43eabb5238f6135580771322c35034140965e75a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 19:05:42 +0200

    General Dark Fiend

---
+++ DuelMastersCards/Cards/DM02/GeneralDarkFiend.cs
@@ -0,0 +1,14 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class GeneralDarkFiend : Creature
+    {
+        public GeneralDarkFiend() : base("General Dark Fiend", 5, Civilization.Darkness, 6000, Subtype.DarkLord)
+        {
+            // Whenever this creature attacks, choose one of your shields without looking and put it into your graveyard. You can't use the "shield trigger" ability of that shield.
+            Abilities.Add(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.ShieldBurnEffect(new CardFilters.OwnersShieldZoneCardFilter(), 1, 1, true)));
+            Abilities.Add(new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
