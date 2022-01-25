276ea9b4514d96b4e01364edf68a3c13a3c2a832
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 20:55:07 +0200

    Logic Sphere

---
+++ DuelMastersCards/Cards/DM03/LogicSphere.cs
@@ -0,0 +1,16 @@
+﻿using DuelMastersModels;
+using DuelMastersModels.Abilities;
+
+namespace DuelMastersCards.Cards.DM03
+{
+    class LogicSphere : Spell
+    {
+        public LogicSphere() : base("Logic Sphere", 3, Civilization.Light)
+        {
+            ShieldTrigger = true;
+
+            // Return a spell from your mana zone to your hand.
+            Abilities.Add(new SpellAbility(new OneShotEffects.ManaRecoveryEffect(new CardFilters.OwnersManaZoneSpellFilter(), 1, 1, true)));
+        }
+    }
+}
