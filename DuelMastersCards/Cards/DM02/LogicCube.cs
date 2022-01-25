329ae99eebb30cab67c56b24c09b6fa7fb2d1186
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 19:13:04 +0200

    Logic Cube

---
+++ DuelMastersCards/Cards/DM02/LogicCube.cs
@@ -0,0 +1,16 @@
+﻿using DuelMastersModels;
+using DuelMastersModels.Abilities;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class LogicCube : Spell
+    {
+        public LogicCube() : base("Logic Cube", 3, Civilization.Light)
+        {
+            ShieldTrigger = true;
+
+            // Search your deck. You may take a spell from your deck, show that spell to your opponent, and put it into your hand. Then shuffle your deck.
+            Abilities.Add(new SpellAbility(new OneShotEffects.SearchDeckEffect(new CardFilters.OwnersDeckSpellFilter(), true)));
+        }
+    }
+}
