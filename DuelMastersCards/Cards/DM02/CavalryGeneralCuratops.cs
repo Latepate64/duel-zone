80582a53dcb42df251701fbe8fb17c3d5df3e600
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 18:54:20 +0200

    Cavalry General Curatops

---
+++ DuelMastersCards/Cards/DM02/CavalryGeneralCuratops.cs
@@ -0,0 +1,12 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class CavalryGeneralCuratops : Creature
+    {
+        public CavalryGeneralCuratops() : base("Cavalry General Curatops", 3, Civilization.Fire, 2000, Subtype.Dragonoid)
+        {
+            Abilities.Add(new StaticAbilities.CanAttackUntappedCreaturesAbility());
+        }
+    }
+}
