d4bbb9d153528a825df53783fc8949417d0274d8
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 20:08:47 +0200

    Xeno Mantis

---
+++ DuelMastersCards/Cards/DM02/XenoMantis.cs
@@ -0,0 +1,15 @@
+﻿using DuelMastersCards.CardFilters;
+using DuelMastersCards.StaticAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class XenoMantis : Creature
+    {
+        public XenoMantis() : base("Xeno Mantis", 7, Civilization.Nature, 6000, Subtype.GiantInsect)
+        {
+            Abilities.Add(new UnblockableAbility(new BattleZoneMaxPowerCreatureFilter(5000)));
+            Abilities.Add(new DoubleBreakerAbility());
+        }
+    }
+}
