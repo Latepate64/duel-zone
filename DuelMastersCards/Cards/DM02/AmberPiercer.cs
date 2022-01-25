29bc5dbcaa5c3186b0bf1a648d173a7cd750e8b5
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 18:50:21 +0200

    Amber Piercer

---
+++ DuelMastersCards/Cards/DM02/AmberPiercer.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class AmberPiercer : Creature
+    {
+        public AmberPiercer() : base("Amber Piercer", 4, Civilization.Darkness, 2000, Subtype.BrainJacker)
+        {
+            // Whenever this creature attacks, you may return a creature from your graveyard to your hand.
+            Abilities.Add(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.SalvageEffect(new CardFilters.OwnersGraveyardCreatureFilter(), 0, 1, true)));
+        }
+    }
+}
