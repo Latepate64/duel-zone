e7b40082bc3133f28a6f944bc71b08bd1b713b05
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 19:24:03 +0200

    Phal Eega, Dawn Guardian

---
+++ DuelMastersCards/CardFilters/OwnersGraveyardSpellFilter.cs
@@ -0,0 +1,12 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.CardFilters
+{
+    class OwnersGraveyardSpellFilter : OwnersGraveyardCardFilter
+    {
+        public override bool Applies(Card card, Game game, Player player)
+        {
+            return base.Applies(card, game, player) && card.CardType == CardType.Spell;
+        }
+    }
+}
---
+++ DuelMastersCards/Cards/DM02/PhalEegaDawnGuardian.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class PhalEegaDawnGuardian : Creature
+    {
+        public PhalEegaDawnGuardian() : base("Phal Eega, Dawn Guardian", 5, Civilization.Light, 4000, Subtype.Guardian)
+        {
+            // When you put this creature into the battle zone, you may return a spell from your graveyard to your hand.
+            Abilities.Add(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.SalvageEffect(new CardFilters.OwnersGraveyardSpellFilter(), 0, 1, true)));
+        }
+    }
+}
