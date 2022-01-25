dddf536c8391268a6c1b1e5682e1bdb7ed5d3a80
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 16:38:44 +0200

    Implemented Gigaberos.

---
+++ DuelMastersCards/Cards/Gigaberos.cs
@@ -0,0 +1,16 @@
+﻿using DuelMastersCards.OneShotEffects;
+using DuelMastersCards.StaticAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards
+{
+    class Gigaberos : Creature
+    {
+        public Gigaberos() : base("Gigaberos", 5, Civilization.Darkness, 8000, Subtype.Chimera)
+        {
+            // When you put this creature into the battle zone, destroy 2 of your other creatures or destroy this creature.
+            Abilities.Add(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new GigaberosEffect()));
+            Abilities.Add(new DoubleBreakerAbility());
+        }
+    }
+}
---
+++ DuelMastersCards/OneShotEffects/GigaberosEffect.cs
@@ -0,0 +1,53 @@
+﻿using DuelMastersModels;
+using DuelMastersModels.Abilities;
+using DuelMastersModels.Choices;
+using DuelMastersModels.Zones;
+using System.Linq;
+
+namespace DuelMastersCards.OneShotEffects
+{
+    class GigaberosEffect : OneShotEffect
+    {
+        public override void Apply(Game game, Ability source)
+        {
+            // Destroy 2 of your other creatures or destroy this creature.
+            var creatures = game.BattleZone.GetCreatures(source.Owner);
+            var otherCreatures = creatures.Where(x => x.Id != source.Source);
+            var thisCreature = creatures.SingleOrDefault(x => x.Id == source.Source);
+            if (thisCreature == null)
+            {
+                if (otherCreatures.Count() > 2)
+                {
+                    var selection = game.GetPlayer(source.Owner).Choose(new GuidSelection(source.Owner, otherCreatures, 2, 2)).Decision;
+                    game.Move(selection.Select(x => game.GetCard(x)), ZoneType.BattleZone, ZoneType.Graveyard);
+                }
+                else
+                {
+                    game.Move(otherCreatures, ZoneType.BattleZone, ZoneType.Graveyard);
+                }
+            }
+            else if(otherCreatures.Count() < 2)
+            {
+                game.Move(thisCreature, ZoneType.BattleZone, ZoneType.Graveyard);
+            }
+            else
+            {
+                var selection = game.GetPlayer(source.Owner).Choose(new GuidSelection(source.Owner, creatures, 1, 2)).Decision;
+                if ((selection.Count() == 1 && selection.Single() == thisCreature.Id) || (selection.Count() == 2 && selection.All(x => x != thisCreature.Id)))
+                {
+                    game.Move(selection.Select(x => game.GetCard(x)), ZoneType.BattleZone, ZoneType.Graveyard);
+                }
+                else
+                {
+                    // Selection was illegal, try selecting again.
+                    Apply(game, source);
+                }
+            }
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new GigaberosEffect();
+        }
+    }
+}
