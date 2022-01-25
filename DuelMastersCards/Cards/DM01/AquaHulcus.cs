c4f4c30bd87080d0c6e01aaec5462a9399820ebb
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 18:33:11 +0200

    Started refactoring cards namespaces.

---
+++ DuelMastersCards/Cards/DM01/AquaHulcus.cs
@@ -0,0 +1,15 @@
+﻿using DuelMastersCards.OneShotEffects;
+using DuelMastersCards.TriggeredAbilities;
+using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM01
+{
+    public class AquaHulcus : Creature
+    {
+        public AquaHulcus() : base("Aqua Hulcus", 3, Civilization.Water, 2000, Subtype.LiquidPeople)
+        {
+            // When you put this creature into the battle zone, you may draw a card.
+            Abilities.Add(new WhenYouPutThisCreatureIntoTheBattleZoneAbility(new ControllerMayDrawCardsEffect(1)));
+        }
+    }
+}
