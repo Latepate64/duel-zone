c2d4fff862c881ebfe73f635bec129178ea780fc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 19:03:23 +0200

    Fortress Shell

---
+++ DuelMastersCards/Cards/DM02/FortressShell.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class FortressShell : Creature
+    {
+        public FortressShell() : base("Fortress Shell", 9, Civilization.Nature, 5000, Subtype.ColonyBeetle)
+        {
+            // When you put this creature into the battle zone, choose up to 2 cards in your opponent's mana zone and put them into his graveyard.
+            Abilities.Add(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.ManaBurnEffect(new CardFilters.OpponentsManaZoneCardFilter(), 0, 2, true)));
+        }
+    }
+}
