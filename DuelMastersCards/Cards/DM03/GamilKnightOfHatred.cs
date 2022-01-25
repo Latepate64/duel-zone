a36a757b59b6d9fe3e2e26ef8d76f592360def05
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 20:40:33 +0200

    Gamil, Knight of Hatred

---
+++ DuelMastersCards/Cards/DM03/GamilKnightOfHatred.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM03
+{
+    class GamilKnightOfHatred : Creature
+    {
+        public GamilKnightOfHatred() : base("Gamil, Knight of Hatred", 6, Civilization.Darkness, 4000, Subtype.DemonCommand)
+        {
+            // Whenever this creature attacks, you may return a darkness creature from your graveyard to your hand.
+            Abilities.Add(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.SalvageEffect(new CardFilters.OwnersGraveyardCivilizationCreatureFilter(Civilization.Darkness), 0, 1, true)));
+        }
+    }
+}
