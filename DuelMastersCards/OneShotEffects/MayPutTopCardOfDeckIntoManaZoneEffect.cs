aa66cbce3ee471196cb79f9046c476c1855ce8cc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 19:52:05 +0200

    Silver Axe

---
+++ DuelMastersCards/OneShotEffects/MayPutTopCardOfDeckIntoManaZoneEffect.cs
@@ -0,0 +1,26 @@
+﻿using DuelMastersModels;
+using DuelMastersModels.Abilities;
+using DuelMastersModels.Choices;
+
+namespace DuelMastersCards.OneShotEffects
+{
+    class MayPutTopCardOfDeckIntoManaZoneEffect : OneShotEffect
+    {
+        public override void Apply(Game game, Ability source)
+        {
+            var player = game.GetPlayer(source.Owner);
+            if (player != null)
+            {
+                if (player.Choose(new YesNoChoice(source.Owner)).Decision)
+                {
+                    player.PutFromTopOfDeckIntoManaZone(game, 1);
+                }
+            }
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new MayPutTopCardOfDeckIntoManaZoneEffect();
+        }
+    }
+}
