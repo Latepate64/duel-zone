3e3655f1a86f07a29e53697f529e343f7fc9bdf5
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 19:08:13 +0200

    Hypersquid Walter

---
+++ DuelMastersCards/Cards/DM02/HypersquidWalter.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class HypersquidWalter : Creature
+    {
+        public HypersquidWalter() : base("Hypersquid Walter", 3, Civilization.Water, 1000, Subtype.CyberLord)
+        {
+            // Whenever this creature attacks, you may draw a card.
+            Abilities.Add(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.ControllerMayDrawCardsEffect(1)));
+        }
+    }
+}
