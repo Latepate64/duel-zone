70ba3e9dee76e33ddc624ac8238775a96f07a2b8
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 19:17:30 +0200

    Metalwing Skyterror

---
+++ DuelMastersCards/Cards/DM02/MetalwingSkyterror.cs
@@ -0,0 +1,14 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM02
+{
+    class MetalwingSkyterror : Creature
+    {
+        public MetalwingSkyterror() : base("Metalwing Skyterror", 7, Civilization.Fire, 6000, Subtype.ArmoredWyvern)
+        {
+            // Whenever this creature attacks, you may destroy one of your opponent's creatures that has "blocker."
+            Abilities.Add(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.DestroyEffect(new CardFilters.OpponentsBattleZoneChoosableBlockerCreatureFilter(), 0, 1, true)));
+            Abilities.Add(new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
