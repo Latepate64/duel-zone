5f14acd946384e56cec1c43fc92ce6a6e3431058
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 20:29:17 +0200

    Boltail Dragon

---
+++ DuelMastersCards/Cards/DM03/BoltailDragon.cs
@@ -0,0 +1,12 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM03
+{
+    class BoltailDragon : Creature
+    {
+        public BoltailDragon() : base("Boltail Dragon", 7, Civilization.Fire, 9000, Subtype.ArmoredDragon)
+        {
+            Abilities.Add(new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
