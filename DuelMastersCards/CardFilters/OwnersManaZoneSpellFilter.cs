ed49d4c3036ee20f01cbf861700b92c3e173761c
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-25 20:53:32 +0200

    Lena, Vizier of Brilliance

---
+++ DuelMastersCards/CardFilters/OwnersManaZoneSpellFilter.cs
@@ -0,0 +1,12 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.CardFilters
+{
+    class OwnersManaZoneSpellFilter : OwnersManaZoneCardFilter
+    {
+        public override bool Applies(Card card, Game game, Player player)
+        {
+            return base.Applies(card, game, player) && card.CardType == CardType.Spell;
+        }
+    }
+}
---
+++ DuelMastersCards/Cards/DM03/LenaVizierOfBrilliance.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM03
+{
+    class LenaVizierOfBrilliance : Creature
+    {
+        public LenaVizierOfBrilliance() : base("Lena, Vizier of Brilliance", 4, Civilization.Light, 2000, Subtype.Initiate)
+        {
+            // When you put this creature into the battle zone, you may return a spell from your mana zone to your hand.
+            Abilities.Add(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.ManaRecoveryEffect(new CardFilters.OwnersManaZoneSpellFilter(), 0, 1, true)));
+        }
+    }
+}
