41f82b2d704cfa0a0af88adbeded9183f05c0c5b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-01-26 20:42:05 +0200

    Muramasa, Duke of Blades

---
+++ DuelMastersCards/Cards/DM03/MuramasaDukeOfBlades.cs
@@ -0,0 +1,13 @@
+﻿using DuelMastersModels;
+
+namespace DuelMastersCards.Cards.DM03
+{
+    class MuramasaDukeOfBlades : Creature
+    {
+        public MuramasaDukeOfBlades() : base("Muramasa, Duke of Blades", 6, Civilization.Fire, 3000, Subtype.Human)
+        {
+            // Whenever this creature attacks, you may destroy one of your opponent's creatures that has power 2000 or less.
+            Abilities.Add(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.DestroyEffect(new CardFilters.OpponentsBattleZoneChoosableMaxPowerCreatureFilter(2000), 0, 1, true)));
+        }
+    }
+}
