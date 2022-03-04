e267e4008fd494ba7725882439251e05f6ebf8d5
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 20:05:25 +0200

    Aqua Ranger

---
+++ Cards/Cards/DM08/AquaRanger.cs
@@ -0,0 +1,14 @@
+﻿using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM08
+{
+    public class AquaRanger : Creature
+    {
+        public AquaRanger() : base("Aqua Ranger", 6, Civilization.Water, 3000, Subtype.LiquidPeople)
+        {
+            Abilities.Add(new UnblockableAbility());
+            Abilities.Add(new WhenThisCreatureWouldBeDestroyedReturnItToYourHandInsteadAbility());
+        }
+    }
+}
