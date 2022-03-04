d2dae82f51980fd5bb6f95aecb1a0df448bbb429
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 18:13:48 +0200

    Photocide, Lord of the Wastes

---
+++ Cards/Cards/DM04/PhotocideLordOfTheWastes.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class PhotocideLordOfTheWastes : Creature
+    {
+        public PhotocideLordOfTheWastes() : base("Photocide, Lord of the Wastes", 5, Civilization.Darkness, 9000, Subtype.DemonCommand)
+        {
+            Abilities.Add(new StaticAbilities.CannotAttackPlayersAbility());
+
+            // This creature can attack untapped light creatures.
+            var filter = new CardFilters.OpponentsBattleZoneUntappedCreatureFilter();
+            filter.Civilizations.Add(Civilization.Light);
+            Abilities.Add(new StaticAbilities.CanAttackUntappedCreaturesAbility(filter));
+        }
+    }
+}
