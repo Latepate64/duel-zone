e4e6bf0feecbb924285793fbae13eeadcdbc98ee
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 18:56:23 +0200

    Angry Maple

---
+++ Cards/Cards/Promo/AngryMaple.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.Promo
+{
+    public class AngryMaple : Creature
+    {
+        public AngryMaple() : base("Angry Maple", 3, Civilization.Nature, 1000, Subtype.TreeFolk)
+        {
+            Abilities.Add(new StaticAbilities.PowerAttackerAbility(4000));
+        }
+    }
+}
