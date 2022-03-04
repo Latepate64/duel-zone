2bf78a6788533f292d491f817fbfb88ebf585f16
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 20:01:13 +0200

    Aqua Guard

---
+++ Cards/Cards/DM04/AquaGuard.cs
@@ -0,0 +1,15 @@
+﻿using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM04
+{
+    public class AquaGuard : Creature
+    {
+        public AquaGuard() : base("Aqua Guard", 1, Civilization.Water, 2000, Subtype.LiquidPeople)
+        {
+            Abilities.Add(new BlockerAbility());
+            Abilities.Add(new CannotAttackCreaturesAbility());
+            Abilities.Add(new CannotAttackPlayersAbility());
+        }
+    }
+}
