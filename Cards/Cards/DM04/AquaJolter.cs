bc8745d35e855acbec2a701b67c6a6b64c6763bc
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 20:02:28 +0200

    Aqua Jolter

---
+++ Cards/Cards/DM04/AquaJolter.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    public class AquaJolter : Creature
+    {
+        public AquaJolter() : base("Aqua Jolter", 3, Civilization.Water, 2000, Subtype.LiquidPeople)
+        {
+            ShieldTrigger = true;
+        }
+    }
+}
