efb0cf2549aa969904fa73ba80e23983c29c442e
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 20:09:00 +0200

    Aqua Skydiver

---
+++ Cards/Cards/DM10/AquaSkydiver.cs
@@ -0,0 +1,15 @@
+﻿using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM10
+{
+    public class AquaSkydiver : Creature
+    {
+        public AquaSkydiver() : base("Aqua Skydiver", 4, new[] { Civilization.Light, Civilization.Water }, 1000, new[] { Subtype.LiquidPeople })
+        {
+            ShieldTrigger = true;
+            Abilities.Add(new BlockerAbility());
+            Abilities.Add(new WhenThisCreatureWouldBeDestroyedReturnItToYourHandInsteadAbility());
+        }
+    }
+}
