761b706a523fd7d145ce9a58b31c014838e5219d
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 19:58:14 +0200

    Aqua Fencer

---
+++ Cards/Cards/DM07/AquaFencer.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM07
+{
+    public class AquaFencer : Creature
+    {
+        public AquaFencer() : base("Aqua Fencer", 7, Civilization.Water, 3000, Subtype.LiquidPeople)
+        {
+            Abilities.Add(new TapAbility(new OneShotEffects.ManaRecoveryEffect(new CardFilters.OpponentsManaZoneCardFilter(), 1, 1, true)));
+        }
+    }
+}
