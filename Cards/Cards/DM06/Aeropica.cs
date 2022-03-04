bf483ff46ab05b0eebd8c641c9ca34b300ebdbbd
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 18:16:49 +0200

    Aeropica

---
+++ Cards/Cards/DM06/Aeropica.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM06
+{
+    class Aeropica : Creature
+    {
+        public Aeropica() : base("Aeropica", 7, Civilization.Water, 4000, Subtype.SeaHacker)
+        {
+            Abilities.Add(new TapAbility(new OneShotEffects.BounceEffect(1, 1, new CardFilters.BattleZoneChoosableCreatureFilter())));
+        }
+    }
+}
