fed0b83d27743012f5eb7120eb1182c6a678b02b
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 18:53:31 +0200

    Ancient Horn, the Watcher

---
+++ Cards/Cards/DM10/AncientHornTheWatcher.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class AncientHornTheWatcher : Creature
+    {
+        public AncientHornTheWatcher() : base("Ancient Horn, the Watcher", 5, Civilization.Nature, 5000, Subtype.HornedBeast)
+        {
+            // When you put this creature into the battle zone, if you have 5 or more shields, untap all the cards in your mana zone.
+            Abilities.Add(new TriggeredAbilities.AncientHornTheWatcherAbility());
+        }
+    }
+}
---
+++ Cards/TriggeredAbilities/AncientHornTheWatcherAbility.cs
@@ -0,0 +1,27 @@
+﻿using Engine;
+using Engine.Abilities;
+
+namespace Cards.TriggeredAbilities
+{
+    public class AncientHornTheWatcherAbility : WhenYouPutThisCreatureIntoTheBattleZoneAbility
+    {
+        public AncientHornTheWatcherAbility() : base(new OneShotEffects.UntapEffect(new CardFilters.OwnersManaZoneCardFilter()))
+        {
+        }
+
+        public AncientHornTheWatcherAbility(WhenYouPutThisCreatureIntoTheBattleZoneAbility ability) : base(ability)
+        {
+        }
+
+        public override Ability Copy()
+        {
+            return new AncientHornTheWatcherAbility(this);
+        }
+
+        public override bool CheckInterveningIfClause(Game game)
+        {
+            // if you have 5 or more shields
+            return game.GetPlayer(Owner).ShieldZone.Cards.Count >= 5;
+        }
+    }
+}
