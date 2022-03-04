cdd7b73386f70fac8d0b6502e3f1f6cb6bfda8eb
Latepate64 <lauri.koriseva@gmail.com>
2022-03-04 17:24:29 +0200

    Adventure Boar

---
+++ Cards/Cards/DM10/AdventureBoar.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    public class AdventureBoar : Creature
+    {
+        public AdventureBoar() : base("Adventure Boar", 2, Civilization.Nature, 1000, Subtype.BeastFolk)
+        {
+            Abilities.Add(new StaticAbilities.PowerAttackerAbility(1000));
+        }
+    }
+}
