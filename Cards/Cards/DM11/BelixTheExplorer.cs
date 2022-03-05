9bb287f344f1735d84572c730d6ceaf553f68aa9
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 15:29:13 +0200

    Belix, the Explorer

---
+++ Cards/Cards/DM11/BelixTheExplorer.cs
@@ -0,0 +1,15 @@
+﻿using Cards.StaticAbilities;
+using Cards.TriggeredAbilities;
+using Common;
+
+namespace Cards.Cards.DM11
+{
+    class BelixTheExplorer : Creature
+    {
+        public BelixTheExplorer() : base("Belix, the Explorer", 2, Civilization.Light, 3000, Subtype.Gladiator)
+        {
+            Abilities.Add(new BlockerAbility());
+            Abilities.Add(new PutIntoPlayAbility(new OneShotEffects.ManaRecoveryEffect(new CardFilters.OwnersManaZoneCardFilter { CardType = CardType.Spell }, 1, 1, true)));
+        }
+    }
+}
