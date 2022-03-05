c2be97f1b42da22f4312aaaa3e09174445474a00
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 15:40:45 +0200

    Betrale, the Explorer

---
+++ Cards/Cards/DM09/BetraleTheExplorer.cs
@@ -0,0 +1,19 @@
+﻿using Cards.OneShotEffects;
+using Cards.StaticAbilities;
+using Cards.TriggeredAbilities;
+using Common;
+using Engine;
+
+namespace Cards.Cards.DM09
+{
+    class BetraleTheExplorer : Creature
+    {
+        public BetraleTheExplorer() : base("Betrale, the Explorer", 5, Civilization.Light, 5000, Subtype.Gladiator)
+        {
+            Abilities.Add(new BlockerAbility());
+            Abilities.Add(new CannotAttackPlayersAbility());
+            // At the end of each of your turns, you may untap this creature.
+            Abilities.Add(new AtTheEndOfYourTurnAbility(new ControllerMayUntapCreatureEffect(new TargetFilter())));
+        }
+    }
+}
