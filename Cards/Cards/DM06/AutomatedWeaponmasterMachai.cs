b7dbc5d9f0c3ea5b67b9bb79cd66618e57e63389
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 09:20:07 +0200

    Automated Weaponmaster Machai

---
+++ Cards/Cards/DM06/AutomatedWeaponmasterMachai.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    public class AutomatedWeaponmasterMachai : Creature
+    {
+        public AutomatedWeaponmasterMachai() : base("Automated Weaponmaster Machai", 4, Civilization.Fire, 4000, Subtype.Armorloid)
+        {
+            Abilities.Add(new StaticAbilities.AttacksIfAbleAbility());
+        }
+    }
+}
