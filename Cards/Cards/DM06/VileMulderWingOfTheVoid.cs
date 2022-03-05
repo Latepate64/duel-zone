229309b8b61bf8906ecb4e1b5b373bb22ee0eee5
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 12:11:56 +0200

    Vile Mulder, Wing of the Void

---
+++ Cards/Cards/DM06/VileMulderWingOfTheVoid.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    public class VileMulderWingOfTheVoid : Creature
+    {
+        public VileMulderWingOfTheVoid() : base("Vile Mulder, Wing of the Void", 4, Civilization.Darkness, 7000, Subtype.DemonCommand)
+        {
+            Abilities.Add(new StaticAbilities.CannotAttackCreaturesAbility());
+            Abilities.Add(new StaticAbilities.DoubleBreakerAbility());
+
+            // When this creature battles, destroy it after the battle.
+            Abilities.Add(new TriggeredAbilities.BattleAbility(new OneShotEffects.DestroyAfterBattleEffect()));
+        }
+    }
+}
