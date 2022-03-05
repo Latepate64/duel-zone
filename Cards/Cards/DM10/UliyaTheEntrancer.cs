6f8146a84a22f7bb4ec00f140d8b5a7971987c0b
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 15:34:41 +0200

    Uliya, the Entrancer

---
+++ Cards/Cards/DM10/UliyaTheEntrancer.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class UliyaTheEntrancer : Creature
+    {
+        public UliyaTheEntrancer() : base("Uliya, the Entrancer", 6, Civilization.Darkness, 5000, Subtype.DarkLord)
+        {
+            Abilities.Add(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.ShieldRecoveryEffect(true)));
+        }
+    }
+}
