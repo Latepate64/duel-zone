d912e5681f28f9e60b4b3e977fba6fdb5f9daba7
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 16:23:49 +0200

    Brood Shell

---
+++ Cards/Cards/DM07/BroodShell.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM07
+{
+    class BroodShell : Creature
+    {
+        public BroodShell() : base("Brood Shell", 4, Civilization.Nature, 3000, Subtype.ColonyBeetle)
+        {
+            Abilities.Add(new TapAbility(new OneShotEffects.ManaRecoveryEffect(new CardFilters.OwnersManaZoneCardFilter { CardType = CardType.Creature }, 1, 1, true)));
+        }
+    }
+}
