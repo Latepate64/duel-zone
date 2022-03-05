b083a218ac6f3c926db55dcad73e39c40417451d
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 16:46:08 +0200

    Searing Wave

---
+++ Cards/CardFilters/OpponentsBattleZoneMaxPowerCreatureFilter.cs
@@ -0,0 +1,34 @@
+﻿using Engine;
+
+namespace Cards.CardFilters
+{
+    class OpponentsBattleZoneMaxPowerCreatureFilter : OpponentsBattleZoneCardFilter
+    {
+        public int Power { get; set; }
+
+        public OpponentsBattleZoneMaxPowerCreatureFilter(int power)
+        {
+            Power = power;
+        }
+
+        public OpponentsBattleZoneMaxPowerCreatureFilter(OpponentsBattleZoneMaxPowerCreatureFilter filter) : base(filter)
+        {
+            Power = filter.Power;
+        }
+
+        public override CardFilter Copy()
+        {
+            return new OpponentsBattleZoneMaxPowerCreatureFilter(this);
+        }
+
+        public override string ToString()
+        {
+            return $"your opponent's {ToStringBase()}s that have power ${Power} or less";
+        }
+
+        public override bool Applies(Card card, Game game, Player player)
+        {
+            return base.Applies(card, game, player) && card.Power <= Power;
+        }
+    }
+}
