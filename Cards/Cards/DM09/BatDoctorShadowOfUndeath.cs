bf1df404140e9673f44e53217ae72048dc42a1be
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 15:14:23 +0200

    Bat Doctor, Shadow of Undeath

---
+++ Cards/Cards/DM09/BatDoctorShadowOfUndeath.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM09
+{
+    class BatDoctorShadowOfUndeath : Creature
+    {
+        public BatDoctorShadowOfUndeath() : base("Bat Doctor, Shadow of Undeath", 3, Civilization.Darkness, 2000, Subtype.Ghost)
+        {
+            Abilities.Add(new TriggeredAbilities.DestroyedAbility(new OneShotEffects.SalvageEffect(new CardFilters.AnotherGraveyardCardFilter(), 0, 1, true)));
+        }
+    }
+}
