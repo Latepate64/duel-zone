efc9f27febad3ed16cbae7356da63e4959eced89
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 16:21:09 +0200

    Brad's Cutter

---
+++ Cards/Cards/DM11/BradsCutter.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class BradsCutter : Creature
+    {
+        public BradsCutter() : base("Brad's Cutter", 2, Civilization.Fire, 1000, Subtype.Xenoparts)
+        {
+            ShieldTrigger = true;
+        }
+    }
+}
