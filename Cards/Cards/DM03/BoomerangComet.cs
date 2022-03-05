b609ee943dd7bcb2b7cab6ecf92f58318b408ccd
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 16:19:47 +0200

    Boomerang Comet

---
+++ Cards/Cards/DM03/BoomerangComet.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM03
+{
+    class BoomerangComet : Spell
+    {
+        public BoomerangComet() : base("Boomerang Comet", 6, Civilization.Light)
+        {
+            ShieldTrigger = true;
+            Abilities.Add(new SpellAbility(new OneShotEffects.ManaRecoveryEffect(new CardFilters.OwnersManaZoneCardFilter(), 1, 1, true)));
+            Abilities.Add(new StaticAbilities.ChargerAbility());
+        }
+    }
+}
