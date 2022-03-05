12999855c59673b96f29cb50f0e39708181248e7
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 12:14:10 +0200

    Zorvaz, the Bonecrusher

---
+++ Cards/Cards/DM06/ZorvazTheBonecrusher.cs
@@ -0,0 +1,19 @@
+﻿using Cards.OneShotEffects;
+using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM06
+{
+    public class ZorvazTheBonecrusher : Creature
+    {
+        public ZorvazTheBonecrusher() : base("Zorvaz, the Bonecrusher", 5, Civilization.Darkness, 8000, Subtype.DemonCommand)
+        {
+            Abilities.Add(new BlockerAbility());
+            Abilities.Add(new CannotAttackCreaturesAbility());
+            Abilities.Add(new CannotAttackPlayersAbility());
+
+            // When this creature battles, destroy it after the battle.
+            Abilities.Add(new TriggeredAbilities.BattleAbility(new DestroyAfterBattleEffect()));
+        }
+    }
+}
