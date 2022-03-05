c036bd88161c2a4b12658d74240955f34e2b5653
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 15:36:57 +0200

    Beratcha, the Hidden Glutton

---
+++ Cards/Cards/DM11/BeratchaTheHiddenGlutton.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class BeratchaTheHiddenGlutton : Creature
+    {
+        public BeratchaTheHiddenGlutton() : base("Beratcha, the Hidden Glutton", 5, Civilization.Darkness, 3000, Subtype.PandorasBox)
+        {
+            Abilities.Add(new StaticAbilities.SlayerAbility());
+        }
+    }
+}
