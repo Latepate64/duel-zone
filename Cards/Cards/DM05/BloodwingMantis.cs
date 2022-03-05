f7fd43b1b6764d9ca99a7ebc9bb82e9102b17927
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 16:02:07 +0200

    Bloodwing Mantis

---
+++ Cards/Cards/DM05/BloodwingMantis.cs
@@ -0,0 +1,14 @@
+﻿using Cards.TriggeredAbilities;
+using Common;
+
+namespace Cards.Cards.DM05
+{
+    class BloodwingMantis : Creature
+    {
+        public BloodwingMantis() : base("Bloodwing Mantis", 5, Civilization.Nature, 6000, Subtype.GiantInsect)
+        {
+            Abilities.Add(new AttackAbility(new OneShotEffects.ManaRecoveryEffect(new CardFilters.OwnersManaZoneCardFilter { CardType = CardType.Creature }, 2, 2, true)));
+            Abilities.Add(new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
