23cc7c58307382a90206268f9f4ff21f801bd01b
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 12:15:48 +0200

    Ballas, Vizier of Electrons

---
+++ Cards/Cards/DM06/BallasVizierOfElectrons.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    public class BallasVizierOfElectrons : Creature
+    {
+        public BallasVizierOfElectrons() : base("Ballas, Vizier of Electrons", 2, Civilization.Light, 2000, Subtype.Initiate)
+        {
+        }
+    }
+}
