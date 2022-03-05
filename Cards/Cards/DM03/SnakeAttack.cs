65cf49841f8359250c9ce5e108c3358676f68d26
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 18:55:20 +0200

    Snake Attack

---
+++ Cards/Cards/DM03/SnakeAttack.cs
@@ -0,0 +1,19 @@
+﻿using Cards.OneShotEffects;
+using Cards.StaticAbilities;
+using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM03
+{
+    class SnakeAttack : Spell
+    {
+        public SnakeAttack() : base("Snake Attack", 4, Civilization.Darkness)
+        {
+            // Each of your creatures in the battle zone gets "double breaker" until the end of the turn.
+            Abilities.Add(new SpellAbility(new GrantAbilityAreaOfEffect(new DoubleBreakerAbility())));
+
+            // Choose one of your shields and put it into your graveyard.
+            Abilities.Add(new SpellAbility(new SelfShieldBurnEffect()));
+        }
+    }
+}
