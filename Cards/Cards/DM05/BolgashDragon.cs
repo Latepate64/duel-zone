f4d9bf2f918023ca15c7614cf5c3dacaf909faa3
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 16:05:28 +0200

    Bombat, General of Speed, Bolgash Dragon

---
+++ Cards/Cards/DM05/BolgashDragon.cs
@@ -0,0 +1,14 @@
+﻿using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM05
+{
+    class BolgashDragon : Creature
+    {
+        public BolgashDragon() : base("Bolgash Dragon", 8, Civilization.Fire, 4000, Subtype.ArmoredDragon)
+        {
+            Abilities.Add(new PowerAttackerAbility(8000));
+            Abilities.Add(new TripleBreakerAbility());
+        }
+    }
+}
---
+++ Cards/Cards/DM05/BombatGeneralOfSpeed.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class BombatGeneralOfSpeed : Creature
+    {
+        public BombatGeneralOfSpeed() : base("Bombat, General of Speed", 5, Civilization.Fire, 3000, Subtype.Dragonoid)
+        {
+            Abilities.Add(new StaticAbilities.SpeedAttackerAbility());
+        }
+    }
+}
