b7786b781327b232f12f7457a8979d9354291276
Latepate64 <lauri.koriseva@gmail.com>
2022-03-05 08:18:30 +0200

    Armored Warrior Quelos

---
+++ Cards/Cards/DM03/ArmoredWarriorQuelos.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM03
+{
+    public class ArmoredWarriorQuelos : Creature
+    {
+        public ArmoredWarriorQuelos() : base("Armored Warrior Quelos", 5, Civilization.Fire, 2000, Subtype.Armorloid)
+        {
+            // Whenever this creature attacks, put a non-fire card from your mana zone into your graveyard. Then your opponent chooses a non-fire card in his mana zone and puts it into his graveyard.
+            Abilities.Add(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.ArmoredWarriorQuelosEffect()));
+        }
+    }
+}
--- Cards/Cards/DM10/AquaSkydiver.cs
+++ Cards/Cards/DM10/AquaSkydiver.cs
@@ -1,11 +1,12 @@
 ﻿using Cards.StaticAbilities;
 using Common;
+using System.Collections.Generic;
 
 namespace Cards.Cards.DM10
 {
     public class AquaSkydiver : Creature
     {
-        public AquaSkydiver() : base("Aqua Skydiver", 4, new[] { Civilization.Light, Civilization.Water }, 1000, new[] { Subtype.LiquidPeople })
+        public AquaSkydiver() : base("Aqua Skydiver", 4, new List<Civilization> { Civilization.Light, Civilization.Water }, 1000, new List<Subtype> { Subtype.LiquidPeople })
         {
             ShieldTrigger = true;
             Abilities.Add(new BlockerAbility());
