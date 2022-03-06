0b61ebefe110fc289faa3fad2ed698c764d083ff
Latepate64 <lauri.koriseva@gmail.com>
2022-03-06 14:22:06 +0200

    Candy Cluster

---
+++ Cards/Cards/DM08/CandyCluster.cs
@@ -0,0 +1,10 @@
+﻿namespace Cards.Cards.DM08
+{
+    class CandyCluster : Creature
+    {
+        public CandyCluster() : base("Candy Cluster", 3, 1000, Common.Subtype.CyberCluster, Common.Civilization.Water)
+        {
+            AddAbilities(new StaticAbilities.UnblockableAbility());
+        }
+    }
+}
