a66e94117816753f54e83e937b02c7e7ddef0371
Latepate64 <lauri.koriseva@gmail.com>
2022-03-06 15:56:58 +0200

    Colossus Boost

---
+++ Cards/OneShotEffects/GrantPowerChoiceEffect.cs
@@ -0,0 +1,39 @@
+﻿using Cards.CardFilters;
+using Engine;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cards.OneShotEffects
+{
+    class GrantPowerChoiceEffect : GrantChoiceEffect
+    {
+        public int Power { get; }
+
+        public GrantPowerChoiceEffect(GrantPowerChoiceEffect effect) : base(effect)
+        {
+            Power = effect.Power;
+        }
+
+        public GrantPowerChoiceEffect(int power) : base(new CardFilters.OwnersBattleZoneCreatureFilter(), 1, 1, true)
+        {
+            Power = power;
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new GrantPowerChoiceEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return $"{Filter} gets +${Power}{Duration}.";
+        }
+
+        protected override void Apply(Game game, Ability source, IEnumerable<Card> cards)
+        {
+            game.ContinuousEffects.Add(new PowerModifyingEffect(new TargetsFilter(cards.Select(x => x.Id)), Power, Duration));
+        }
+    }
+}
