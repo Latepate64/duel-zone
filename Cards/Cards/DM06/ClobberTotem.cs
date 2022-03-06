aa4b4527517ce2e753e302cd2f17a33b1c236bdc
Latepate64 <lauri.koriseva@gmail.com>
2022-03-06 15:08:51 +0200

    Clobber Totem

---
+++ Cards/Cards/DM06/ClobberTotem.cs
@@ -0,0 +1,13 @@
+﻿using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM06
+{
+    class ClobberTotem : Creature
+    {
+        public ClobberTotem() : base("Clobber Totem", 6, 4000, Subtype.MysteryTotem, Civilization.Nature)
+        {
+            AddAbilities(new PowerAttackerAbility(2000), new UnblockableAbility(new CardFilters.BattleZoneMaxPowerCreatureFilter(5000)), new DoubleBreakerAbility());
+        }
+    }
+}
