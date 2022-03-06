fe878e765fbadd206c5571b25469e9b05b3feb27
Latepate64 <lauri.koriseva@gmail.com>
2022-03-06 15:39:10 +0200

    Clone Factory

---
+++ Cards/Cards/DM04/CloneFactory.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class CloneFactory : Spell
+    {
+        public CloneFactory() : base("Clone Factory", 3, Civilization.Water)
+        {
+            AddAbilities(new Engine.Abilities.SpellAbility(new OneShotEffects.SelfManaRecoveryEffect(0, 2, true, CardType.Any)));
+        }
+    }
+}
