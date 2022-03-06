834fa1d867073e47bd367d407580ca33168753ac
Latepate64 <lauri.koriseva@gmail.com>
2022-03-06 14:37:34 +0200

    Charmilia, the Enticer

---
+++ Cards/Cards/DM06/CharmiliaTheEnticer.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM06
+{
+    class CharmiliaTheEnticer : Creature
+    {
+        public CharmiliaTheEnticer() : base("Charmilia, the Enticer", 4, 3000, Subtype.SnowFaerie, Civilization.Nature)
+        {
+            AddAbilities(new TapAbility(new OneShotEffects.SearchDeckEffect(new CardFilters.OwnersDeckCardFilter { CardType = CardType.Creature }, true)));
+        }
+    }
+}
