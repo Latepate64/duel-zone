f9f1a35a8dc5c96bc4f0e4fe687ecb8ad2f27ba8
Latepate64 <lauri.koriseva@gmail.com>
2022-03-06 16:07:24 +0200

    Gajirabute, Vile Centurion

---
+++ Cards/Cards/DM10/GajirabuteVileCenturion.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class GajirabuteVileCenturion : Creature
+    {
+        public GajirabuteVileCenturion() : base("Gajirabute, Vile Centurion", 6, 3000, Subtype.DemonCommand, Civilization.Darkness)
+    {
+            AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.ShieldBurnEffect(new CardFilters.OpponentsShieldZoneCardFilter(), 1, 1, true)));
+        }
+    }
+}
