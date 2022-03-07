47ff5b4af4a0a6989c0edcd0f7e7797954747c6b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-07 19:21:02 +0200

    Electro Explorer Syrion

--- Cards/Cards/DM10/DolmarksTheShadowWarrior.cs
+++ Cards/Cards/DM10/DolmarksTheShadowWarrior.cs
@@ -6,6 +6,7 @@ namespace Cards.Cards.DM10
     {
         public DolmarksTheShadowWarrior() : base("Dolmarks, the Shadow Warrior", 4, 4000, Civilization.Darkness, Civilization.Fire)
         {
+            AddSubtypes(Subtype.Ghost, Subtype.Human);
             AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.DolmarksTheShadowWarriorEffect()));
         }
     }
