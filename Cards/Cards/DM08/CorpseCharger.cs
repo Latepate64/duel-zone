33fc26b264d798dbce1232af795722f137a3d165
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-07 18:19:33 +0200

    Corpse Charger

---
+++ Cards/Cards/DM08/CorpseCharger.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM08
+{
+    class CorpseCharger : Spell
+    {
+        public CorpseCharger() : base("Corpse Charger", 4, Civilization.Darkness)
+        {
+            AddAbilities(new Engine.Abilities.SpellAbility(new OneShotEffects.SalvageCreatureEffect(1, 1)), new StaticAbilities.ChargerAbility());
+        }
+    }
+}
