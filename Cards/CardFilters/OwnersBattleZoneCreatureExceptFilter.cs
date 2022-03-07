1a784cf5c03f744b4bb76fdf2c2a348077a7113a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-07 18:53:31 +0200

    Death Cruzer, the Annihilator

---
+++ Cards/CardFilters/OwnersBattleZoneCreatureExceptFilter.cs
@@ -0,0 +1,30 @@
+﻿using Engine;
+
+namespace Cards.CardFilters
+{
+    class OwnersBattleZoneCreatureExceptFilter : OwnersBattleZoneCreatureFilter
+    {
+        public OwnersBattleZoneCreatureExceptFilter()
+        {
+        }
+
+        public OwnersBattleZoneCreatureExceptFilter(OwnersBattleZoneCreatureExceptFilter filter) : base(filter)
+        {
+        }
+
+        public override bool Applies(Card card, Game game, Player player)
+        {
+            return base.Applies(card, game, player) && card.Id != Target;
+        }
+
+        public override CardFilter Copy()
+        {
+            return new OwnersBattleZoneCreatureExceptFilter(this);
+        }
+
+        public override string ToString()
+        {
+            return $"your other {ToStringBase()}s";
+        }
+    }
+}
