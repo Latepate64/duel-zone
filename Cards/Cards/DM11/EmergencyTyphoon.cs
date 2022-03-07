9487ebd60654c491146c9e4858579ad58282d3cd
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-07 19:26:08 +0200

    Emergency Typhoon

---
+++ Cards/Cards/DM11/EmergencyTyphoon.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class EmergencyTyphoon : Spell
+    {
+        public EmergencyTyphoon() : base("Emergency Typhoon", 2, Civilization.Water)
+        {
+            ShieldTrigger = true;
+            AddAbilities(new Engine.Abilities.SpellAbility(new OneShotEffects.EmergencyTyphoonEffect()));
+        }
+    }
+}
