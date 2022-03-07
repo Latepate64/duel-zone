1fa75359c7377eb1c0122bf1138b945c3ee1d81d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-07 19:35:43 +0200

    Energy Stream

---
+++ Cards/OneShotEffects/DrawEffect.cs
@@ -0,0 +1,35 @@
+﻿using Engine;
+using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    internal class DrawEffect : OneShotEffect
+    {
+        private int _amount;
+
+        public DrawEffect(int amount)
+        {
+            _amount = amount;
+        }
+
+        public DrawEffect(DrawEffect effect)
+        {
+            _amount = effect._amount;
+        }
+
+        public override void Apply(Game game, Ability source)
+        {
+            game.GetPlayer(source.Owner).DrawCards(_amount, game);
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new DrawEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Draw {_amount} cards.";
+        }
+    }
+}
