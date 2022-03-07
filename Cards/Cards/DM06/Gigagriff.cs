9ca70045f03a42f1b7486d83e835362aadb37e78
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-07 18:41:23 +0200

    Gigagriff

---
+++ Cards/Cards/DM06/Gigagriff.cs
@@ -0,0 +1,13 @@
+﻿using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM06
+{
+    class Gigagriff : Creature
+    {
+        public Gigagriff() : base("Gigagriff", 6, 4000, Subtype.Chimera, Civilization.Darkness)
+        {
+            AddAbilities(new BlockerAbility(), new SlayerAbility(), new CannotAttackCreaturesAbility(), new CannotAttackPlayersAbility());
+        }
+    }
+}
