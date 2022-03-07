cac810dbd54797093fab1aaf90c6e4e56f144936
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-07 19:43:05 +0200

    Faerie Life

---
+++ Cards/Cards/DM06/FaerieLife.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class FaerieLife : Spell
+    {
+        public FaerieLife() : base("Faerie Life", 2, Civilization.Nature)
+        {
+            ShieldTrigger = true;
+            AddAbilities(new Engine.Abilities.SpellAbility(new OneShotEffects.PutTopCardsOfDeckIntoManaZoneEffect(1)));
+        }
+    }
+}
