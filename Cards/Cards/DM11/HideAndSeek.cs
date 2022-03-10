8a4707c655c15b519e9845db6f585a22724e5ac6
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-10 19:51:39 +0200

    Hide and Seek

---
+++ Cards/Cards/DM11/HideAndSeek.cs
@@ -0,0 +1,36 @@
+﻿using Cards.OneShotEffects;
+using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM11
+{
+    class HideAndSeek : Spell
+    {
+        public HideAndSeek() : base("Hide and Seek", 4, Civilization.Water, Civilization.Darkness)
+        {
+            AddAbilities(new SpellAbility(new HideAndSeekEffect()));
+        }
+    }
+
+    class HideAndSeekEffect : OneShotEffect
+    {
+        public override void Apply(Game game, Ability source)
+        {
+            foreach (var effect in new OneShotEffect[] { new BounceEffect(1, 1, new CardFilters.OpponentsBattleZoneChoosableNonEvolutionCreatureFilter()), new OpponentRandomDiscardEffect() })
+            {
+                effect.Apply(game, source);
+            }
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new HideAndSeekEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Choose one of your opponent's non-evolution creatures in the battle zone and return it to his hand. Then he discards a card at random from his hand.";
+        }
+    }
+}
