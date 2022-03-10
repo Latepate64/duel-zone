94b773bb876163346bf01db79395d6c2c277c43c
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-10 18:51:44 +0200

    Galklife Dragon

--- Cards/CardFilters/OpponentsBattleZoneMaxPowerCreatureFilter.cs
+++ Cards/CardFilters/OpponentsBattleZoneMaxPowerCreatureFilter.cs
@@ -4,7 +4,7 @@ namespace Cards.CardFilters
 {
     class OpponentsBattleZoneMaxPowerCreatureFilter : OpponentsBattleZoneCreatureFilter
     {
-        public OpponentsBattleZoneMaxPowerCreatureFilter(int power)
+        public OpponentsBattleZoneMaxPowerCreatureFilter(int power, params Common.Civilization[] civilizations) : base(civilizations)
         {
             Power = new PowerFilter(CompareMode.Max, power);
         }
---
+++ Cards/Cards/DM04/GalklifeDragon.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class GalklifeDragon : Creature
+    {
+        public GalklifeDragon() : base("Galklife Dragon", 7, 6000, Subtype.ArmoredDragon, Civilization.Fire)
+        {
+            AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.DestroyAreaOfEffect(new CardFilters.OpponentsBattleZoneMaxPowerCreatureFilter(4000, Civilization.Light))), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
