53709b320a4ff333e55efdb90fc3ca17aad1ed9c
Latepate64 <lauri.koriseva@gmail.com>
2022-03-12 16:35:04 +0200

    Essence Elf

---
+++ Cards/Cards/DM02/EssenceElf.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM02
+{
+    class EssenceElf : Creature
+    {
+        public EssenceElf() : base("Essence Elf", 2, 1000, Subtype.TreeFolk, Civilization.Nature)
+        {
+            AddAbilities(new StaticAbility(new Engine.ContinuousEffects.CostModifyingEffect(-1, new CardFilters.OwnersHandCardFilter { CardType = CardType.Spell })));
+        }
+    }
+}
