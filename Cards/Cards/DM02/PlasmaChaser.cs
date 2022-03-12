cf831056dd5ea11c602a6181357d212e5774221f
Latepate64 <lauri.koriseva@gmail.com>
2022-03-12 17:39:09 +0200

    Plasma Chaser

---
+++ Cards/Cards/DM02/PlasmaChaser.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using Common.Choices;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM02
+{
+    class PlasmaChaser : Creature
+    {
+        public PlasmaChaser() : base("Plasma Chaser", 6, 4000, Subtype.GelFish, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.AttackAbility(new PlasmaChaserEffect()));
+        }
+    }
+
+    class PlasmaChaserEffect : OneShotEffect
+    {
+        public override void Apply(Game game, Ability source)
+        {
+            var player = game.GetPlayer(source.Owner);
+            if (player != null)
+            {
+                var amount = game.BattleZone.GetCreatures(game.GetOpponent(player).Id).Count();
+                if (amount > 0 && player.Choose(new YesNoChoice(source.Owner, $"You may draw {amount} cards."), game).Decision)
+                {
+                    player.DrawCards(amount, game);
+                }
+            }
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new PlasmaChaserEffect();
+        }
+
+        public override string ToString()
+        {
+            return "You may draw a number of cards equal to the number of creatures your opponent has in the battle zone.";
+        }
+    }
+}
