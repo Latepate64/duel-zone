742c1be1b5cc7b27a870f45c7aa4fa5501e7e4eb
Latepate64 <lauri.koriseva@gmail.com>
2022-03-12 15:39:53 +0200

    Crystal Paladin

---
+++ Cards/Cards/DM02/CrystalPaladin.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM02
+{
+    class CrystalPaladin : EvolutionCreature
+    {
+        public CrystalPaladin() : base("Crystal Paladin", 4, 5000, Subtype.LiquidPeople, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.BounceAreaOfEffect(new CardFilters.BattleZoneBlockerCreatureFilter())));
+        }
+    }
+}
