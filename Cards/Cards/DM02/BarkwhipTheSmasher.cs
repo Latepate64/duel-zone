2e064a95a7e1bf29a9b39d95bf0fcac6d4db67e0
Latepate64 <lauri.koriseva@gmail.com>
2022-03-12 15:25:52 +0200

    Barkwhip, the Smasher

---
+++ Cards/Cards/DM02/BarkwhipTheSmasher.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM02
+{
+    class BarkwhipTheSmasher : EvolutionCreature
+    {
+        public BarkwhipTheSmasher() : base("Barkwhip, the Smasher", 2, 5000, Subtype.BeastFolk, Civilization.Nature)
+        {
+            AddAbilities(new StaticAbility(new Engine.ContinuousEffects.PowerModifyingEffect(2000, new CardFilters.OwnersBattleZoneCreatureExceptFilter(Subtype.BeastFolk), new Engine.Durations.Indefinite(), new Conditions.TappedCondition(new Engine.TargetFilter()))));
+        }
+    }
+}
