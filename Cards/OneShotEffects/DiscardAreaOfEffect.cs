9116a24d99eff4e7ed2e168637736949ae6c2b1a
Latepate64 <lauri.koriseva@gmail.com>
2022-03-12 17:19:51 +0200

    Lost Soul

---
+++ Cards/OneShotEffects/DiscardAreaOfEffect.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class DiscardAreaOfEffect : CardMovingAreaOfEffect
+    {
+        public DiscardAreaOfEffect(CardFilter filter) : base(ZoneType.Hand, ZoneType.Graveyard, filter)
+        {
+        }
+
+        public DiscardAreaOfEffect(DiscardAreaOfEffect effect) : base(effect)
+        {
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new DiscardAreaOfEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Discard {Filter}.";
+        }
+    }
+}
