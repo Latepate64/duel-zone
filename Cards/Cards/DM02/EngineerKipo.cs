136b6d6920527a99462aa2611aa7e90d2d228edf
Latepate64 <lauri.koriseva@gmail.com>
2022-03-12 16:33:18 +0200

    Engineer Kipo

---
+++ Cards/Cards/DM02/EngineerKipo.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM02
+{
+    class EngineerKipo : Creature
+    {
+        public EngineerKipo() : base("Engineer Kipo", 2, 2000, Subtype.Xenoparts, Civilization.Fire)
+        {
+            AddAbilities(new TriggeredAbilities.DestroyedAbility(new OneShotEffects.MutualManaSacrificeEffect(1)));
+        }
+    }
+}
