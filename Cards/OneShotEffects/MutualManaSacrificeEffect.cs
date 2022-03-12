9c44b8ca92cef350ee5c4a2ebb3b6e6728c1be31
Latepate64 <lauri.koriseva@gmail.com>
2022-03-12 15:34:11 +0200

    Bombersaur

---
+++ Cards/OneShotEffects/MutualManaSacrificeEffect.cs
@@ -0,0 +1,39 @@
+﻿using Cards.CardFilters;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class MutualManaSacrificeEffect : OneShotEffect
+    {
+        public int Amount { get; }
+
+        public MutualManaSacrificeEffect(int amount)
+        {
+            Amount = amount;
+        }
+
+        public MutualManaSacrificeEffect(MutualManaSacrificeEffect effect)
+        {
+            Amount = effect.Amount;
+        }
+
+        public override void Apply(Game game, Ability source)
+        {
+            foreach (var effect in new OneShotEffect[] { new ManaBurnEffect(new OwnersManaZoneCardFilter(), Amount, Amount, true), new ManaBurnEffect(new OpponentsManaZoneCardFilter(), Amount, Amount, false) })
+            {
+                effect.Apply(game, source);
+            }
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new MutualManaSacrificeEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Each player chooses 2 cards in his mana zone and puts them into his graveyard.";
+        }
+    }
+}
