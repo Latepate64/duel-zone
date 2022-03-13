6abdb9efc45b932acea3c1bbfcf24a979aa3eaee
Latepate64 <lauri.koriseva@gmail.com>
2022-03-13 11:43:25 +0200

    Refactoring.

--- Cards/Cards/DM07/AquaAgent.cs
+++ Cards/Cards/DM07/AquaAgent.cs
@@ -1,4 +1,8 @@
-﻿using Cards.StaticAbilities;
+﻿using Common;
+using Common.Choices;
+using Common.GameEvents;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
 
 namespace Cards.Cards.DM07
 {
@@ -8,7 +12,51 @@ namespace Cards.Cards.DM07
         {
             //TODO: Water stealth
             // When this creature would be destroyed, you may return it to your hand instead.
-            AddAbilities(new AquaAgentAbility());
+            AddAbilities(new StaticAbility(new AquaAgentAbilityEffect(new CardMovedEvent { Source = ZoneType.BattleZone, Destination = ZoneType.Graveyard })));
+        }
+    }
+
+    public class AquaAgentAbilityEffect : ReplacementEffect
+    {
+        public AquaAgentAbilityEffect(GameEvent gameEvent) : base(gameEvent)
+        {
+        }
+
+        public AquaAgentAbilityEffect(AquaAgentAbilityEffect effect) : base(effect)
+        {
+        }
+
+        public override ContinuousEffect Copy()
+        {
+            return new AquaAgentAbilityEffect(this);
+        }
+
+        public override GameEvent Apply(Engine.Game game, Engine.Player player)
+        {
+            if (player.Choose(new YesNoChoice(player.Id, ToString()), game).Decision)
+            {
+                var newEvent = EventToReplace.Copy() as CardMovedEvent;
+                newEvent.Destination = ZoneType.Hand;
+                return newEvent;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override bool Replaceable(GameEvent gameEvent, Engine.Game game)
+        {
+            if (gameEvent is CardMovedEvent e)
+            {
+                return e.Source == ZoneType.BattleZone && e.Destination == ZoneType.Graveyard && Filter.Applies(game.GetCard(e.CardInSourceZone), game, game.GetPlayer(e.Player.Id));
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "When this creature would be destroyed, you may return it to your hand instead.";
         }
     }
 }
--- Cards/Cards/DM09/PetrovaChannelerOfSuns.cs
+++ Cards/Cards/DM09/PetrovaChannelerOfSuns.cs
@@ -1,4 +1,9 @@
 ﻿using Cards.StaticAbilities;
+using Common;
+using Common.GameEvents;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
+using System;
 
 namespace Cards.Cards.DM09
 {
@@ -7,10 +12,45 @@ namespace Cards.Cards.DM09
         public PetrovaChannelerOfSuns() : base("Petrova, Channeler of Suns", 5, 3500, Common.Subtype.MechaDelSol, Common.Civilization.Light)
         {
             // When you put this creature into the battle zone, choose a race other than Mecha del Sol. Each creature of that race gets +4000 power.
-            AddAbilities(new PetrovaAbility());
+            AddAbilities(new StaticAbility(new PetrovaChannelerOfSunsEffect(new CardMovedEvent { Source = ZoneType.Anywhere, Destination = ZoneType.BattleZone })));
 
             // Whenever your opponent would choose a creature in the battle zone, he can't choose this one. (It can still be attacked and blocked.)
             AddAbilities(new UnchoosableAbility());
+        }
+    }
+
+    internal class PetrovaChannelerOfSunsEffect : ReplacementEffect
+    {
+        public PetrovaChannelerOfSunsEffect(GameEvent gameEvent) : base(gameEvent)
+        {
+        }
+
+        public PetrovaChannelerOfSunsEffect(PetrovaChannelerOfSunsEffect effect) : base(effect)
+        {
+        }
+
+        public override ContinuousEffect Copy()
+        {
+            return new PetrovaChannelerOfSunsEffect(this);
+        }
+
+        public override GameEvent Apply(Engine.Game game, Engine.Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Replaceable(GameEvent gameEvent, Engine.Game game)
+        {
+            if (gameEvent is CardMovedEvent e)
+            {
+                return e.Destination == ZoneType.BattleZone && Filter.Applies(game.GetCard(e.CardInSourceZone), game, game.GetPlayer(e.Player.Id));
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "As you put this creature into the battle zone, choose a race other than Mecha Del Sol. Each creature of that race gets +4000 power.";
         }
     }
 }
