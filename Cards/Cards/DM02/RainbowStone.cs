10da2f3cdfa453f2668f3abedc59ee319313bf77
Latepate64 <lauri.koriseva@gmail.com>
2022-03-13 13:25:18 +0200

    Rainbow Stone. Refactored SearchDeckEffect.

---
+++ Cards/Cards/DM02/RainbowStone.cs
@@ -0,0 +1,41 @@
+﻿using Cards.OneShotEffects;
+using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM02
+{
+    class RainbowStone : Spell
+    {
+        public RainbowStone() : base("Rainbow Stone", 4, Civilization.Nature)
+        {
+            AddAbilities(new SpellAbility(new RainbowStoneEffect()));
+        }
+    }
+
+    class RainbowStoneEffect : SearchDeckEffect
+    {
+        public RainbowStoneEffect() : base(new CardFilters.OwnersDeckCardFilter())
+        {
+        }
+
+        public RainbowStoneEffect(SearchDeckEffect effect) : base(effect)
+        {
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new RainbowStoneEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "Search your deck. You may take a card from your deck and put it into your mana zone. Then shuffle your deck.";
+        }
+
+        protected override void Apply(Game game, Ability source, params Engine.Card[] cards)
+        {
+            game.Move(ZoneType.Deck, ZoneType.ManaZone, cards);
+        }
+    }
+}
---
+++ Cards/OneShotEffects/TutoringEffect.cs
@@ -0,0 +1,43 @@
+﻿using Engine;
+using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class TutoringEffect : SearchDeckEffect
+    {
+        public bool Reveal { get; }
+
+        public TutoringEffect(CardFilter filter, bool reveal) : base(filter)
+        {
+            Reveal = reveal;
+        }
+
+        public TutoringEffect(TutoringEffect effect) : base(effect)
+        {
+            Reveal = effect.Reveal;
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new TutoringEffect(this);
+        }
+
+        public override string ToString()
+        {
+            var reveal = Reveal ? ", show it to your opponent," : "";
+            return $"Search your deck. You may take {Filter}{reveal} and put it into your hand. Then shuffle your deck.";
+        }
+
+        protected override void Apply(Game game, Ability source, params Card[] cards)
+        {
+            if (Reveal)
+            {
+                foreach (var card in cards)
+                {
+                    game.GetOwner(card).Reveal(game, card);
+                }
+            }
+            game.Move(Common.ZoneType.Deck, Common.ZoneType.Hand, cards);
+        }
+    }
+}
