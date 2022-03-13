13775c40b461686e43c76f826e6daea888cd0e84
Latepate64 <lauri.koriseva@gmail.com>
2022-03-13 14:52:13 +0200

    Baby Zoppe

---
+++ Cards/Cards/DM03/BabyZoppe.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM03
+{
+    class BabyZoppe : Creature
+    {
+        public BabyZoppe() : base("Baby Zoppe", 3, 2000, Subtype.FireBird, Civilization.Fire)
+        {
+            AddAbilities(new Engine.Abilities.StaticAbility(new Engine.ContinuousEffects.PowerModifyingEffect(2000, new Conditions.AllOfCivilizationCondition(Civilization.Fire))));
+        }
+    }
+}
