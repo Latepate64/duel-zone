21e83cd88cc628e308ba2f4119ac21552aa99f11
Latepate64 <lauri.koriseva@gmail.com>
2022-03-13 15:01:14 +0200

    Chaos Fish

---
+++ Cards/Cards/DM03/ChaosFish.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM03
+{
+    class ChaosFish : Creature
+    {
+        public ChaosFish() : base("Chaos Fish", 7, 1000, Subtype.GelFish, Civilization.Water)
+        {
+            AddAbilities(new StaticAbility(new ContinuousEffects.PowerModifyingMultiplierEffect(1000, new CardFilters.OwnersBattleZoneCreatureExceptFilter(Civilization.Water))), new TriggeredAbilities.AttackAbility(new ChaosFishEffect()));
+        }
+    }
+
+    class ChaosFishEffect : OneShotEffect
+    {
+        public override void Apply(Game game, Ability source)
+        {
+            new OneShotEffects.ControllerMayDrawCardsEffect(game.GetAllCards(new CardFilters.OwnersBattleZoneCreatureExceptFilter(Civilization.Water), source.Owner).Count()).Apply(game, source);
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new ChaosFishEffect();
+        }
+
+        public override string ToString()
+        {
+            return "You may draw a card for each of your other water creatures in the battle zone.";
+        }
+    }
+}
