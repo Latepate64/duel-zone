d7fc4f20e494926c592a7af9a0cb0e1de07ebfb5
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-16 19:52:52 +0200

    Masked Pomegranate

---
+++ Cards/Cards/DM03/MaskedPomegranate.cs
@@ -0,0 +1,15 @@
+﻿using Cards.CardFilters;
+using Cards.StaticAbilities;
+using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM03
+{
+    class MaskedPomegranate : Creature
+    {
+        public MaskedPomegranate() : base("Masked Pomegranate", 5, 1000, Subtype.TreeFolk, Civilization.Nature)
+        {
+            AddAbilities(new StaticAbility(new ContinuousEffects.PowerModifyingMultiplierEffect(1000, new OwnersBattleZoneCivilizationCreatureExceptFilter(Civilization.Nature))), new UnblockableAbility(new BattleZoneMaxPowerCreatureFilter(4000)));
+        }
+    }
+}
