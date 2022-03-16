f0f89a75f2ef32f03adc7cbf2cc9148f18c8f3d3
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-16 19:58:12 +0200

    Raging Dash-Horn

---
+++ Cards/Cards/DM03/RagingDashHorn.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM03
+{
+    class RagingDashHorn : Creature
+    {
+        public RagingDashHorn() : base("Raging Dash-Horn", 5, 4000, Subtype.HornedBeast, Civilization.Nature)
+        {
+            var condition = new Conditions.AllOfCivilizationCondition(Civilization.Nature);
+            AddAbilities(new Engine.Abilities.StaticAbility(new Engine.ContinuousEffects.PowerModifyingEffect(3000, condition)), new StaticAbilities.DoubleBreakerAbility(condition));
+        }
+    }
+}
