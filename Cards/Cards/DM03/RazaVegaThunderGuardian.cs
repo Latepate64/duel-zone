e29df37e5866de91ce20bac87e04ffb046ca69dc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-16 19:59:59 +0200

    Raza Vega, Thunder Guardian

---
+++ Cards/Cards/DM03/RazaVegaThunderGuardian.cs
@@ -0,0 +1,13 @@
+﻿using Cards.StaticAbilities;
+using Common;
+
+namespace Cards.Cards.DM03
+{
+    class RazaVegaThunderGuardian : Creature
+    {
+        public RazaVegaThunderGuardian() : base("Raza Vega, Thunder Guardian", 10, 3000, Subtype.Guardian, Civilization.Light)
+        {
+            AddAbilities(new BlockerAbility(), new WhenThisCreatureWouldBeDestroyedAddItToYourShieldsFaceDownInsteadAbility());
+        }
+    }
+}
