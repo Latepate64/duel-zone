8f04a532bc54a5e9641cd14eed9173bad61e3f1a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-16 19:50:58 +0200

    Mana Nexus

---
+++ Cards/Cards/DM03/ManaNexus.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM03
+{
+    class ManaNexus : Spell
+    {
+        public ManaNexus() : base("Mana Nexus", 4, Civilization.Nature)
+        {
+            ShieldTrigger = true;
+            AddAbilities(new Engine.Abilities.SpellAbility(new OneShotEffects.FromManaZoneToShieldsFaceDownEffect()));
+        }
+    }
+}
---
+++ Cards/OneShotEffects/FromManaZoneToShieldsFaceDownEffect.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class FromManaZoneToShieldsFaceDownEffect : CardMovingChoiceEffect
+    {
+        public FromManaZoneToShieldsFaceDownEffect(FromManaZoneToShieldsFaceDownEffect effect) : base(effect)
+        {
+        }
+
+        public FromManaZoneToShieldsFaceDownEffect() : base(new CardFilters.OwnersManaZoneCardFilter(), 1, 1, true, ZoneType.ManaZone, ZoneType.ShieldZone)
+        {
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new FromManaZoneToShieldsFaceDownEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Add {Filter} from your mana zone to your shields face down.";
+        }
+    }
+}
