b0473e8681370acd76b5bb91d14edbb99545cefc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-16 19:20:21 +0200

    Garkago Dragon

---
+++ Cards/Cards/DM03/GarkagoDragon.cs
@@ -0,0 +1,14 @@
+﻿using Cards.StaticAbilities;
+using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM03
+{
+    class GarkagoDragon : Creature
+    {
+        public GarkagoDragon() : base("Garkago Dragon", 7, 6000, Subtype.ArmoredDragon, Civilization.Fire)
+        {
+            AddAbilities(new DoubleBreakerAbility(), new StaticAbility(new ContinuousEffects.PowerModifyingMultiplierEffect(1000, new CardFilters.OwnersBattleZoneCivilizationCreatureExceptFilter(Civilization.Fire))), new CanAttackUntappedCreaturesAbility());
+        }
+    }
+}
