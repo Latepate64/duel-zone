e142828b09aedf347401951ceba3f32862e7c74e
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-16 19:44:35 +0200

    Legendary Bynor

---
+++ Cards/Cards/DM03/LegendaryBynor.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM03
+{
+    class LegendaryBynor : EvolutionCreature
+    {
+        public LegendaryBynor() : base("Legendary Bynor", 6, 8000, Subtype.Leviathan, Civilization.Water)
+        {
+            AddAbilities(new Engine.Abilities.StaticAbility(new Engine.ContinuousEffects.UnblockableEffect(new CardFilters.OwnersBattleZoneCivilizationCreatureExceptFilter(Civilization.Water), new CardFilters.BattleZoneCreatureFilter())));
+        }
+    }
+}
