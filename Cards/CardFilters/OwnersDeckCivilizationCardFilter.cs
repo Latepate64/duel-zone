fff5ac2493b96e7be5fd1c8049066f34218a0f7d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-16 19:41:36 +0200

    King Ponitas

---
+++ Cards/CardFilters/OwnersDeckCivilizationCardFilter.cs
@@ -0,0 +1,34 @@
+﻿using Engine;
+
+namespace Cards.CardFilters
+{
+    class OwnersDeckCivilizationCardFilter : OwnersDeckCardFilter, ICivilizationFilterable
+    {
+        public OwnersDeckCivilizationCardFilter(Common.Civilization civilization)
+        {
+            CivilizationFilter = new CivilizationFilter(civilization);
+        }
+
+        public OwnersDeckCivilizationCardFilter(OwnersDeckCivilizationCardFilter filter) : base(filter)
+        {
+            CivilizationFilter = filter.CivilizationFilter;
+        }
+
+        public CivilizationFilter CivilizationFilter { get; }
+
+        public override bool Applies(Card card, Game game, Player player)
+        {
+            return base.Applies(card, game, player) && CivilizationFilter.Applies(card, game, player);
+        }
+
+        public override CardFilter Copy()
+        {
+            return new OwnersDeckCivilizationCardFilter(this);
+        }
+
+        public override string ToString()
+        {
+            return $"a {CivilizationFilter} card from your deck";
+        }
+    }
+}
