69e3417cb3da7f5d5b4496c1deeba3450c36e65b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-17 17:54:56 +0200

    Stinger Ball

---
+++ Cards/Cards/DM03/StingerBall.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM03
+{
+    class StingerBall : Creature
+    {
+        public StingerBall() : base("Stinger Ball", 3, 1000, Subtype.CyberVirus, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.AttackAbility(new OneShotEffects.LookEffect(new CardFilters.OpponentsShieldZoneCardFilter(), 0, 1, true)));
+        }
+    }
+}
