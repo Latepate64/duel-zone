62b8f1779d3480058239e65373823cec6f8948ba
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-17 17:55:59 +0200

    Curious Eye

---
+++ Cards/Cards/DM07/CuriousEye.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class CuriousEye : Creature
+    {
+        public CuriousEye() : base("Curious Eye", 3, 1000, Subtype.CyberVirus, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.AttackAbility(new OneShotEffects.LookEffect(new CardFilters.OpponentsShieldZoneCardFilter(), 0, 1, true)));
+        }
+    }
+}
