ce7ce3675c8a32b1270efc2c511404b1f9a5bb04
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-17 17:58:27 +0200

    Sundrop Armor

---
+++ Cards/Cards/DM03/SundropArmor.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM03
+{
+    class SundropArmor : Spell
+    {
+        public SundropArmor() : base("Sundrop Armor", 4, Civilization.Light)
+        {
+            AddAbilities(new Engine.Abilities.SpellAbility(new OneShotEffects.ShieldAdditionEffect(new CardFilters.OwnersHandCardFilter(), 1, 1, true)));
+        }
+    }
+}
