b1cd20e75f1b39bf6ce2816470b0e645ffdfc135
Latepate64 <lauri.koriseva@gmail.com>
2022-03-19 19:01:35 +0200

    Ballom, Master of Death

---
+++ Cards/Cards/DM04/BallomMasterOfDeath.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class BallomMasterOfDeath : EvolutionCreature
+    {
+        public BallomMasterOfDeath() : base("Ballom, Master of Death", 8, 12000, Subtype.DemonCommand, Civilization.Darkness)
+        {
+            AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.DestroyAreaOfEffect(new CardFilters.BattleZoneNonCivilizationCreatureFilter(Civilization.Darkness))), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
