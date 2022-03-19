1750e05c4fc30ade32fb0bd7defcb919ec441031
Latepate64 <lauri.koriseva@gmail.com>
2022-03-19 17:03:23 +0200

    Alcadeias, Lord of Spirits

---
+++ Cards/Cards/DM04/AlcadeiasLordOfSpirits.cs
@@ -0,0 +1,35 @@
+﻿using Cards.CardFilters;
+using Common;
+using Engine.ContinuousEffects;
+
+namespace Cards.Cards.DM04
+{
+    class AlcadeiasLordOfSpirits : EvolutionCreature
+    {
+        public AlcadeiasLordOfSpirits() : base("Alcadeias, Lord of Spirits", 6, 12500, Subtype.AngelCommand, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.DoubleBreakerAbility(), new Engine.Abilities.StaticAbility(new AlcadeiasLordOfSpiritsEffect()));
+        }
+    }
+
+    class AlcadeiasLordOfSpiritsEffect : CannotUseCardEffect
+    {
+        public AlcadeiasLordOfSpiritsEffect(AlcadeiasLordOfSpiritsEffect effect) : base(effect)
+        {
+        }
+
+        public AlcadeiasLordOfSpiritsEffect() : base(new NonCivilizationSpellFilter(Civilization.Light))
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new AlcadeiasLordOfSpiritsEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "Players can't cast spells other than light spells.";
+        }
+    }
+}
