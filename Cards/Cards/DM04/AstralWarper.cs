a3b9b1acb1baee0c6cd5f7b45d5754dec5dd82c6
Latepate64 <lauri.koriseva@gmail.com>
2022-03-19 17:05:32 +0200

    Astral Warper

---
+++ Cards/Cards/DM04/AstralWarper.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class AstralWarper : EvolutionCreature
+    {
+        public AstralWarper() : base("Astral Warper", 6, 5000, Subtype.CyberVirus, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.ControllerMayDrawCardsEffect(3)));
+        }
+    }
+}
