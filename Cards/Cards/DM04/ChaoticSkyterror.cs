3679ed5a65201c944f6fc4f8f37ff4aa18f75986
Latepate64 <lauri.koriseva@gmail.com>
2022-03-20 06:54:48 +0200

    Chaotic Skyterror

---
+++ Cards/Cards/DM04/ChaoticSkyterror.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using Engine.ContinuousEffects;
+using Engine.Durations;
+
+namespace Cards.Cards.DM04
+{
+    class ChaoticSkyterror : Creature
+    {
+        public ChaoticSkyterror() : base("Chaotic Skyterror", 5, 4000, Subtype.ArmoredWyvern, Civilization.Fire)
+        {
+            AddAbilities(new Engine.Abilities.StaticAbility(new ChaoticSkyterrorEffect()));
+        }
+    }
+
+    class ChaoticSkyterrorEffect : AbilityGrantingEffect
+    {
+        public ChaoticSkyterrorEffect(ChaoticSkyterrorEffect effect) : base(effect)
+        {
+        }
+
+        public ChaoticSkyterrorEffect() : base(new CardFilters.BattleZoneSubtypeCreatureFilter(Subtype.DemonCommand), new Indefinite(), new StaticAbilities.PowerAttackerAbility(4000), new StaticAbilities.DoubleBreakerAbility())
+        {
+        }
+
+        public override ContinuousEffect Copy()
+        {
+            return new ChaoticSkyterrorEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "Each Demon Command in the battle zone has \"power attacker +4000\" and \"double breaker.\"";
+        }
+    }
+}
