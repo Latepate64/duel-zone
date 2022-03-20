c8e7f1672bead79fafa122932ec859d8e50dbde0
Latepate64 <lauri.koriseva@gmail.com>
2022-03-20 07:59:59 +0200

    Fu Reil, Seeker of Storms

---
+++ Cards/Cards/DM04/FuReilSeekerOfStorms.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class FuReilSeekerOfStorms : Creature
+    {
+        public FuReilSeekerOfStorms() : base("Fu Reil, Seeker of Storms", 6, 5000, Subtype.MechaThunder, Civilization.Light)
+        {
+            AddAbilities(new Engine.Abilities.StaticAbility(new ContinuousEffects.PlayersCannotUseShieldTriggerAbilitiesOfCivilizationCardsEffect(Civilization.Darkness)));
+        }
+    }
+}
---
+++ Engine/ContinuousEffects/CannotUseShieldTriggerEffect.cs
@@ -0,0 +1,15 @@
+﻿using Engine.Durations;
+
+namespace Engine.ContinuousEffects
+{
+    public abstract class CannotUseShieldTriggerEffect : ContinuousEffect
+    {
+        protected CannotUseShieldTriggerEffect(CannotUseShieldTriggerEffect effect) : base(effect)
+        {
+        }
+
+        protected CannotUseShieldTriggerEffect(ICardFilter filter, params Condition[] conditions) : base(filter, conditions)
+        {
+        }
+    }
+}
