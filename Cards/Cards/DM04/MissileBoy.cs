913f0c9284883eaeb9f381377cc793e3754218d4
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-21 18:46:03 +0200

    Missile Boy

---
+++ Cards/Cards/DM04/MissileBoy.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class MissileBoy : Creature
+    {
+        public MissileBoy() : base("Missile Boy", 3, 1000, Subtype.Human, Civilization.Fire)
+        {
+            AddAbilities(new StaticAbilities.EachCivilizationCardCostsMoreAbility(Civilization.Light, 1));
+        }
+    }
+}
