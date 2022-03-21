ec240845ab91c51ee3ae43bceee21df8c6f1af1e
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-21 19:58:56 +0200

    Smile Angler

---
+++ Cards/Cards/DM04/SmileAngler.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class SmileAngler : Creature
+    {
+        public SmileAngler() : base("Smile Angler", 6, 3000, Subtype.GelFish, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.AttackAbility(new OneShotEffects.OpponentManaRecoveryEffect(0, 1, true)));
+        }
+    }
+}
