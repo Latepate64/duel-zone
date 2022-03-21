2c708c4d9cd80e2bef963e900114cc01c5972783
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-21 19:26:49 +0200

    Purple Piercer

---
+++ Cards/Cards/DM04/PurplePiercer.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM04
+{
+    class PurplePiercer : Creature
+    {
+        public PurplePiercer() : base("Purple Piercer", 3, 2000, Subtype.BrainJacker, Civilization.Darkness)
+        {
+            AddAbilities(new StaticAbilities.ThisCreatureCannotBeAttackedByCivilizationCreaturesAbility(Civilization.Light), new StaticAbilities.UnblockableAbility(new CardFilters.OpponentsBattleZoneCivilizationCreatureFilter(Civilization.Light)));
+        }
+    }
+}
