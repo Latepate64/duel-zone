ddda8b491dcecfa8c19f1093dc6d3dabc8b828bc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-21 18:42:06 +0200

    Abduction Charger name fix

--- Cards/Cards/DM09/AbductionCharger.cs
+++ Cards/Cards/DM09/AbductionCharger.cs
@@ -5,7 +5,7 @@ namespace Cards.Cards.DM09
 {
     class AbductionCharger : Charger
     {
-        public AbductionCharger() : base("AbductionCharger", 7, Civilization.Water)
+        public AbductionCharger() : base("Abduction Charger", 7, Civilization.Water)
         {
             AddSpellAbilities(new BounceEffect(0, 2, new CardFilters.BattleZoneCreatureFilter()));
         }
