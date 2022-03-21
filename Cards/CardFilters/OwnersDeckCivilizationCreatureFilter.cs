3bd88cfc375a1ffa76f89ecd64e17bb19ee39d55
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-21 19:09:19 +0200

    Niofa, Horned Protector

---
+++ Cards/CardFilters/OwnersDeckCivilizationCreatureFilter.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using Engine;
+
+namespace Cards.CardFilters
+{
+    class OwnersDeckCivilizationCreatureFilter : OwnersDeckCivilizationCardFilter
+    {
+        public OwnersDeckCivilizationCreatureFilter(Civilization civilization) : base(civilization)
+        {
+        }
+
+        public OwnersDeckCivilizationCreatureFilter(OwnersDeckCivilizationCreatureFilter filter) : base(filter)
+        {
+        }
+
+        public override bool Applies(Engine.ICard card, IGame game, Engine.IPlayer player)
+        {
+            return base.Applies(card, game, player) && card.CardType == CardType.Creature;
+        }
+
+        public override CardFilter Copy()
+        {
+            return new OwnersDeckCivilizationCreatureFilter(this);
+        }
+    }
+}
