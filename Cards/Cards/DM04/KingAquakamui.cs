5e25b94e527c3a507c5a1e717c417dda1aa653ed
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-21 18:12:47 +0200

    King Aquakamui

---
+++ Cards/Cards/DM04/KingAquakamui.cs
@@ -0,0 +1,69 @@
+﻿using Common;
+using Common.Choices;
+using Engine;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
+using Engine.Durations;
+using System.Linq;
+
+namespace Cards.Cards.DM04
+{
+    class KingAquakamui : Creature
+    {
+        public KingAquakamui() : base("King Aquakamui", 7, 5000, Subtype.Leviathan, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new KingAquakamuiOneShotEffect()), new KingAquakamuiStaticAbility());
+        }
+    }
+
+    class KingAquakamuiOneShotEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            var player = game.GetPlayer(source.Owner);
+            if (player.Choose(new YesNoChoice(player.Id, ToString()), game).Decision)
+            {
+                game.Move(ZoneType.Graveyard, ZoneType.Hand, player.Graveyard.Cards.Where(x => x.Subtypes.Contains(Subtype.AngelCommand) || x.Subtypes.Contains(Subtype.DemonCommand)).ToArray());
+            }
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new KingAquakamuiOneShotEffect();
+        }
+
+        public override string ToString()
+        {
+            return "You may return all Angel Commands and all Demon Commands from your graveyard to your hand.";
+        }
+    }
+
+    class KingAquakamuiStaticAbility : StaticAbility
+    {
+        public KingAquakamuiStaticAbility() : base(new KingAquakamuiContinuousEffect())
+        {
+        }
+    }
+
+    class KingAquakamuiContinuousEffect : PowerModifyingEffect
+    {
+        public KingAquakamuiContinuousEffect(KingAquakamuiContinuousEffect effect) : base(effect)
+        {
+        }
+
+        public KingAquakamuiContinuousEffect() : base(2000, new CardFilters.BattleZoneSubtypeCreatureFilter(Subtype.AngelCommand, Subtype.DemonCommand), new Indefinite())
+        {
+        }
+
+        public override ContinuousEffect Copy()
+        {
+            return new KingAquakamuiContinuousEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "You may return all Angel Commands and all Demon Commands from your graveyard to your hand.";
+        }
+    }
+}
