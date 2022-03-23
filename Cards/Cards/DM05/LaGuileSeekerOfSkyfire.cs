42176abdc0f375d0ca897c206b61f10b0f025926
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 18:28:52 +0200

    La Guile, Seeker of Skyfire

---
+++ Cards/Cards/DM05/LaGuileSeekerOfSkyfire.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class LaGuileSeekerOfSkyfire : Creature
+    {
+        public LaGuileSeekerOfSkyfire() : base("La Guile, Seeker of Skyfire", 6, 7500, Subtype.MechaThunder, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
