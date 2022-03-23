a8f63eb1c9368c51020eca7a072a2e845b98e773
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 19:43:03 +0200

    Spikestrike Ichthys Q

---
+++ Cards/Cards/DM05/SpikestrikeIchthysQ.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class SpikestrikeIchthysQ : Creature
+    {
+        public SpikestrikeIchthysQ() : base("Spikestrike Ichthys Q", 6, 3000, Civilization.Water)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.Fish);
+            AddAbilities(new StaticAbilities.SurvivorAbility(new StaticAbilities.UnblockableAbility()));
+        }
+    }
+}
