b00d72df54ba59acf4f39f6ebf3fd9fc201c2fc5
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 17:45:23 +0200

    Gigazoul

---
+++ Cards/Cards/DM05/Gigazoul.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class Gigazoul : Creature
+    {
+        public Gigazoul() : base("Gigazoul", 3, 3000, Subtype.Chimera, Civilization.Darkness)
+        {
+            AddAbilities(new StaticAbilities.WhileYourOpponentHasNoShieldsThisCreatureCannotAttackAbility());
+        }
+    }
+}
