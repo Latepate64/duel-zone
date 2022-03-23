4f913fd7b044399bdd4a346836291cf73e03d4dd
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 19:51:47 +0200

    Syrius, Firmament Elemental

---
+++ Cards/Cards/DM05/SyriusFirmamentElemental.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class SyriusFirmamentElemental : Creature
+    {
+        public SyriusFirmamentElemental() : base("Syrius, Firmament Elemental", 11, 12000, Subtype.AngelCommand, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.BlockerAbility(), new StaticAbilities.TripleBreakerAbility());
+        }
+    }
+}
