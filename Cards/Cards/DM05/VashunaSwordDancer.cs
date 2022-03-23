7ca9a26bf54e9337b0559af37ab1b0b764288c5e
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 17:47:18 +0200

    Vashuna, Sword Dancer

---
+++ Cards/Cards/DM05/VashunaSwordDancer.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class VashunaSwordDancer : Creature
+    {
+        public VashunaSwordDancer() : base("Vashuna, Sword Dancer", 5, 7000, Subtype.DemonCommand, Civilization.Darkness)
+        {
+            AddAbilities(new StaticAbilities.WhileYourOpponentHasNoShieldsThisCreatureCannotAttackAbility(), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
