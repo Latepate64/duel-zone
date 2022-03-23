e4ebb4c4d4e3a073bfb8406028794ccfc286b4fe
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 19:28:16 +0200

    Skullsweeper Q

---
+++ Cards/Cards/DM05/SkullsweeperQ.cs
@@ -0,0 +1,21 @@
+﻿using Cards.OneShotEffects;
+using Common;
+
+namespace Cards.Cards.DM05
+{
+    class SkullsweeperQ : Creature
+    {
+        public SkullsweeperQ() : base("Skullsweeper Q", 4, 1000, Civilization.Darkness)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.BrainJacker);
+            AddAbilities(new StaticAbilities.SurvivorAbility(new TriggeredAbilities.AttackAbility(new YourOpponentChoosesAndDiscardsCardFromHisHandEffect())));
+        }
+    }
+
+    class YourOpponentChoosesAndDiscardsCardFromHisHandEffect : DiscardEffect
+    {
+        public YourOpponentChoosesAndDiscardsCardFromHisHandEffect() : base(new CardFilters.OpponentsHandCardFilter(), 1, 1, false)
+        {
+        }
+    }
+}
