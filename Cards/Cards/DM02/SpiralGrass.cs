7a9a6f2d235116a4c2faff842398c98ba425110b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 18:27:17 +0200

    La Byle, Seeker of the Winds

--- Cards/Cards/DM02/SpiralGrass.cs
+++ Cards/Cards/DM02/SpiralGrass.cs
@@ -1,7 +1,6 @@
-﻿using Cards.TriggeredAbilities;
+﻿using Cards.OneShotEffects;
+using Cards.TriggeredAbilities;
 using Common;
-using Engine;
-using Engine.Abilities;
 
 namespace Cards.Cards.DM02
 {
@@ -9,30 +8,7 @@ namespace Cards.Cards.DM02
     {
         public SpiralGrass() : base("Spiral Grass", 4, 2500, Subtype.StarlightTree, Civilization.Light)
         {
-            AddAbilities(new StaticAbilities.BlockerAbility(), new BlockAbility(new SpiralGrassEffect()));
-        }
-    }
-
-    class SpiralGrassEffect : OneShotEffect
-    {
-        public SpiralGrassEffect()
-        {
-        }
-
-        public override object Apply(IGame game, IAbility source)
-        {
-            game.AddDelayedTriggeredAbility(new AfterBattleAbility(new OneShotEffects.UntapAreaOfEffect(new TargetFilter())), new Engine.Durations.Once());
-            return true;
-        }
-
-        public override OneShotEffect Copy()
-        {
-            return new SpiralGrassEffect();
-        }
-
-        public override string ToString()
-        {
-            return "Untap it after it battles.";
+            AddAbilities(new StaticAbilities.BlockerAbility(), new BlockAbility(new UntapItAfterItBattlesEffect()));
         }
     }
 }
---
+++ Cards/Cards/DM05/LaByleSeekerOfTheWinds.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class LaByleSeekerOfTheWinds : Creature
+    {
+        public LaByleSeekerOfTheWinds() : base("La Byle, Seeker of the Winds", 7, 5000, Subtype.MechaThunder, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.BlockerAbility(), new TriggeredAbilities.BlockAbility(new OneShotEffects.UntapItAfterItBattlesEffect()));
+        }
+    }
+}
