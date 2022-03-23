3976befacbbe7c48d1982693eb3449cb8ee6293a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 19:33:58 +0200

    Smash Horn Q

---
+++ Cards/Cards/DM05/SmashHornQ.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class SmashHornQ : Creature
+    {
+        public SmashHornQ() : base("Smash Horn Q", 3, 2000, Civilization.Nature)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.HornedBeast);
+            AddAbilities(new StaticAbilities.SurvivorAbility(new StaticAbilities.ThisCreatureGetsPowerAbility(1000)));
+        }
+    }
+}
---
+++ Cards/StaticAbilities/ThisCreatureGetsPowerAbility.cs
@@ -0,0 +1,9 @@
+﻿namespace Cards.StaticAbilities
+{
+    class ThisCreatureGetsPowerAbility : Engine.Abilities.StaticAbility
+    {
+        public ThisCreatureGetsPowerAbility(int power) : base(new Engine.ContinuousEffects.PowerModifyingEffect(power))
+        {
+        }
+    }
+}
