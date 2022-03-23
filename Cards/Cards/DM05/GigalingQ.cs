fb8c4b981d445168de27e6294c27c236c9a1e419
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-23 17:39:24 +0200

    Gigaling Q

---
+++ Cards/Cards/DM05/GigalingQ.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM05
+{
+    class GigalingQ : Creature
+    {
+        public GigalingQ() : base("Gigaling Q", 5, 2000, Civilization.Darkness)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.Chimera);
+            AddAbilities(new StaticAbilities.SurvivorAbility(new StaticAbilities.SlayerAbility()));
+        }
+    }
+}
