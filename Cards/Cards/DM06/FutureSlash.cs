1ec76c17e324b0916686c792194ee6e1b65cb14d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 19:38:49 +0200

    Future Slash

---
+++ Cards/Cards/DM06/FutureSlash.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM06
+{
+    class FutureSlash : Spell
+    {
+        public FutureSlash() : base("Future Slash", 7, Civilization.Darkness)
+        {
+            AddSpellAbilities(new FutureSlashEffect());
+        }
+    }
+
+    class FutureSlashEffect : OneShotEffects.SearchDeckEffect
+    {
+        public FutureSlashEffect() : base(new CardFilters.OpponentsDeckCardFilter(), 2, true)
+        {
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new FutureSlashEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Search your opponent's deck. Take up to 2 cards from his deck and put them into his graveyard. Then your opponent shuffles his deck.";
+        }
+
+        protected override void Apply(IGame game, IAbility source, params Engine.ICard[] cards)
+        {
+            game.Move(ZoneType.Deck, ZoneType.Graveyard, cards);
+        }
+    }
+}
--- Cards/OneShotEffects/SearchDeckEffect.cs
+++ Cards/OneShotEffects/SearchDeckEffect.cs
@@ -8,31 +8,35 @@ namespace Cards.OneShotEffects
     abstract class SearchDeckEffect : OneShotEffect
     {
         private readonly int _maximum;
+        private readonly bool _searchOpponentsDeck;
 
         public ICardFilter Filter { get; }
 
-        protected SearchDeckEffect(CardFilter filter, int maximum = 1)
+        protected SearchDeckEffect(CardFilter filter, int maximum = 1, bool searchOpponentsDeck = false)
         {
             Filter = filter;
             _maximum = maximum;
+            _searchOpponentsDeck = searchOpponentsDeck;
         }
 
         protected SearchDeckEffect(SearchDeckEffect effect)
         {
             Filter = effect.Filter.Copy();
             _maximum = effect._maximum;
+            _searchOpponentsDeck = effect._searchOpponentsDeck;
         }
 
         public override object Apply(IGame game, IAbility source)
         {
             var player = game.GetPlayer(source.Owner);
+            var deckOwner = _searchOpponentsDeck ? game.GetOpponent(player) : player;
             var cards = game.GetAllCards().Where(x => Filter.Applies(x, game, player));
             if (cards.Any())
             {
                 var selectedCards = player.Choose(new BoundedCardSelectionInEffect(player.Id, cards, 0, _maximum, ToString()), game).Decision.Select(x => game.GetCard(x));
                 Apply(game, source, selectedCards.ToArray());
             }
-            player.ShuffleDeck(game);
+            deckOwner.ShuffleDeck(game);
             return cards.Any();
         }
 
