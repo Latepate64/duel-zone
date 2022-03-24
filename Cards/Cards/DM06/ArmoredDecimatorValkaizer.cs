53d954d4f76c8733aff4dfc8863c96e99b955ea3
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 17:26:33 +0200

    Armored Decimator Valkaizer

---
+++ Cards/Cards/DM06/ArmoredDecimatorValkaizer.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class ArmoredDecimatorValkaizer : EvolutionCreature
+    {
+        public ArmoredDecimatorValkaizer() : base("Armored Decimator Valkaizer", 5, 5000, Subtype.Human, Civilization.Fire)
+        {
+            AddAbilities(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.YouMayDestroyOneOfYourOpponentsCreaturesThatHasMaxPowerEffect(4000)));
+        }
+    }
+}
