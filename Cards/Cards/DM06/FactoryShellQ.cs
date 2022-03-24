ada45ab564dd500509e5f645eef79dca57c7818f
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 17:52:56 +0200

    Factory Shell Q

---
+++ Cards/Cards/DM06/FactoryShellQ.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class FactoryShellQ : Creature
+    {
+        public FactoryShellQ() : base("Factory Shell Q", 6, 2000, Civilization.Nature)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.ColonyBeetle);
+            AddAbilities(new StaticAbilities.SurvivorAbility(new TriggeredAbilities.PutIntoPlayAbility(new OneShotEffects.TutoringEffect(new CardFilters.OwnersDeckSubtypeCreatureFilter(Subtype.Survivor), true))));
+        }
+    }
+}
