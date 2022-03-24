8c80cfe47c42ed932da73b6562c55dae15817a6d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 20:02:27 +0200

    Illusory Berry

---
+++ Cards/Cards/DM06/IllusoryBerry.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class IllusoryBerry : Creature
+    {
+        public IllusoryBerry() : base("Illusory Berry", 3, 3000, Subtype.TreeFolk, Civilization.Nature)
+        {
+        }
+    }
+}
