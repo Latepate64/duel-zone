a9bc1afb0f4573a4563762b8cccf5c4843569745
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 17:44:01 +0200

    Crystal Jouster

---
+++ Cards/Cards/DM06/CrystalJouster.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class CrystalJouster : EvolutionCreature
+    {
+        public CrystalJouster() : base("Crystal Jouster", 7, 7000, Subtype.LiquidPeople, Civilization.Water)
+        {
+            AddAbilities(new StaticAbilities.DoubleBreakerAbility(), new StaticAbilities.WhenThisCreatureWouldBeDestroyedReturnItToYourHandInsteadAbility());
+        }
+    }
+}
