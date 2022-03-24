86fdbbdb15d0c4b14b8741a404579d01332b4578
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 19:54:21 +0200

    Skullcutter, Swarm Leader

---
+++ Cards/Cards/DM06/SkullcutterSwarmLeader.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class SkullcutterSwarmLeader : Creature
+    {
+        public SkullcutterSwarmLeader() : base("Skullcutter, Swarm Leader", 4, 4000, Subtype.DevilMask, Civilization.Darkness)
+        {
+            AddAbilities(new TriggeredAbilities.AtTheEndOfYourTurnIfThisIsYourOnlyCreatureInTheBattleZoneDestroyItAbility());
+        }
+    }
+}
