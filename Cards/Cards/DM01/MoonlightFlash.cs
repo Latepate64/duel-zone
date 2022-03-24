ba86aecbd05af2f46b33c7e1d316553f9b5a7f79
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 17:41:57 +0200

    Craze Valkyrie, the Drastic

--- Cards/Cards/DM01/MoonlightFlash.cs
+++ Cards/Cards/DM01/MoonlightFlash.cs
@@ -6,8 +6,7 @@ namespace Cards.Cards.DM01
     {
         public MoonlightFlash() : base("Moonlight Flash", 4, Common.Civilization.Light)
         {
-            // Choose up to 2 of your opponent's creatures in the battle zone and tap them.
-            AddSpellAbilities(new TapChoiceEffect(0, 2, true));
+            AddSpellAbilities(new ChooseUpTo2OfYourOpponentsCreaturesInTheBattleZoneAndTapThemEffect());
         }
     }
 }
---
+++ Cards/OneShotEffects/ChooseUpTo2OfYourOpponentsCreaturesInTheBattleZoneAndTapThemEffect.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class ChooseUpTo2OfYourOpponentsCreaturesInTheBattleZoneAndTapThemEffect : TapChoiceEffect
+    {
+        public ChooseUpTo2OfYourOpponentsCreaturesInTheBattleZoneAndTapThemEffect() : base(0, 2, true)
+        {
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new ChooseUpTo2OfYourOpponentsCreaturesInTheBattleZoneAndTapThemEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Choose up to 2 of your opponent's creatures in the battle zone and tap them.";
+        }
+    }
+}
