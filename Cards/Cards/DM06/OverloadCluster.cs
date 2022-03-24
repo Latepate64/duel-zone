977f5feb3391b1fb6cc789d2ed8d97121dfefcfc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 16:48:20 +0200

    Overload Cluster

---
+++ Cards/Cards/DM06/OverloadCluster.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class OverloadCluster : Creature
+    {
+        public OverloadCluster() : base("Overload Cluster", 5, 4000, Subtype.CyberCluster, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.OpponentSummonOrCastAbility(new OneShotEffects.ThisCreatureGetsBlockerUntilTheEndOfTheTurnEffect()));
+        }
+    }
+}
