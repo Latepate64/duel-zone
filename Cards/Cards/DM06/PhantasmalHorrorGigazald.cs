01c59db17fb287bad3124c1b406eab5cc14fd94b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-24 17:19:42 +0200

    Phantasmal Horror Gigazald

---
+++ Cards/Cards/DM06/PhantasmalHorrorGigazald.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class PhantasmalHorrorGigazald : Creature
+    {
+        public PhantasmalHorrorGigazald() : base("Phantasmal Horror Gigazald", 5, 5000, Subtype.Chimera, Civilization.Darkness)
+        {
+            AddAbilities(new StaticAbilities.TapAbilityAddingAbility(Civilization.Darkness, new OneShotEffects.OpponentRandomDiscardEffect()));
+        }
+    }
+}
