1c1d3ee189c2587b236c0ba162f9e3d0eeb38878
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-25 19:00:57 +0200

    Moved RaceEvolutionEffect from Engine to Cards

--- Engine/ContinuousEffects/EvolutionEffect.cs
+++ Engine/ContinuousEffects/EvolutionEffect.cs
@@ -14,39 +14,4 @@ namespace Engine.ContinuousEffects
 
         public abstract bool CanEvolveFrom(ICard card);
     }
-
-    public class RaceEvolutionEffect : EvolutionEffect
-    {
-        public RaceEvolutionEffect(RaceEvolutionEffect effect) : base(effect)
-        {
-            Race = effect.Race;
-        }
-
-        public RaceEvolutionEffect(ICardFilter filter, Subtype race) : base(filter)
-        {
-            Race = race;
-        }
-
-        public Subtype Race { get; }
-
-        public override bool CanEvolveFrom(ICard card)
-        {
-            return card.Subtypes.Contains(Race);
-        }
-
-        public override ContinuousEffect Copy()
-        {
-            return new RaceEvolutionEffect(this);
-        }
-
-        public override string ToString()
-        {
-            return $"Evolution - Put on one of your {SplitByCase(Race.ToString())}s.";
-        }
-
-        private static string SplitByCase(string text)
-        {
-            return string.Join(' ', System.Text.RegularExpressions.Regex.Split(text, @"(?<!^)(?=[A-Z])"));
-        }
-    } 
 }
