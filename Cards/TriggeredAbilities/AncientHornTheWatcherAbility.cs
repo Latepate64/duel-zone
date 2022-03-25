ba1010f8f824c35cf7f7bb8396cf044a83d52929
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-25 16:40:11 +0200

    UntapAllTheCardsInYourManaZoneEffect

--- Cards/TriggeredAbilities/AncientHornTheWatcherAbility.cs
+++ Cards/TriggeredAbilities/AncientHornTheWatcherAbility.cs
@@ -1,11 +1,12 @@
-﻿using Engine;
+﻿using Cards.OneShotEffects;
+using Engine;
 using Engine.Abilities;
 
 namespace Cards.TriggeredAbilities
 {
     public class AncientHornTheWatcherAbility : WhenThisCreatureIsPutIntoTheBattleZoneAbility
     {
-        public AncientHornTheWatcherAbility() : base(new OneShotEffects.UntapAreaOfEffect(new CardFilters.OwnersManaZoneCardFilter()))
+        public AncientHornTheWatcherAbility() : base(new UntapAllTheCardsInYourManaZoneEffect())
         {
         }
 
@@ -22,6 +23,28 @@ namespace Cards.TriggeredAbilities
         {
             // if you have 5 or more shields
             return game.GetPlayer(Owner)?.ShieldZone.Cards.Count >= 5;
+        }
+
+        public override string ToString()
+        {
+            return $"When you put this creature into the battle zone, if you have 5 or more shields, {GetEffectText()}";
+        }
+    }
+
+    class UntapAllTheCardsInYourManaZoneEffect : UntapAreaOfEffect
+    {
+        public UntapAllTheCardsInYourManaZoneEffect() : base(new CardFilters.OwnersManaZoneCardFilter())
+        {
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new UntapAllTheCardsInYourManaZoneEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Untap all the cards in your mana zone.";
         }
     }
 }
