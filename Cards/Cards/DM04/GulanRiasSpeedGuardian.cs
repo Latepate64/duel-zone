eb6b56c904a5fac3a05d866c674809ee0c2baaae
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-25 16:24:33 +0200

    ThisCreatureCannotBeBlockedByCivilizationCreaturesEffect

--- Cards/Cards/DM04/GulanRiasSpeedGuardian.cs
+++ Cards/Cards/DM04/GulanRiasSpeedGuardian.cs
@@ -6,7 +6,7 @@ namespace Cards.Cards.DM04
     {
         public GulanRiasSpeedGuardian() : base("Gulan Rias, Speed Guardian", 3, 2000, Subtype.Guardian, Civilization.Light)
         {
-            AddAbilities(new StaticAbilities.ThisCreatureCannotBeAttackedByCivilizationCreaturesAbility(Civilization.Darkness), new StaticAbilities.UnblockableAbility(new CardFilters.OpponentsBattleZoneCivilizationCreatureFilter(Civilization.Darkness)));
+            AddAbilities(new StaticAbilities.ThisCreatureCannotBeAttackedByCivilizationCreaturesAbility(Civilization.Darkness), new StaticAbilities.ThisCreatureCannotBeBlockedByCivilizationCreaturesAbility(Civilization.Darkness));
         }
     }
 }
