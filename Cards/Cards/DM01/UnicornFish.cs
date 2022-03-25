7e17e8e4e5b5611fb85d49d9dd0fa588e60e0242
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-25 15:13:51 +0200

    ChooseCreatureInTheBattleZoneAndReturnItToItsOwnersHandEffect

--- Cards/Cards/DM01/UnicornFish.cs
+++ Cards/Cards/DM01/UnicornFish.cs
@@ -7,8 +7,7 @@ namespace Cards.Cards.DM01
     {
         public UnicornFish() : base("Unicorn Fish", 4, 1000, Common.Subtype.Fish, Common.Civilization.Water)
         {
-            // When you put this creature into the battle zone, you may choose 1 creature in the battle zone and return it to its owner's hand.
-            AddAbilities(new PutIntoPlayAbility(new BounceEffect(0, 1, new CardFilters.BattleZoneChoosableCreatureFilter())));
+            AddAbilities(new PutIntoPlayAbility(new YouMayChooseCreatureInTheBattleZoneAndReturnItToItsOwnersHandEffect()));
         }
     }
 }
---
+++ Cards/OneShotEffects/ChooseCreatureInTheBattleZoneAndReturnItToItsOwnersHandEffect.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class ChooseCreatureInTheBattleZoneAndReturnItToItsOwnersHandEffect : BounceEffect
+    {
+        public ChooseCreatureInTheBattleZoneAndReturnItToItsOwnersHandEffect() : base(1, 1, new CardFilters.BattleZoneChoosableCreatureFilter())
+        {
+        }
+
+        public override OneShotEffect Copy()
+        {
+            return new ChooseCreatureInTheBattleZoneAndReturnItToItsOwnersHandEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Choose a creature in the battle zone and return it to its owner's hand.";
+        }
+    }
+}
