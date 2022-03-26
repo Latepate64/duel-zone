27b3614203819f4df84be775ea0352850275739c
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-26 12:55:45 +0200

    ManaFeedEffect is now abstract

--- Cards/Cards/DM01/PangaeasSong.cs
+++ Cards/Cards/DM01/PangaeasSong.cs
@@ -1,14 +1,10 @@
-﻿using Cards.CardFilters;
-using Cards.OneShotEffects;
-
-namespace Cards.Cards.DM01
+﻿namespace Cards.Cards.DM01
 {
     class PangaeasSong : Spell
     {
         public PangaeasSong() : base("Pangaea's Song", 1, Common.Civilization.Nature)
         {
-            // Put 1 of your creatures from the battle zone into your mana zone.
-            AddSpellAbilities(new ManaFeedEffect(new OwnersBattleZoneCreatureFilter(), 1, 1, true));
+            AddSpellAbilities(new OneShotEffects.PutOneOfYourCreaturesFromTheBattleZoneIntoYourManaZoneEffect());
         }
     }
 }
