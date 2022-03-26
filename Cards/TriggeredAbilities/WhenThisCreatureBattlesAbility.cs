7bbbcbedc6b516e738de8a9f8d9c38cbc1f82769
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-26 19:01:17 +0200

    WhenThisCreatureBattlesAbility

---
+++ Cards/TriggeredAbilities/WhenThisCreatureBattlesAbility.cs
@@ -0,0 +1,32 @@
+﻿using Common.GameEvents;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.TriggeredAbilities
+{
+    public class WhenThisCreatureBattlesAbility : CardTriggeredAbility
+    {
+        public WhenThisCreatureBattlesAbility(IOneShotEffect effect) : base(effect)
+        {
+        }
+
+        public WhenThisCreatureBattlesAbility(WhenThisCreatureBattlesAbility ability) : base(ability)
+        {
+        }
+
+        public override bool CanTrigger(IGameEvent gameEvent, IGame game)
+        {
+            return base.CanTrigger(gameEvent, game) && gameEvent is BattleEvent;
+        }
+
+        public override IAbility Copy()
+        {
+            return new WhenThisCreatureBattlesAbility(this);
+        }
+
+        public override string ToString()
+        {
+            return $"When this creature battles, {GetEffectText()}";
+        }
+    }
+}
