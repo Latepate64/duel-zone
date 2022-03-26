42b93564419fb9cf50fca13389e5b61d0a67f378
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-26 19:40:56 +0200

    Invincible Abyss

---
+++ Cards/Cards/DM06/InvincibleAbyss.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM06
+{
+    class InvincibleAbyss : Spell
+    {
+        public InvincibleAbyss() : base("Invincible Abyss", 13, Civilization.Darkness)
+        {
+            AddSpellAbilities(new InvincibleAbyssEffect());
+        }
+    }
+
+    class InvincibleAbyssEffect : OneShotEffects.DestroyAreaOfEffect
+    {
+        public InvincibleAbyssEffect() : base(new CardFilters.OpponentsBattleZoneCreatureFilter())
+        {
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new InvincibleAbyssEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Destroy all your opponent's creatures.";
+        }
+    }
+}
