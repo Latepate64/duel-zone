8fdf12e7af4350f161c8463fcaff82c42ad20508
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:14:24 +0300

    Phantasmal Horror Gigazabal

---
+++ Cards/Cards/DM07/PhantasmalHorrorGigazabal.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class PhantasmalHorrorGigazabal : EvolutionCreature
+    {
+        public PhantasmalHorrorGigazabal() : base("Phantasmal Horror Gigazabal", 5, 9000, Subtype.Chimera, Civilization.Darkness)
+        {
+            AddAbilities(new StaticAbilities.StealthAbility(Civilization.Light), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
