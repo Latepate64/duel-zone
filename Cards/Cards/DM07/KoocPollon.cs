c0fbf553a1671c4f26b1f99bcbc45e018b85fcd9
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:05:42 +0300

    Kooc Pollon

---
+++ Cards/Cards/DM07/KoocPollon.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class KoocPollon : Creature
+    {
+        public KoocPollon() : base("Kooc Pollon", 2, 1000, Subtype.FireBird, Civilization.Fire)
+        {
+            AddAbilities(new StaticAbilities.ThisCreatureCannotBeAttackedAbility());
+        }
+    }
+}
---
+++ Cards/StaticAbilities/ThisCreatureCannotBeAttackedAbility.cs
@@ -0,0 +1,29 @@
+﻿using Engine;
+using Engine.ContinuousEffects;
+
+namespace Cards.StaticAbilities
+{
+    class ThisCreatureCannotBeAttackedAbility : Engine.Abilities.StaticAbility
+    {
+        public ThisCreatureCannotBeAttackedAbility() : base(new ThisCreatureCannotBeAttackedEffect())
+        {
+        }
+    }
+
+    class ThisCreatureCannotBeAttackedEffect : CannotBeAttackedEffect
+    {
+        public ThisCreatureCannotBeAttackedEffect() : base(new TargetFilter(), new CardFilters.BattleZoneCreatureFilter(), new Durations.Indefinite())
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new ThisCreatureCannotBeAttackedEffect();
+        }
+
+        public override string ToString()
+        {
+            return "This creature can't be attacked.";
+        }
+    }
+}
