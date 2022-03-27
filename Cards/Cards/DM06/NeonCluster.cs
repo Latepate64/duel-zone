ab38b0b0e99fac13c36f84a6a426cde670a69bcc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 09:25:26 +0300

    Neon Cluster

---
+++ Cards/Cards/DM06/NeonCluster.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class NeonCluster : Creature
+    {
+        public NeonCluster() : base("Neon Cluster", 7, 4000, Subtype.CyberCluster, Civilization.Water)
+        {
+            AddAbilities(new Engine.Abilities.TapAbility(new OneShotEffects.DrawCardsEffect(2)));
+        }
+    }
+}
