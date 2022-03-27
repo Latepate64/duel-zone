c57043da6c75c41fa7d54e228178c343bd5a7ee6
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:21:31 +0300

    Rom, Vizier of Tendrils

---
+++ Cards/Cards/DM07/RomVizierOfTendrils.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class RomVizierOfTendrils : Creature
+    {
+        public RomVizierOfTendrils() : base("Rom, Vizier of Tendrils", 4, 2000, Subtype.Initiate, Civilization.Light)
+        {
+            AddAbilities(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.YouMayChooseOneOfYourOpponentsCreaturesAndTapItEffect()));
+        }
+    }
+}
