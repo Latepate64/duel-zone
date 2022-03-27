35fa50731eeced9c469e164cf8515fe832a561e8
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 08:59:25 +0300

    Venom Charger

---
+++ Cards/Cards/DM07/VenomCharger.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class VenomCharger : Charger
+    {
+        public VenomCharger() : base("Venom Charger", 3, Civilization.Darkness)
+        {
+            AddSpellAbilities(new OneShotEffects.OneOfYourCreaturesGetsAbilityUntilTheEndOfTheTurnEffect(new StaticAbilities.SlayerAbility()));
+        }
+    }
+}
