eac1037914f7de6a785c2e6b66d3feb5cb38d60b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 17:16:29 +0300

    Cratersaur

---
+++ Cards/Cards/DM07/Cratersaur.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class Cratersaur : Creature
+    {
+        public Cratersaur() : base("Cratersaur", 3, 2000, Subtype.RockBeast, Civilization.Fire)
+        {
+            AddAbilities(new StaticAbilities.WhileYouHaveNoShieldsAbility(new StaticAbilities.ThisCreatureCanAttackUntappedCreaturesAbility(), new StaticAbilities.PowerAttackerAbility(3000)));
+        }
+    }
+}
