28b8bcb62f3ee40e498d09ab13b39523a8bd556b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 16:28:59 +0300

    Ultra Mantis, Scourge of Fate

---
+++ Cards/Cards/DM06/UltraMantisScourgeOfFate.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class UltraMantisScourgeOfFate : EvolutionCreature
+    {
+        public UltraMantisScourgeOfFate() : base("Ultra Mantis, Scourge of Fate", 6, 9000, Subtype.GiantInsect, Civilization.Nature)
+        {
+            AddAbilities(new StaticAbilities.ThisCreatureCannotBeBlockedByAnyCreatureThatHasMaxPowerAbility(8000), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
