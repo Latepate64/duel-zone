7317e6bb81cce2146f4c95759de0918d21c50fda
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 17:20:24 +0300

    Siri, Glory Elemental

---
+++ Cards/Cards/DM07/SiriGloryElemental.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class SiriGloryElemental : Creature
+    {
+        public SiriGloryElemental() : base("Siri, Glory Elemental", 6, 7000, Subtype.AngelCommand, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.DoubleBreakerAbility(), new StaticAbilities.WhileYouHaveNoShieldsAbility(new StaticAbilities.BlockerAbility(), new TriggeredAbilities.AtTheEndOfYourTurnAbility(new OneShotEffects.YouMayUntapThisCreatureEffect())));
+        }
+    }
+}
