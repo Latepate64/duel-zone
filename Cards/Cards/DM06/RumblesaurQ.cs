0ac1c7f12f8bccf6e8eae17b043de65cbe39613d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 13:58:44 +0300

    Rumblesaur Q

---
+++ Cards/Cards/DM06/RumblesaurQ.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class RumblesaurQ : Creature
+    {
+        public RumblesaurQ() : base("Rumblesaur Q", 6, 3000, Civilization.Fire)
+        {
+            AddAbilities(new StaticAbilities.SurvivorAbility(new StaticAbilities.SpeedAttackerAbility()));
+        }
+    }
+}
