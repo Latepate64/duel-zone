81ce744de6ce8c072f9b6944fd3d890caedd503d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 14:44:38 +0300

    Tank Mutant

---
+++ Cards/Cards/DM06/TankMutant.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class TankMutant : Creature
+    {
+        public TankMutant() : base("Tank Mutant", 9, 6000, Subtype.Hedrian, Civilization.Darkness)
+        {
+            AddAbilities(new Engine.Abilities.TapAbility(new OneShotEffects.OpponentSacrificeEffect()));
+        }
+    }
+}
