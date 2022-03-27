708be5fe849fd16c308c154fe9df97ab81e889bc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:12:48 +0300

    Otherworldly Warrior Naglu

---
+++ Cards/Cards/DM07/OtherworldlyWarriorNaglu.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class OtherworldlyWarriorNaglu : Creature
+    {
+        public OtherworldlyWarriorNaglu() : base("Otherworldly Warrior Naglu", 6, 4000, Subtype.Armorloid, Civilization.Fire)
+        {
+            AddAbilities(new StaticAbilities.ThisCreatureCannotBeAttackedAbility(), new StaticAbilities.PowerAttackerAbility(3000), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
