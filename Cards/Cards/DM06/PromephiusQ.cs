b835aa2f7054d99cc2c12872618aa811dcaacfa9
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 09:31:35 +0300

    Promephius Q

---
+++ Cards/Cards/DM06/PromephiusQ.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class PromephiusQ : Creature
+    {
+        public PromephiusQ() : base("Promephius Q", 3, 2000, Civilization.Water)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.SeaHacker);
+        }
+    }
+}
