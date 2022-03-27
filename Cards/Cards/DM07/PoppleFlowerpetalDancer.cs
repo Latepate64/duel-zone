6ecb3bee4763bae880ebb6a105e414ca33779289
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:15:40 +0300

    Popple, Flowerpetal Dancer

---
+++ Cards/Cards/DM07/PoppleFlowerpetalDancer.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class PoppleFlowerpetalDancer : Creature
+    {
+        public PoppleFlowerpetalDancer() : base("Popple, Flowerpetal Dancer", 4, 2000, Subtype.SnowFaerie, Civilization.Nature)
+        {
+            AddAbilities(new Engine.Abilities.TapAbility(new OneShotEffects.PutTopCardsOfDeckIntoManaZoneEffect(1)));
+        }
+    }
+}
