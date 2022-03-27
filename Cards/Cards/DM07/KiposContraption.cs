f42e44c7e87ade3e74ac42bb99df456cba60f6c7
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 17:59:28 +0300

    Kipo's Contraption

---
+++ Cards/Cards/DM07/KiposContraption.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class KiposContraption : Creature
+    {
+        public KiposContraption() : base("Kipo's Contraption", 6, 3000, Subtype.Xenoparts, Civilization.Fire)
+        {
+            AddAbilities(new Engine.Abilities.TapAbility(new OneShotEffects.DestroyOnefYourOpponentsCreaturesThatHasMaxPowerEffect(2000)));
+        }
+    }
+}
