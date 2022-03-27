933dc599f290c82c92385578bc749f5f1278ff69
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:09:38 +0300

    Lightning Charger

---
+++ Cards/Cards/DM07/LightningCharger.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class LightningCharger : Charger
+    {
+        public LightningCharger() : base("Lightning Charger", 4, Civilization.Light)
+        {
+            AddSpellAbilities(new OneShotEffects.ChooseOneOfYourOpponentsCreaturesInTheBattleZoneAndTapItEffect());
+        }
+    }
+}
