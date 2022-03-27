7be021232939dabf4a80e8ec4378e7421fc080eb
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 17:14:00 +0300

    Bex, the Oracle

---
+++ Cards/Cards/DM07/BexTheOracle.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class BexTheOracle : Creature
+    {
+        public BexTheOracle() : base("Bex, the Oracle", 3, 2500, Subtype.LightBringer, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.WhileYouHaveNoShieldsAbility(new StaticAbilities.BlockerAbility()));
+        }
+    }
+}
