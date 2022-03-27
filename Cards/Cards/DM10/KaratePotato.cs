b585a643484cab3d7dd504b53d62d9c2b4d8622f
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:52:38 +0300

    Karate Potato

---
+++ Cards/Cards/DM10/KaratePotato.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class KaratePotato : Creature
+    {
+        public KaratePotato() : base("Karate Potato", 4, 1000, Subtype.WildVeggies, Civilization.Nature)
+        {
+            ShieldTrigger = true;
+            AddAbilities(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.YouMayPutUpToCardsFromYourHandIntoYourManaZoneEffect(2)));
+        }
+    }
+}
