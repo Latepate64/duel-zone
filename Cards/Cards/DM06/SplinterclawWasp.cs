da39a0722d1a478e9e7bab2bb7371b64f426187a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 14:37:40 +0300

    Splinterclaw Wasp

---
+++ Cards/Cards/DM06/SplinterclawWasp.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class SplinterclawWasp : Creature
+    {
+        public SplinterclawWasp() : base("Splinterclaw Wasp", 7, 4000, Subtype.GiantInsect, Civilization.Nature)
+        {
+            AddAbilities(new StaticAbilities.PowerAttackerAbility(3000), new StaticAbilities.DoubleBreakerAbility(), new TriggeredAbilities.WheneverThisCreatureBecomesBlockedAbility(new OneShotEffects.ItBreaksOneOfYourOpponentsShieldsEffect()));
+        }
+    }
+}
