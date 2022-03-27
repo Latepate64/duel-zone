6ca5258559078d6cf0f1a21cb46609d919b2f000
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:25:59 +0300

    Rondobil, the Explorer

---
+++ Cards/Cards/DM07/RondobilTheExplorer.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM07
+{
+    class RondobilTheExplorer : Creature
+    {
+        public RondobilTheExplorer() : base("Rondobil, the Explorer", 6, 5000, Subtype.Gladiator, Civilization.Light)
+        {
+            AddAbilities(new TapAbility(new RondobilTheExplorerEffect()));
+        }
+    }
+
+    class RondobilTheExplorerEffect : OneShotEffects.CardMovingChoiceEffect
+    {
+        public RondobilTheExplorerEffect() : base(new CardFilters.OwnersBattleZoneCreatureFilter(), 1, 1, true, ZoneType.BattleZone, ZoneType.ShieldZone)
+        {
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new RondobilTheExplorerEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Add one of your creatures from the battle zone to your shields face down.";
+        }
+    }
+}
