4349f69e2c46073db85eb1b6885c7315bf7cd926
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 17:51:06 +0300

    Gezary, Undercover Doll

---
+++ Cards/Cards/DM07/GezaryUndercoverDoll.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class GezaryUndercoverDoll : Creature
+    {
+        public GezaryUndercoverDoll() : base("Gezary, Undercover Doll", 3, 2000, Subtype.DeathPuppet, Civilization.Darkness)
+        {
+            AddAbilities(new StaticAbilities.StealthAbility(Civilization.Nature));
+        }
+    }
+}
