b363928293227e409670011a4aaee1ffae612c99
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 09:58:34 +0300

    Q-tronic Hypermind

---
+++ Cards/Cards/DM06/QTronicHypermind.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM06
+{
+    class QTronicHypermind : EvolutionCreature
+    {
+        public QTronicHypermind() : base("Q-tronic Hypermind", 8, 8000, Subtype.Survivor, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new QTronicHypermindEffect()), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+
+    class QTronicHypermindEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            return new OneShotEffects.YouMayDrawCardsEffect(game.BattleZone.Creatures.Count(x => x.Subtypes.Contains(Subtype.Survivor)));
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new QTronicHypermindEffect();
+        }
+
+        public override string ToString()
+        {
+            return "You may draw a card for each Survivor in the battle zone.";
+        }
+    }
+}
--- Cards/StaticAbilities/CrewBreakerAbility.cs
+++ Cards/StaticAbilities/CrewBreakerAbility.cs
@@ -32,7 +32,7 @@ namespace Cards.StaticAbilities
         public override int GetAmount(Engine.IGame game)
         {
             var ability = game.GetAbility(SourceAbility);
-            return game.BattleZone.GetCreatures(ability.Owner).Where(x => x.Id != ability.Source && x.Subtypes.Contains(_subtype)).Count();
+            return game.BattleZone.GetCreatures(ability.Owner).Count(x => x.Id != ability.Source && x.Subtypes.Contains(_subtype));
         }
 
         public override string ToString()
