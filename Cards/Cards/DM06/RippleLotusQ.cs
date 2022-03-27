167fba5b0b31caee7341273541903be5b7ce4e9c
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 13:56:57 +0300

    Ripple Lotus Q

---
+++ Cards/Cards/DM06/RippleLotusQ.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class RippleLotusQ : Creature
+    {
+        public RippleLotusQ() : base("Ripple Lotus Q", 6, 2000, Civilization.Water)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.CyberVirus);
+            AddAbilities(new StaticAbilities.SurvivorAbility(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.YouMayChooseOneOfYourOpponentsCreaturesAndTapItEffect())));
+        }
+    }
+}
