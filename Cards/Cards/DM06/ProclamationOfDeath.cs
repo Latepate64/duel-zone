279b17172ca2618f5d1e50e5a98694e745e8bd2c
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 09:30:08 +0300

    Proclamation of Death

---
+++ Cards/Cards/DM06/ProclamationOfDeath.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class ProclamationOfDeath : Spell
+    {
+        public ProclamationOfDeath() : base("Proclamation of Death", 4, Civilization.Darkness)
+        {
+            ShieldTrigger = true;
+            AddSpellAbilities(new OneShotEffects.OpponentSacrificeEffect());
+        }
+    }
+}
