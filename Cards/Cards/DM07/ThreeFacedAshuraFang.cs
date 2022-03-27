82c6183cba8d7de048adaee59de6f70c57a0b407
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:54:38 +0300

    Three-Faced Ashura Fang

---
+++ Cards/Cards/DM07/ThreeFacedAshuraFang.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class ThreeFacedAshuraFang : Creature
+    {
+        public ThreeFacedAshuraFang() : base("Three-Faced Ashura Fang", 4, 4000, Subtype.DevilMask, Civilization.Darkness)
+        {
+            AddAbilities(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.ShieldRecoveryCannotUseShieldTriggerEffect()));
+        }
+    }
+}
