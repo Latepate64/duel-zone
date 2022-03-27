0db7006eb481d459a4f0d6f4eef309a55f76b19b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 14:29:21 +0300

    Sphere of Wonder

---
+++ Cards/Cards/DM06/SphereOfWonder.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM06
+{
+    class SphereOfWonder : Spell
+    {
+        public SphereOfWonder() : base("Sphere of Wonder", 4, Civilization.Light)
+        {
+            AddSpellAbilities(new SphereOfWonderEffect());
+        }
+    }
+
+    class SphereOfWonderEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            if (source.GetOpponent(game).ShieldZone.Cards.Count > source.GetController(game).ShieldZone.Cards.Count)
+            {
+                source.GetController(game).PutFromTopOfDeckIntoShieldZone(1, game);
+            }
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new SphereOfWonderEffect();
+        }
+
+        public override string ToString()
+        {
+            return "If your opponent has more shields than you do, add the top card of your deck to your shields face down.";
+        }
+    }
+}
