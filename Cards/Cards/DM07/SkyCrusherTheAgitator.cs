cc07734242855f5829df4f2b74d08960b6d4907a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:40:57 +0300

    Sky Crusher, the Agitator

---
+++ Cards/Cards/DM07/SkyCrusherTheAgitator.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class SkyCrusherTheAgitator : Creature
+    {
+        public SkyCrusherTheAgitator() : base("Sky Crusher, the Agitator", 7, 4000, Subtype.Dragonoid, Civilization.Fire)
+        {
+            AddAbilities(new Engine.Abilities.TapAbility(new OneShotEffects.MutualManaSacrificeEffect(1)));
+        }
+    }
+}
