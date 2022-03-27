d8b16320e1d807944df731ba18dfe13d9bf92ee6
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:45:00 +0300

    Stinger Horn, the Delver

---
+++ Cards/Cards/DM07/StingerHornTheDelver.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class StingerHornTheDelver : Creature
+    {
+        public StingerHornTheDelver() : base("Stinger Horn, the Delver", 4, 3000, Subtype.HornedBeast, Civilization.Nature)
+        {
+            AddAbilities(new StaticAbilities.PowerAttackerAbility(1000), new StaticAbilities.StealthAbility(Civilization.Water));
+        }
+    }
+}
