ba8194fff34d5935dd674f5831d73f1cf1febc91
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:01:05 +0300

    Kizar Basiku, the Outrageous

---
+++ Cards/Cards/DM07/KizarBasikuTheOutrageous.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class KizarBasikuTheOutrageous : EvolutionCreature
+    {
+        public KizarBasikuTheOutrageous() : base("Kizar Basiku, the Outrageous", 5, 8500, Subtype.Initiate, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.BlockerAbility(), new StaticAbilities.StealthAbility(Civilization.Fire), new StaticAbilities.DoubleBreakerAbility());
+        }
+    }
+}
