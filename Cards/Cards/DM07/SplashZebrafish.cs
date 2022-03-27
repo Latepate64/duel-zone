c8d0142d5053675d5441e153edbf0c7012593b5d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:43:27 +0300

    Splash Zebrafish

---
+++ Cards/Cards/DM07/SplashZebrafish.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class SplashZebrafish : Creature
+    {
+        public SplashZebrafish() : base("Splash Zebrafish", 4, 3000, Subtype.GelFish, Civilization.Water)
+        {
+            AddAbilities(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.ReturnCardFromYourManaZoneToYourHandEffect()), new StaticAbilities.ThisCreatureCannotBeBlockedAbility());
+        }
+    }
+}
