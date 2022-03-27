e921c979207b3a7b8e0a31b6ea53361a054d92ca
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:08:15 +0300

    Launch Locust

--- Cards/Cards/DM02/LeapingTornadoHorn.cs
+++ Cards/Cards/DM02/LeapingTornadoHorn.cs
@@ -1,6 +1,4 @@
 ﻿using Common;
-using Engine.Abilities;
-using Engine.ContinuousEffects;
 
 namespace Cards.Cards.DM02
 {
@@ -8,31 +6,7 @@ namespace Cards.Cards.DM02
     {
         public LeapingTornadoHorn() : base("Leaping Tornado Horn", 3, 2000, Subtype.HornedBeast, Civilization.Nature)
         {
-            AddAbilities(new LeapingTornadoHornAbility());
-        }
-    }
-
-    class LeapingTornadoHornAbility : StaticAbility
-    {
-        public LeapingTornadoHornAbility() : base(new LeapingTornadoHornEffect())
-        {
-        }
-    }
-
-    class LeapingTornadoHornEffect : ContinuousEffects.PowerAttackerMultiplierEffect
-    {
-        public LeapingTornadoHornEffect() : base(1000, new CardFilters.OwnersOtherBattleZoneCreatureFilter())
-        {
-        }
-
-        public override IContinuousEffect Copy()
-        {
-            return new LeapingTornadoHornEffect();
-        }
-
-        public override string ToString()
-        {
-            return "While attacking, this creature gets +1000 power for each other creature you have in the battle zone.";
+            AddAbilities(new StaticAbilities.LeapingTornadoHornAbility());
         }
     }
 }
---
+++ Cards/Cards/DM07/LaunchLocust.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class LaunchLocust : Creature
+    {
+        public LaunchLocust() : base("Launch Locust", 3, 2000, Subtype.HornedBeast, Civilization.Nature)
+        {
+            AddAbilities(new StaticAbilities.LeapingTornadoHornAbility());
+        }
+    }
+}
