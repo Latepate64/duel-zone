d879fa6f59380bf80480300f4df7212cbdfa2926
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 16:03:14 +0300

    Thrash Crawler

---
+++ Cards/Cards/DM06/ThrashCrawler.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class ThrashCrawler : Creature
+    {
+        public ThrashCrawler() : base("Thrash Crawler", 4, 5000, Subtype.EarthEater, Civilization.Water)
+        {
+            AddAbilities(new StaticAbilities.BlockerAbility(), new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.ReturnCardsFromYourManaZoneToYourHandEffect(1)), new StaticAbilities.ThisCreatureCannotAttackAbility());
+        }
+    }
+}
