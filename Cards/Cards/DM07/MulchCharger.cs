7f7b11e710de1491b155100ea3276be7b2fd6918
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:10:52 +0300

    Mulch Charger

---
+++ Cards/Cards/DM07/MulchCharger.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class MulchCharger : Charger
+    {
+        public MulchCharger() : base("Mulch Charger", 3, Civilization.Nature)
+        {
+            AddSpellAbilities(new OneShotEffects.PutOneOfYourCreaturesFromTheBattleZoneIntoYourManaZoneEffect());
+        }
+    }
+}
