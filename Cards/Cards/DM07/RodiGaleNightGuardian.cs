22c442d670cc0500ed40fd263ee113f434c906a5
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:19:47 +0300

    Rodi Gale, Night Guardian

---
+++ Cards/Cards/DM07/RodiGaleNightGuardian.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class RodiGaleNightGuardian : Creature
+    {
+        public RodiGaleNightGuardian() : base("Rodi Gale, Night Guardian", 4, 3500, Subtype.Guardian, Civilization.Light)
+        {
+            AddAbilities(new StaticAbilities.StealthAbility(Civilization.Darkness));
+        }
+    }
+}
