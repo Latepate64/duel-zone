1387ad974a9ec7b815c5e6ad1b317e2a61f01553
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 14:39:06 +0300

    Steam Star

---
+++ Cards/Cards/DM06/SteamStar.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace Cards.Cards.DM06
+{
+    class SteamStar : Creature
+    {
+        public SteamStar() : base("Steam Star", 2, 1000, Subtype.CyberVirus, Civilization.Water)
+        {
+        }
+    }
+}
