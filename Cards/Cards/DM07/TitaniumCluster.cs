0989c3c92dbaea98c8629285ceec762160fb9839
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-27 18:56:49 +0300

    Titanium Cluster

---
+++ Cards/Cards/DM07/TitaniumCluster.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class TitaniumCluster : Creature
+    {
+        public TitaniumCluster() : base("Titanium Cluster", 4, 4000, Subtype.CyberCluster, Civilization.Water)
+        {
+            AddAbilities(new StaticAbilities.BlockerAbility(), new StaticAbilities.ThisCreatureCannotBeAttackedAbility(), new StaticAbilities.ThisCreatureCannotAttackAbility());
+        }
+    }
+}
