7ea3f1053a36b153bb691447cd1699ec3e547372
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-28 18:51:32 +0300

    Valkrowzer, Ultra Rock Beast

---
+++ Cards/Cards/DM07/ValkrowzerUltraRockBeast.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class ValkrowzerUltraRockBeast : EvolutionCreature
+    {
+        public ValkrowzerUltraRockBeast() : base("Valkrowzer, Ultra Rock Beast", 6, 9000, Subtype.RockBeast, Civilization.Fire)
+        {
+            AddStaticAbilities(new ContinuousEffects.StealthEffect(Civilization.Water));
+            AddDoubleBreakerAbility();
+        }
+    }
+}
