e7f21b02eb3d718bdd07f2fd159a82841dde4d63
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-28 18:54:48 +0300

    World Tree, Root of Life

---
+++ Cards/Cards/DM07/WorldTreeRootOfLife.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM07
+{
+    class WorldTreeRootOfLife : EvolutionCreature
+    {
+        public WorldTreeRootOfLife() : base("World Tree, Root of Life", 6, 7000, Subtype.TreeFolk, Civilization.Nature)
+        {
+            AddPowerAttackerAbility(2000);
+            AddStaticAbilities(new ContinuousEffects.StealthEffect(Civilization.Darkness));
+            AddDoubleBreakerAbility();
+        }
+    }
+}
