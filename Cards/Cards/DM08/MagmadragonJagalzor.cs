0349a8e07b2f5bb47a664c33e5920ad1482e4588
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-29 19:48:48 +0300

    MagmadragonJagalzorEffect

---
+++ Cards/Cards/DM08/MagmadragonJagalzor.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+using Engine.ContinuousEffects;
+
+namespace Cards.Cards.DM08
+{
+    class MagmadragonJagalzor : TurboRushCreature
+    {
+        public MagmadragonJagalzor() : base("Magmadragon Jagalzor", 6, 6000, Subtype.VolcanoDragon, Civilization.Fire)
+        {
+            AddDoubleBreakerAbility();
+            AddTurboRushAbility(new MagmadragonJagalzorEffect());
+        }
+    }
+
+    class MagmadragonJagalzorEffect : SpeedAttackerEffect
+    {
+        public MagmadragonJagalzorEffect() : base(new CardFilters.OwnersBattleZoneCreatureFilter(), new Durations.Indefinite())
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new MagmadragonJagalzorEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Each of your creatures in the battle zone has \"speed attacker.\"";
+        }
+    }
+}
