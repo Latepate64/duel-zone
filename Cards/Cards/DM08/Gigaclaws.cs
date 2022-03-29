f5035285445ba64ff4541f1cdfdbf17a09c642a2
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-29 19:23:34 +0300

    Gigaclaws

---
+++ Cards/Cards/DM08/Gigaclaws.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM08
+{
+    class Gigaclaws : Creature
+    {
+        public Gigaclaws() : base("Gigaclaws", 5, 2000, Subtype.Chimera, Civilization.Darkness)
+        {
+            AddTurboRushAbility(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new OneShotEffects.YourOpponentDiscardsHisHandEffect()));
+        }
+    }
+}
