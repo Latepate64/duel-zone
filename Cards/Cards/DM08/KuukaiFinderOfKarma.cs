514cca41e02449b10fd3db2578a82414da3d2cc3
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-29 19:38:04 +0300

    Kuukai, Finder of Karma

---
+++ Cards/Cards/DM08/KuukaiFinderOfKarma.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM08
+{
+    class KuukaiFinderOfKarma : EvolutionCreature
+    {
+        public KuukaiFinderOfKarma() : base("Kuukai, Finder of Karma", 5, 10500, Subtype.MechaThunder, Civilization.Light)
+        {
+            AddBlockerAbility();
+            AddTriggeredAbility(new TriggeredAbilities.BlockAbility(new OneShotEffects.UntapItAfterItBattlesEffect()));
+            AddThisCreatureCannotAttackPlayersAbility();
+        }
+    }
+}
