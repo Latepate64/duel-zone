f3b481465a4cbb41f9dcc9e6239f174b85c8f2e8
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-30 19:53:16 +0300

    Rocketdive Skyterror

---
+++ Cards/Cards/DM08/RocketdiveSkyterror.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM08
+{
+    class RocketdiveSkyterror : Creature
+    {
+        public RocketdiveSkyterror() : base("Rocketdive Skyterror", 4, 5000, Subtype.ArmoredWyvern, Civilization.Fire)
+        {
+            AddThisCreatureCannotBeAttackedAbility();
+            AddThisCreatureCannotAttackPlayersAbility();
+            AddPowerAttackerAbility(1000);
+        }
+    }
+}
