f4d6a87e276a0bfe3be68c40419b8df873c23ea8
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-30 18:22:48 +0300

    Dracodance Totem

---
+++ Cards/Cards/DM08/DracodanceTotem.cs
@@ -0,0 +1,84 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
+using System.Linq;
+
+namespace Cards.Cards.DM08
+{
+    class DracodanceTotem : Creature
+    {
+        public DracodanceTotem() : base("Dracodance Totem", 2, 1000, Subtype.MysteryTotem, Civilization.Nature)
+        {
+            AddStaticAbilities(new DracodanceTotemEffect());
+        }
+    }
+
+    class DracodanceTotemEffect : ContinuousEffects.DestructionReplacementEffect
+    {
+        public DracodanceTotemEffect() : base(new TargetFilter()) 
+        {
+        }
+
+        public override bool Apply(IGame game, Engine.IPlayer player)
+        {
+            var filter = new CardFilters.DragonFilter();
+            var manaZoneDragons = player.ManaZone.Creatures.Where(x => filter.Applies(x, game, game.GetOwner(x)));
+            if (manaZoneDragons.Any())
+            {
+                game.Move(ZoneType.BattleZone, ZoneType.ManaZone, game.GetAllCards(Filter, player.Id).ToArray());
+                new DracodanceTotemRecoveryEffect().Apply(game, null);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new DracodanceTotemEffect();
+        }
+
+        public override string ToString()
+        {
+            return "When this creature would be destroyed, if you have a creature that has Dragon in its race in your mana zone, put this creature into your mana zone instead of destroying it. Then return a creature that has Dragon in its race from your mana zone to your hand.";
+        }
+    }
+
+    class DracodanceTotemRecoveryEffect : OneShotEffects.ManaRecoveryEffect
+    {
+        public DracodanceTotemRecoveryEffect() : base(1, 1, true, new DragonInYourManaZoneFilter())
+        {
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new DracodanceTotemRecoveryEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Return a creature that has Dragon in its race from your mana zone to your hand.";
+        }
+    }
+
+    class DragonInYourManaZoneFilter : CardFilters.OwnersManaZoneCreatureFilter
+    {
+        public override bool Applies(Engine.ICard card, IGame game, Engine.IPlayer player)
+        {
+            return base.Applies(card, game, player) && new CardFilters.DragonFilter().Applies(card, game, player);
+        }
+
+        public override CardFilter Copy()
+        {
+            return new DragonInYourManaZoneFilter();
+        }
+
+        public override string ToString()
+        {
+            return "a creature that has Dragon in its race from your mana zone";
+        }
+    }
+}
