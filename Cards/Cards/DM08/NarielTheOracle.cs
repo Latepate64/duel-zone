385cb9b46d7252bc5629f7365bf79fcfd407a740
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-30 19:51:31 +0300

    Nariel, the Oracle

---
+++ Cards/Cards/DM08/NarielTheOracle.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using Engine.ContinuousEffects;
+
+namespace Cards.Cards.DM08
+{
+    class NarielTheOracle : Creature
+    {
+        public NarielTheOracle() : base("Nariel, the Oracle", 4, 1000, Subtype.LightBringer, Civilization.Light)
+        {
+            AddStaticAbilities(new NarielTheOracleEffect());
+        }
+    }
+
+    class NarielTheOracleEffect : CannotAttackEffect
+    {
+        public NarielTheOracleEffect() : base(new CardFilters.BattleZoneMinPowerCreatureFilter(3000), new Durations.Indefinite())
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new NarielTheOracleEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Creatures that have power 3000 or more can't attack.";
+        }
+    }
+}
