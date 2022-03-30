be595c974ab00ff5a895d8303f9089d9f5d2641d
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-30 19:48:50 +0300

    Muscle Charger

---
+++ Cards/Cards/DM08/MuscleCharger.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM08
+{
+    class MuscleCharger : Charger
+    {
+        public MuscleCharger() : base("Muscle Charger", 3, Civilization.Nature)
+        {
+            AddSpellAbilities(new OneShotEffects.EachOfYourCreaturesGetsPowerUntilTheEndOfTheTurnEffect(3000));
+        }
+    }
+}
