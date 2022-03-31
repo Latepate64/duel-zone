7764dd9d3765fe6fcdb52036d8e78f01feabcf92
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 18:52:51 +0300

    Balesk Baj, the Timeburner

---
+++ Cards/OneShotEffects/TakeExtraTurnAfterThisOneEffect.cs
@@ -0,0 +1,25 @@
+﻿using Engine;
+using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class TakeExtraTurnAfterThisOneEffect : OneShotEffect
+    {
+        public override Turn Apply(IGame game, IAbility source)
+        {
+            Turn turn = new() { ActivePlayer = source.GetController(game), NonActivePlayer = source.GetOpponent(game) };
+            game.ExtraTurns.Push(turn);
+            return turn;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new TakeExtraTurnAfterThisOneEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Take an extra turn after this one.";
+        }
+    }
+}
