9e6a622a7b81ba513d00d8b9fbfec30b870a96fb
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 17:54:39 +0300

    Terradragon Gamiratar

---
+++ Cards/Cards/DM08/TerradragonGamiratar.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM08
+{
+    class TerradragonGamiratar : Creature
+    {
+        public TerradragonGamiratar() : base("Terradragon Gamiratar", 4, 6000, Subtype.EarthDragon, Civilization.Nature)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new TerradragonGamiratarEffect());
+            AddDoubleBreakerAbility();
+        }
+    }
+
+    class TerradragonGamiratarEffect : OneShotEffects.CardMovingChoiceEffect
+    {
+        public TerradragonGamiratarEffect() : base(new CardFilters.OpponentsHandCreatureFilter(), 0, 1, false, ZoneType.Hand, ZoneType.BattleZone)
+        {
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new TerradragonGamiratarEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Your opponent may choose a creature in his hand and put it into the battle zone.";
+        }
+    }
+}
