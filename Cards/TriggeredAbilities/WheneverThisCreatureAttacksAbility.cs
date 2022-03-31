2db19b6f8ff8233165684eb42787bf858fd0a4aa
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 18:04:07 +0300

    Thrumiss, Zephyr Guardian

--- Cards/TriggeredAbilities/WheneverThisCreatureAttacksAbility.cs
+++ Cards/TriggeredAbilities/WheneverThisCreatureAttacksAbility.cs
@@ -4,19 +4,14 @@ using Engine.Abilities;
 
 namespace Cards.TriggeredAbilities
 {
-    public class WheneverThisCreatureAttacksAbility : CardTriggeredAbility
+    class WheneverThisCreatureAttacksAbility : WheneverCreatureAttacksAbility
     {
-        public WheneverThisCreatureAttacksAbility(IOneShotEffect effect) : base(effect)
+        public WheneverThisCreatureAttacksAbility(IOneShotEffect effect) : base(effect, new TargetFilter())
         {
         }
 
         public WheneverThisCreatureAttacksAbility(WheneverThisCreatureAttacksAbility ability) : base(ability)
         {
-        }
-
-        public override bool CanTrigger(IGameEvent gameEvent, IGame game)
-        {
-            return base.CanTrigger(gameEvent, game) && gameEvent is CreatureAttackedEvent;
         }
 
         public override IAbility Copy()
@@ -27,6 +22,43 @@ namespace Cards.TriggeredAbilities
         public override string ToString()
         {
             return $"Whenever this creature attacks, {GetEffectText()}";
+        }
+    }
+
+    abstract class WheneverCreatureAttacksAbility : CardTriggeredAbility
+    {
+        protected WheneverCreatureAttacksAbility(WheneverCreatureAttacksAbility ability) : base(ability)
+        {
+        }
+
+        protected WheneverCreatureAttacksAbility(IOneShotEffect effect, ICardFilter filter) : base(effect, filter)
+        {
+        }
+
+        public override bool CanTrigger(IGameEvent gameEvent, IGame game)
+        {
+            return base.CanTrigger(gameEvent, game) && gameEvent is CreatureAttackedEvent;
+        }
+    }
+
+    class WheneverAnyOfYourCreaturesAttacksAbility : WheneverCreatureAttacksAbility
+    {
+        public WheneverAnyOfYourCreaturesAttacksAbility(IOneShotEffect effect) : base(effect, new CardFilters.OwnersBattleZoneCreatureFilter())
+        {
+        }
+
+        public WheneverAnyOfYourCreaturesAttacksAbility(WheneverAnyOfYourCreaturesAttacksAbility ability) : base(ability)
+        {
+        }
+
+        public override IAbility Copy()
+        {
+            return new WheneverAnyOfYourCreaturesAttacksAbility(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Whenever any of your creatures attacks, {GetEffectText()}";
         }
     }
 }
