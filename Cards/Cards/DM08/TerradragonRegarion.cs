7a99cc10105ead456f71a1da7319c575aeaf92a2
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 17:55:44 +0300

    Terradragon Regarion

---
+++ Cards/Cards/DM08/TerradragonRegarion.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM08
+{
+    class TerradragonRegarion : Creature
+    {
+        public TerradragonRegarion() : base("Terradragon Regarion", 5, 4000, Subtype.EarthDragon, Civilization.Nature)
+        {
+            AddPowerAttackerAbility(3000);
+            AddDoubleBreakerAbility();
+        }
+    }
+}
