334d265e827d4ac5a8475fa855d636dee9e9e94f
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 19:14:56 +0300

    Jil Warka, Time Guardian

---
+++ Cards/Cards/DM09/JilWarkaTimeGuardian.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM09
+{
+    class JilWarkaTimeGuardian : Creature
+    {
+        public JilWarkaTimeGuardian() : base("Jil Warka, Time Guardian", 3, 2000, Subtype.Guardian, Civilization.Light)
+        {
+            AddBlockerAbility();
+            AddThisCreatureCannotAttackPlayersAbility();
+            AddWhenThisCreatureIsDestroyedAbility(new OneShotEffects.ChooseUpToOfYourOpponentsCreaturesInTheBattleZoneAndTapThemEffect(2));
+        }
+    }
+}
