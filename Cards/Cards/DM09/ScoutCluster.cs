502709518cc129de796dc65a2c47617205951b9c
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 19:39:48 +0300

    Scout Cluster

---
+++ Cards/Cards/DM09/ScoutCluster.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM09
+{
+    class ScoutCluster : Creature
+    {
+        public ScoutCluster() : base("Scout Cluster", 3, 4000, Subtype.CyberCluster, Civilization.Water)
+        {
+            AddBlockerAbility();
+            AddTriggeredAbility(new TriggeredAbilities.WhenYouPutAnotherCreatureIntoTheBattleZoneAbility(new OneShotEffects.ReturnThisCreatureToYourHandEffect()));
+        }
+    }
+}
