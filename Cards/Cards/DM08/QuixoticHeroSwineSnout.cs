941d6284d609e4037331f19dc929ce7a0e51e9fd
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 17:29:01 +0300

    Sol Galla, Halo Guardian

--- Cards/Cards/DM08/QuixoticHeroSwineSnout.cs
+++ Cards/Cards/DM08/QuixoticHeroSwineSnout.cs
@@ -1,33 +1,10 @@
-﻿using Cards.TriggeredAbilities;
-using Engine;
-using Engine.Abilities;
-
-namespace Cards.Cards.DM08
+﻿namespace Cards.Cards.DM08
 {
     class QuixoticHeroSwineSnout : Creature
     {
         public QuixoticHeroSwineSnout() : base("Quixotic Hero Swine Snout", 2, 1000, Common.Subtype.BeastFolk, Common.Civilization.Nature)
         {
-            AddTriggeredAbility(new WheneverAnotherCreatureIsPutIntoTheBattleZoneAbility(new QuixoticHeroSwineSnoutEffect()));
-        }
-    }
-
-    class QuixoticHeroSwineSnoutEffect : OneShotEffect
-    {
-        public override IOneShotEffect Copy()
-        {
-            return new QuixoticHeroSwineSnoutEffect();
-        }
-
-        public override object Apply(IGame game, IAbility source)
-        {
-            game.AddContinuousEffects(source, new ContinuousEffects.ThisCreatureGetsPowerUntilTheEndOfTheTurnEffect(3000, game.GetCard(source.Source)));
-            return null;
-        }
-
-        public override string ToString()
-        {
-            return $"This creature gets +3000 power until the end of the turn.";
+            AddTriggeredAbility(new TriggeredAbilities.WheneverAnotherCreatureIsPutIntoTheBattleZoneAbility(new OneShotEffects.ThisCreatureGetsPowerUntilTheEndOfTheTurnEffect()));
         }
     }
 }
---
+++ Cards/TriggeredAbilities/WheneverPlayerCastsSpellAbility.cs
@@ -0,0 +1,32 @@
+﻿using Common.GameEvents;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.TriggeredAbilities
+{
+    class WheneverPlayerCastsSpellAbility : TriggeredAbility
+    {
+        public WheneverPlayerCastsSpellAbility(IOneShotEffect effect) : base(effect)
+        {
+        }
+
+        public WheneverPlayerCastsSpellAbility(WheneverPlayerCastsSpellAbility ability) : base(ability)
+        {
+        }
+
+        public override bool CanTrigger(IGameEvent gameEvent, IGame game)
+        {
+            return gameEvent is SpellCastEvent;
+        }
+
+        public override IAbility Copy()
+        {
+            return new WheneverPlayerCastsSpellAbility(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Whenever a player casts a spell, {GetEffectText()}";
+        }
+    }
+}
