c61555c60abb15264460c85e9d094bd627ff9f72
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 18:20:46 +0300

    Überdragon Bajula

---
+++ Cards/Cards/DM08/ÜberdragonBajula.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM08
+{
+    class ÜberdragonBajula : DragonEvolutionCreature
+    {
+        public ÜberdragonBajula() : base("Überdragon Bajula", 7, 13000, Subtype.ArmoredDragon, Civilization.Fire)
+        {
+            AddWheneverThisCreatureAttacksAbility(new OneShotEffects.ChooseUpToCardsInYourOpponentsManaZoneAndPutThemIntoHisGraveyardEffect(2));
+        }
+    }
+}
