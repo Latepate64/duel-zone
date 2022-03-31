f9479f445699ca02e77e5a2773857f820ad129c3
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 18:55:37 +0300

    Cavern Raider

---
+++ Cards/Cards/DM09/CavernRaider.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM09
+{
+    class CavernRaider : Creature
+    {
+        public CavernRaider() : base("Cavern Raider", 3, 2000, Subtype.BeastFolk, Civilization.Nature)
+        {
+            AddTriggeredAbility(new TriggeredAbilities.WheneverThisCreatureIsAttackingYourOpponentAndIsNotBlockedAbility(new OneShotEffects.SearchCreatureEffect()));
+        }
+    }
+}
