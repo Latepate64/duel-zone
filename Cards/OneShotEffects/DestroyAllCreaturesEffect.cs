fb0c8cd22f08b44a07866ad07fcd686d5a6e23ee
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 19:57:01 +0300

    Stallob, the Lifequasher

---
+++ Cards/OneShotEffects/DestroyAllCreaturesEffect.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Abilities;
+
+namespace Cards.OneShotEffects
+{
+    class DestroyAllCreaturesEffect : DestroyAreaOfEffect
+    {
+        public DestroyAllCreaturesEffect() : base(new CardFilters.BattleZoneCreatureFilter())
+        {
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new DestroyAllCreaturesEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Destroy all creatures.";
+        }
+    }
+}
