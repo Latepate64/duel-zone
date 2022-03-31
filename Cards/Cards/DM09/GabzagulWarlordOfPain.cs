9f2c3197998d4c5788e9babe940856ea29655c40
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-03-31 19:03:01 +0300

    Gabzagul, Warlord of Pain

---
+++ Cards/Cards/DM09/GabzagulWarlordOfPain.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using Engine.ContinuousEffects;
+
+namespace Cards.Cards.DM09
+{
+    class GabzagulWarlordOfPain : Creature
+    {
+        public GabzagulWarlordOfPain() : base("Gabzagul, Warlord of Pain", 6, 5000, Subtype.DarkLord, Civilization.Darkness)
+        {
+            AddStaticAbilities(new GabzagulWarlordOfPainEffect());
+        }
+    }
+
+    class GabzagulWarlordOfPainEffect : AttacksIfAbleEffect
+    {
+        public GabzagulWarlordOfPainEffect() : base(new CardFilters.BattleZoneCreatureFilter(), new Durations.Indefinite())
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new GabzagulWarlordOfPainEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Each creature attacks each turn if able.";
+        }
+    }
+}
