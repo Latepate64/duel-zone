2413db8c54cc4a58b64fcc89da71f12482fe9749
Latepate64 <lauri.koriseva@gmail.com>
2022-04-01 17:15:33 +0300

    Vine Charger

---
+++ Cards/Cards/DM09/VineCharger.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM09
+{
+    class VineCharger : Charger
+    {
+        public VineCharger() : base("Vine Charger", 4, Civilization.Nature)
+        {
+            AddSpellAbilities(new OneShotEffects.YourOpponentChoosesOneOfHisCreaturesInTheBattleZoneAndPutsItIntoHisManaZoneEffect());
+        }
+    }
+}
