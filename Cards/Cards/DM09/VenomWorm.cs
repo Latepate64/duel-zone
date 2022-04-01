34fda16ec9a4d2a64040021ee19c8b544758c51b
Latepate64 <lauri.koriseva@gmail.com>
2022-04-01 19:28:29 +0300

    Venom Worm

---
+++ Cards/Cards/DM09/VenomWorm.cs
@@ -0,0 +1,58 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
+using System.Linq;
+
+namespace Cards.Cards.DM09
+{
+    class VenomWorm : Creature
+    {
+        public VenomWorm() : base("Venom Worm", 3, 1000, Subtype.ParasiteWorm, Civilization.Darkness)
+        {
+            AddTapAbility(new VenomWormOneShotEffect());
+        }
+    }
+
+    class VenomWormOneShotEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            var race = source.GetController(game).ChooseRace();
+            var creatures = game.BattleZone.GetCreatures(source.Controller).Where(x => x.Subtypes.Contains(race)).ToArray();
+            game.AddContinuousEffects(source, new VenomWormContinuousEffect(creatures));
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new VenomWormOneShotEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Choose a race. Each creature of that race gets \"slayer\" until the end of the turn.";
+        }
+    }
+
+    class VenomWormContinuousEffect : AbilityAddingEffect
+    {
+        public VenomWormContinuousEffect(VenomWormContinuousEffect effect) : base(effect)
+        {
+        }
+
+        public VenomWormContinuousEffect(params Engine.ICard[] cards) : base(new CardFilters.TargetsFilter(cards), new Durations.UntilTheEndOfTheTurn(), new StaticAbilities.SlayerAbility())
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new VenomWormContinuousEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return $"{Filter} have \"slayer\" until the end of the turn.";
+        }
+    }
+}
