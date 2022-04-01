d0ebf9ce93a68589b1d28b6ba1736c93692c2d03
Latepate64 <lauri.koriseva@gmail.com>
2022-04-01 19:36:01 +0300

    Zombie Carnival

---
+++ Cards/Cards/DM09/ZombieCarnival.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM09
+{
+    class ZombieCarnival : Spell
+    {
+        public ZombieCarnival() : base("Zombie Carnival", 5, Civilization.Darkness)
+        {
+            AddSpellAbilities(new ZombieCarnivalEffect());
+        }
+    }
+
+    class ZombieCarnivalEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            return new OneShotEffects.YouMayReturnSubtypeCreatureFromYourGraveyardToYourHandEffect(source.GetController(game).ChooseRace(), 3).Apply(game, source);
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new ZombieCarnivalEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Choose a race. Return up to 3 creatures of that race from your graveyard to your hand.";
+        }
+    }
+}
