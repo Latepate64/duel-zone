966b2a6b9ad180d6602bdaafe89f3f369ce63a5e
Latepate64 <lauri.koriseva@gmail.com>
2022-04-01 16:07:06 +0300

    Tekorax

---
+++ Cards/Cards/DM09/Tekorax.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM09
+{
+    class Tekorax : Creature
+    {
+        public Tekorax() : base("Tekorax", 5, 3000, Subtype.SeaHacker, Civilization.Water)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new TekoraxEffect());
+        }
+    }
+
+    class TekoraxEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            var cards = source.GetOpponent(game).ShieldZone.Cards;
+            if (cards.Any())
+            {
+                source.GetController(game).Look(source.GetOpponent(game), game, cards.ToArray());
+                source.GetOpponent(game).Unreveal(cards);
+            }
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new TekoraxEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Look at your opponent's shields. Then put them back where they were.";
+        }
+    }
+}
