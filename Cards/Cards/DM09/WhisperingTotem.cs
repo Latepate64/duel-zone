e2d2edb2d4ef62b1dbd4ce11cf56ffd258428ca3
Latepate64 <lauri.koriseva@gmail.com>
2022-04-01 17:36:10 +0300

    Whispering Totem

---
+++ Cards/Cards/DM09/WhisperingTotem.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM09
+{
+    class WhisperingTotem : Creature
+    {
+        public WhisperingTotem() : base("Whispering Totem", 4, 2000, Subtype.MysteryTotem, Civilization.Nature)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.SearchCardWithNameEffect(Name));
+        }
+    }
+}
