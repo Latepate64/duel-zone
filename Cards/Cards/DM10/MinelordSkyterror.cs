d275c6291c2863f01e3b68e344a26d8697f1ab38
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 16:47:18 +0300

    Minelord Skyterror

---
+++ Cards/Cards/DM10/MinelordSkyterror.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class MinelordSkyterror : SilentSkillCreature
+    {
+        public MinelordSkyterror() : base("Minelord Skyterror", 4, 3000, Subtype.ArmoredWyvern, Civilization.Fire)
+        {
+            AddSilentSkillAbility(new OneShotEffects.DestroyMaxPowerAreaOfEffect(3000));
+        }
+    }
+}
