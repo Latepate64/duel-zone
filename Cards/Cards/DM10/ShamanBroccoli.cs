a90b343b7aee15bb570a3f0a250ec8e1b5aeee2d
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 17:21:03 +0300

    Shaman Broccoli

---
+++ Cards/Cards/DM10/ShamanBroccoli.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class ShamanBroccoli : Creature
+    {
+        public ShamanBroccoli() : base("Shaman Broccoli", 2, 1000, Subtype.WildVeggies, Civilization.Nature)
+        {
+            AddStaticAbilities(new ContinuousEffects.WhenThisCreatureWouldBeDestroyedPutItIntoYourManaZoneInsteadEffect());
+        }
+    }
+}
