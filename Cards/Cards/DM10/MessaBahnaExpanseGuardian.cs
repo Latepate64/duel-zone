c05d633f020b53307e060a03c4abb949863ed23d
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 10:40:45 +0300

    Messa Bahna, Expanse Guardian. Pierr, Psycho Doll

---
+++ Cards/Cards/DM10/MessaBahnaExpanseGuardian.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class MessaBahnaExpanseGuardian : Creature
+    {
+        public MessaBahnaExpanseGuardian() : base("Messa Bahna, Expanse Guardian", 3, 5000, Subtype.Guardian, Civilization.Light)
+        {
+            AddBlockerAbility();
+            AddStaticAbilities(new ContinuousEffects.ThisCreatureBlocksIfAble());
+            AddThisCreatureCannotAttackPlayersAbility();
+        }
+    }
+}
---
+++ Cards/Cards/DM10/PierrPsychoDoll.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class PierrPsychoDoll : Creature
+    {
+        public PierrPsychoDoll() : base("Pierr, Psycho Doll", 2, 1000, Subtype.DeathPuppet, Civilization.Darkness)
+        {
+            AddBlockerAbility();
+            AddStaticAbilities(new ContinuousEffects.ThisCreatureBlocksIfAble());
+            AddThisCreatureCannotAttackAbility();
+            AddSlayerAbility();
+        }
+    }
+}
