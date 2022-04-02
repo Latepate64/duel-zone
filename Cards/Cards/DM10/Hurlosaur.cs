b1c5dafe48fc5b6dabbfcd3f0456068969d7243e
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 15:23:17 +0300

    Hurlosaur

---
+++ Cards/Cards/DM10/Hurlosaur.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class Hurlosaur : Creature
+    {
+        public Hurlosaur() : base("Hurlosaur", 6, 2000, Subtype.RockBeast, Civilization.Fire)
+        {
+            ShieldTrigger = true;
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.DestroyOnefYourOpponentsCreaturesThatHasMaxPowerEffect(1000));
+        }
+    }
+}
