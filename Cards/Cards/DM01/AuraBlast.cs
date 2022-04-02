1157447fae8757af7fcb756b6ebd7fdaf0e6f01c
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 18:04:32 +0300

    Thirst for the Hunt

--- Cards/Cards/DM01/AuraBlast.cs
+++ Cards/Cards/DM01/AuraBlast.cs
@@ -1,38 +1,10 @@
-﻿using Cards.OneShotEffects;
-using Engine;
-using Engine.Abilities;
-using System.Linq;
-
-namespace Cards.Cards.DM01
+﻿namespace Cards.Cards.DM01
 {
     class AuraBlast : Spell
     {
         public AuraBlast() : base("Aura Blast", 4, Common.Civilization.Nature)
         {
-            AddSpellAbilities(new AuraBlastEffect());
-        }
-    }
-
-    class AuraBlastEffect : OneShotAreaOfEffect
-    {
-        public AuraBlastEffect() : base(new CardFilters.OwnersBattleZoneCreatureFilter())
-        {
-        }
-
-        public override object Apply(IGame game, IAbility source)
-        {
-            game.AddContinuousEffects(source, new ContinuousEffects.ThisCreatureGetsPowerAttackerUntilTheEndOfTheTurnEffect(GetAffectedCards(game, source).ToArray()));
-            return null;
-        }
-
-        public override IOneShotEffect Copy()
-        {
-            return new AuraBlastEffect();
-        }
-
-        public override string ToString()
-        {
-            return "Each of your creatures in the battle zone gets \"power attacker +2000\" until the end of the turn.";
+            AddSpellAbilities(new OneShotEffects.AuraBlastEffect(2000));
         }
     }
 }
---
+++ Cards/Cards/DM10/ThirstForTheHunt.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class ThirstForTheHunt : Spell
+    {
+        public ThirstForTheHunt() : base("Thirst for the Hunt", 1, Civilization.Nature)
+        {
+            AddSpellAbilities(new OneShotEffects.AuraBlastEffect(1000));
+        }
+    }
+}
