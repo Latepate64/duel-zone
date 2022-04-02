1e7e377692710a94a26b701b6d998836ab8bfba4
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 10:53:35 +0300

    Siege Roller Bagash

---
+++ Cards/Cards/DM10/SiegeRollerBagash.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class SiegeRollerBagash : Creature
+    {
+        public SiegeRollerBagash() : base("Siege Roller Bagash", 4, 3000, Subtype.Armorloid, Civilization.Fire)
+        {
+            AddStaticAbilities(new ContinuousEffects.DogarnTheMarauderEffect(1000));
+        }
+    }
+}
