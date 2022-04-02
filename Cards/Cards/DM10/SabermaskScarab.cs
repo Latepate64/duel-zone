d485722080d3640be1e5d128ecf72de930bd84c6
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 17:17:36 +0300

    Sabermask Scarab

---
+++ Cards/Cards/DM10/SabermaskScarab.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class SabermaskScarab : Creature
+    {
+        public SabermaskScarab() : base("Sabermask Scarab", 4, 4000, Subtype.GiantInsect, Civilization.Nature)
+        {
+            AddWheneverThisCreatureAttacksAbility(new OneShotEffects.ReturnCardFromYourManaZoneToYourHandEffect());
+        }
+    }
+}
