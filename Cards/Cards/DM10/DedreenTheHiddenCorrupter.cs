16b7202f5a506d060516baebb269bd6518e64196
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 13:04:01 +0300

    Dedreen, the Hidden Corrupter

---
+++ Cards/Cards/DM10/DedreenTheHiddenCorrupter.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class DedreenTheHiddenCorrupter : Creature
+    {
+        public DedreenTheHiddenCorrupter() : base("Dedreen, the Hidden Corrupter", 5, 4000, Subtype.PandorasBox, Civilization.Darkness)
+        {
+            AddTriggeredAbility(new TriggeredAbilities.DedreenTheHiddenCorrupterAbility(new OneShotEffects.OpponentRandomDiscardEffect()));
+        }
+    }
+}
--- Cards/TriggeredAbilities/AncientHornTheWatcherAbility.cs
+++ Cards/TriggeredAbilities/AncientHornTheWatcherAbility.cs
@@ -9,11 +9,11 @@ namespace Cards.TriggeredAbilities
         {
         }
 
-        public AncientHornTheWatcherAbility(WhenYouPutThisCreatureIntoTheBattleZoneAbility ability) : base(ability)
+        public AncientHornTheWatcherAbility(AncientHornTheWatcherAbility ability) : base(ability)
         {
         }
 
-        public override Ability Copy()
+        public override IAbility Copy()
         {
             return new AncientHornTheWatcherAbility(this);
         }
@@ -26,6 +26,32 @@ namespace Cards.TriggeredAbilities
         public override string ToString()
         {
             return $"When you put this creature into the battle zone, if you have 5 or more shields, {GetEffectText()}";
+        }
+    }
+
+    class DedreenTheHiddenCorrupterAbility : WhenYouPutThisCreatureIntoTheBattleZoneAbility
+    {
+        public DedreenTheHiddenCorrupterAbility(IOneShotEffect effect) : base(effect)
+        {
+        }
+
+        public DedreenTheHiddenCorrupterAbility(DedreenTheHiddenCorrupterAbility ability) : base(ability)
+        {
+        }
+
+        public override IAbility Copy()
+        {
+            return new DedreenTheHiddenCorrupterAbility(this);
+        }
+
+        public override bool CheckInterveningIfClause(IGame game)
+        {
+            return GetOpponent(game).ShieldZone.Cards.Count <= 3;
+        }
+
+        public override string ToString()
+        {
+            return $"When you put this creature into the battle zone, if your opponent has 3 or fewer shields, {GetEffectText()}";
         }
     }
 }
