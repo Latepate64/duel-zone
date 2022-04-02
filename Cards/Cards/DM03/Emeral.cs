0407d313d440ae020f62de07e61df692c3b48d38
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 12:36:17 +0300

    Charge Whipper

--- Cards/Cards/DM03/Emeral.cs
+++ Cards/Cards/DM03/Emeral.cs
@@ -1,55 +1,10 @@
-﻿using Cards.OneShotEffects;
-using Engine;
-using Engine.Abilities;
-using System.Linq;
-
-namespace Cards.Cards.DM03
+﻿namespace Cards.Cards.DM03
 {
     class Emeral : Creature
     {
         public Emeral() : base("Emeral", 2, 1000, Common.Subtype.CyberLord, Common.Civilization.Water)
         {
-            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new EmeralEffect());
-        }
-    }
-
-    class EmeralEffect : OneShotEffect
-    {
-        public override IOneShotEffect Copy()
-        {
-            return new EmeralEffect();
-        }
-
-        public override object Apply(IGame game, IAbility source)
-        {
-            var cards = new EmeralShieldAdditionEffect().Apply(game, source);
-            if (cards.Any())
-            {
-                return new ShieldRecoveryCannotUseShieldTriggerEffect().Apply(game, source);
-            }
-            return cards;
-        }
-
-        public override string ToString()
-        {
-            return "You may add a card from your hand to your shields face down. If you do, choose one of your shields and put it into your hand. You can't use the \"shield trigger\" ability of that shield.";
-        }
-    }
-
-    class EmeralShieldAdditionEffect : ShieldAdditionEffect
-    {
-        public EmeralShieldAdditionEffect() : base(new CardFilters.OwnersHandCardFilter(), 0, 1, true)
-        {
-        }
-
-        public override IOneShotEffect Copy()
-        {
-            return new EmeralShieldAdditionEffect();
-        }
-
-        public override string ToString()
-        {
-            return "You may add a card from your hand to your shields face down.";
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.EmeralEffect());
         }
     }
 }
---
+++ Cards/Cards/DM10/ChargeWhipper.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class ChargeWhipper : SilentSkillCreature
+    {
+        public ChargeWhipper() : base("Charge Whipper", 3, 2000, Subtype.CyberVirus, Civilization.Water)
+        {
+            AddSilentSkillAbility(new OneShotEffects.EmeralEffect());
+        }
+    }
+}
