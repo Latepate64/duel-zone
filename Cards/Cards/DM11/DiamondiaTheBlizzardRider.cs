5467b3113308d140c857a463e42e4b5afbfc0818
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 19:23:55 +0300

    Diamondia, the Blizzard Rider

---
+++ Cards/Cards/DM11/DiamondiaTheBlizzardRider.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM11
+{
+    class DiamondiaTheBlizzardRider : EvolutionCreature
+    {
+        public DiamondiaTheBlizzardRider() : base("Diamondia, the Blizzard Rider", 3, 5000, Subtype.SnowFaerie, Civilization.Nature)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new DiamondiaTheBlizzardRiderEffect());
+        }
+    }
+
+    class DiamondiaTheBlizzardRiderEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            game.Move(ZoneType.Graveyard, ZoneType.Hand, source.GetController(game).Graveyard.Cards.Where(x => x.Subtypes.Contains(Subtype.SnowFaerie)).ToArray());
+            game.Move(ZoneType.ManaZone, ZoneType.Hand, source.GetController(game).ManaZone.Cards.Where(x => x.Subtypes.Contains(Subtype.SnowFaerie)).ToArray());
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new DiamondiaTheBlizzardRiderEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Return all Snow Faeries from your graveyard and your mana zone to your hand.";
+        }
+    }
+}
