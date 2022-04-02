b90c27b390c6f66f1d09def2080c3ec0eae9fb3a
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 11:35:10 +0300

    Bulgluf, the Spydroid

---
+++ Cards/Cards/DM10/BulglufTheSpydroid.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class BulglufTheSpydroid : SilentSkillCreature
+    {
+        public BulglufTheSpydroid() : base("Bulgluf, the Spydroid", 6, 4000, Subtype.Soltrooper, Civilization.Light)
+        {
+            AddSilentSkillAbility(new OneShotEffects.AddTheTopCardOfYourDeckToYourShieldsFaceDownEffect());
+        }
+    }
+}
