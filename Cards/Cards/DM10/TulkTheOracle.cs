f06488f5c0f5d9ee21cec8729af154cab01a23fa
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 18:07:28 +0300

    Tulk, the Oracle

---
+++ Cards/Cards/DM10/TulkTheOracle.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class TulkTheOracle : Creature
+    {
+        public TulkTheOracle() : base("Tulk, the Oracle", 1, 500, Subtype.LightBringer, Civilization.Light)
+        {
+        }
+    }
+}
