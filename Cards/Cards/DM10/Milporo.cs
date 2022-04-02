1954fc140b88df9d22bf0311036c879152a83954
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 16:34:51 +0300

    Milporo

---
+++ Cards/Cards/DM10/Milporo.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class Milporo : SilentSkillCreature
+    {
+        public Milporo() : base("Milporo", 4, 3000, Subtype.CyberLord, Civilization.Water)
+        {
+            AddSilentSkillAbility(new OneShotEffects.DrawCardsEffect(1));
+        }
+    }
+}
