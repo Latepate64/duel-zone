3989c06d6d3ded98327237b5e3a7f9db05d4d61e
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 16:32:44 +0300

    Mezger, Commando Leader

---
+++ Cards/Cards/DM10/MezgerCommandoLeader.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class MezgerCommandoLeader : Creature
+    {
+        public MezgerCommandoLeader() : base("Mezger, Commando Leader", 4, 2000, Subtype.Human, Civilization.Fire)
+        {
+            AddSpeedAttackerAbility();
+        }
+    }
+}
