373ab2c9cf241fec4c23da0fd2aefd33d6d46442
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 18:19:33 +0300

    Upheaval

---
+++ Cards/Cards/DM10/Upheaval.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM10
+{
+    class Upheaval : Spell
+    {
+        public Upheaval() : base("Upheaval", 6, Civilization.Darkness)
+        {
+            ShieldTrigger = true;
+            AddSpellAbilities(new UpheavalEffect());
+        }
+    }
+
+    class UpheavalEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            var mana = game.Players.SelectMany(x => x.ManaZone.Cards);
+            var hand = game.Players.SelectMany(x => x.Hand.Cards);
+            game.Move(ZoneType.ManaZone, ZoneType.Hand, mana.ToArray());
+            game.MoveTapped(ZoneType.Hand, ZoneType.ManaZone, hand.ToArray());
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new UpheavalEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Each player puts all the cards from his mana zone into his hand and, at the same time, puts all the cards from his hand into his mana zone tapped.";
+        }
+    }
+}
