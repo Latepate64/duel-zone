e8998c132a4179aaa7ae041d66f4a554b8aa634a
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 10:36:36 +0300

    Belmol, the Explorer

---
+++ Cards/Cards/DM12/BelmolTheExplorer.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM12
+{
+    class BelmolTheExplorer : Creature
+    {
+        public BelmolTheExplorer() : base("Belmol, the Explorer", 2, 3500, Subtype.Gladiator, Civilization.Light)
+        {
+            AddBlockerAbility();
+            AddStaticAbilities(new ContinuousEffects.ThisCreatureBlocksIfAble());
+            AddThisCreatureCannotAttackPlayersAbility();
+        }
+    }
+}
