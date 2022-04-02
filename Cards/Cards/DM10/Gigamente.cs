05b5185e8339683c11e9ef13f0cf1ab75ab5ea90
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 15:13:11 +0300

    Gigamente

---
+++ Cards/Cards/DM10/Gigamente.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class Gigamente : SilentSkillCreature
+    {
+        public Gigamente() : base("Gigamente", 4, 3000, Subtype.Chimera, Civilization.Darkness)
+        {
+            AddSilentSkillAbility(new OneShotEffects.ReturnCreatureFromYourGraveyardToYourHandEffect());
+        }
+    }
+}
