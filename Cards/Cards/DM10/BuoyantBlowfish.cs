b6d3faaf9f897020ced34e819352a3cde613ebbf
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 12:07:35 +0300

    Buoyant Blowfish

---
+++ Cards/Cards/DM10/BuoyantBlowfish.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM10
+{
+    class BuoyantBlowfish : Creature
+    {
+        public BuoyantBlowfish() : base("Buoyant Blowfish", 5, 1000, Subtype.GelFish, Civilization.Water)
+        {
+            AddStaticAbilities(new ContinuousEffects.GetsPowerForEachTappedCardInYourOpponentsManaZoneEffect(1000));
+        }
+    }
+}
