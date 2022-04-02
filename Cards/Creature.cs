9ad7da650fa77dabdfc302359c5342d04524422a
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 17:32:22 +0300

    Soderlight, the Cold Blade

--- Cards/Creature.cs
+++ Cards/Creature.cs
@@ -163,6 +163,10 @@ namespace Cards
         {
         }
 
+        public SilentSkillCreature(string name, int manaCost, int power, Subtype race, Civilization civilization1, Civilization civilization2) : base(name, manaCost, power, race, civilization1, civilization2)
+        {
+        }
+
         protected void AddSilentSkillAbility(IOneShotEffect effect)
         {
             AddAbilities(new SilentSkillAbility(effect));
