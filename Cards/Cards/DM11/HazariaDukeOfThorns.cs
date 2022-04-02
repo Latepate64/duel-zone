15ab6651fa843987416147352ff36c9a9c7fedf9
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 19:36:43 +0300

    Hazaria, Duke of Thorns

---
+++ Cards/Cards/DM11/HazariaDukeOfThorns.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class HazariaDukeOfThorns : WaveStrikerCreature
+    {
+        public HazariaDukeOfThorns() : base("Hazaria, Duke of Thorns", 4, 2000, Subtype.DarkLord, Civilization.Darkness)
+        {
+            AddWaveStrikerAbility(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.OpponentSacrificeEffect()));
+        }
+    }
+}
