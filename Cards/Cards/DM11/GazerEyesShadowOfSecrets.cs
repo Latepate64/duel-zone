0c3add18ea81baeb266fa84db7fd866e01713a87
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 19:35:01 +0300

    Gazer Eyes, Shadow of Secrets

---
+++ Cards/Cards/DM11/GazerEyesShadowOfSecrets.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class GazerEyesShadowOfSecrets : SilentSkillCreature
+    {
+        public GazerEyesShadowOfSecrets() : base("Gazer Eyes, Shadow of Secrets", 4, 3000, Subtype.Ghost, Civilization.Darkness)
+        {
+            AddSilentSkillAbility(new OneShotEffects.LookAtYourOpponentsHandAndChooseCardFromItYourOpponentDiscardsThatCardEffect());
+        }
+    }
+}
