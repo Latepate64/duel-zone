088e5f954bc227142367b03c7c3e114639ed34ed
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 16:56:48 +0300

    Necrodragon Bryzenaga

---
+++ Cards/Cards/DM10/NecrodragonBryzenaga.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM10
+{
+    class NecrodragonBryzenaga : Creature
+    {
+        public NecrodragonBryzenaga() : base("Necrodragon Bryzenaga", 6, 9000, Subtype.ZombieDragon, Civilization.Darkness)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new NecrodragonBryzenagaEffect());
+            AddDoubleBreakerAbility();
+        }
+    }
+
+    class NecrodragonBryzenagaEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            game.PutFromShieldZoneToHand(source.GetController(game).ShieldZone.Cards, true);
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new NecrodragonBryzenagaEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Put all your shields into your hand.";
+        }
+    }
+}
