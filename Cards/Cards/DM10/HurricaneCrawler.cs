8ea7b8bd86da89f8ae9a2c6ff40c4d09cd212cc4
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 15:26:58 +0300

    Hurricane Crawler

---
+++ Cards/Cards/DM10/HurricaneCrawler.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM10
+{
+    class HurricaneCrawler : Creature
+    {
+        public HurricaneCrawler() : base("Hurricane Crawler", 5, 4000, Subtype.EarthEater, Civilization.Water)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new HurricaneCrawlerEffect());
+        }
+    }
+
+    class HurricaneCrawlerEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            var hand = source.GetController(game).Hand.Cards;
+            var amount = hand.Count;
+            game.Move(ZoneType.Hand, ZoneType.ManaZone, hand.ToArray());
+            return new OneShotEffects.ReturnCardsFromYourManaZoneToYourHandEffect(amount).Apply(game, source);
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new HurricaneCrawlerEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Put all the cards from your hand into your mana zone. Then put that many cards from your mana zone into your hand.";
+        }
+    }
+}
