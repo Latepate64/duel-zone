7ee2d543a61851f535fabb1509dc7f3e3b6d6984
Latepate64 <lauri.koriseva@gmail.com>
2022-04-02 16:49:52 +0300

    Mummy Wrap, Shadow of Fatigue

---
+++ Cards/Cards/DM10/MummyWrapShadowOfFatigue.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM10
+{
+    class MummyWrapShadowOfFatigue : Creature
+    {
+        public MummyWrapShadowOfFatigue() : base("Mummy Wrap, Shadow of Fatigue", 3, 1000, Subtype.Ghost, Civilization.Darkness)
+        {
+            AddTapAbility(new MummyWrapShadowOfFatigueEffect());
+        }
+    }
+
+    class MummyWrapShadowOfFatigueEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            game.Players.ToList().ForEach(x => x.DiscardAtRandom(game, 1));
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new MummyWrapShadowOfFatigueEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Each player discards a card at random from his hand.";
+        }
+    }
+}
