5ad037ce743282671702232148d804267194a6b0
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 11:23:09 +0300

    Yuliana, Channeler of Suns

---
+++ Cards/Cards/DM11/YulianaChannelerOfSuns.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class YulianaChannelerOfSuns : Creature
+    {
+        public YulianaChannelerOfSuns() : base("Yuliana, Channeler of Suns", 3, 3000, Subtype.MechaDelSol, Civilization.Light)
+        {
+            AddBlockerAbility();
+            AddThisCreatureCannotAttackPlayersAbility();
+            AddStaticAbilities(new ContinuousEffects.OpponentCannotChooseThisCreatureEffect());
+        }
+    }
+}
