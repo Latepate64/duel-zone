ab81278892053d12841e9b38e9bc07565379a6bd
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 13:54:26 +0300

    Peppi Pepper

---
+++ Cards/Cards/DM12/PeppiPepper.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM12
+{
+    class PeppiPepper : Creature
+    {
+        public PeppiPepper() : base("Peppi Pepper", 3, 2000, Subtype.FireBird, Civilization.Fire)
+        {
+            AddPowerAttackerAbility(3000);
+        }
+    }
+}
