4042fdb1f952b221c17fbb2979b614362f977fcc
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 11:04:37 +0300

    Skyscraper Shell, Solar Trap

---
+++ Cards/Cards/DM11/SkyscraperShell.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class SkyscraperShell : WaveStrikerCreature
+    {
+        public SkyscraperShell() : base("Skyscraper Shell", 4, 2000, Subtype.ColonyBeetle, Civilization.Nature)
+        {
+            AddWaveStrikerAbility(new TriggeredAbilities.WhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.YourOpponentChoosesOneOfHisCreaturesInTheBattleZoneAndPutsItIntoHisManaZoneEffect()));
+        }
+    }
+}
---
+++ Cards/Cards/DM11/SolarTrap.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class SolarTrap : Spell
+    {
+        public SolarTrap() : base("Solar Trap", 1, Civilization.Light)
+        {
+            AddSpellAbilities(new OneShotEffects.ChooseOneOfYourOpponentsCreaturesInTheBattleZoneAndTapItEffect());
+        }
+    }
+}
