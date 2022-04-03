d501bc6b40b88876c79a676fc74de6d1ac2b137e
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 13:50:37 +0300

    Necrodragon Jagraveen

---
+++ Cards/Cards/DM12/NecrodragonJagraveen.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Cards.Cards.DM12
+{
+    class NecrodragonJagraveen : Creature
+    {
+        public NecrodragonJagraveen() : base("Necrodragon Jagraveen", 6, 6000, Subtype.ZombieDragon, Civilization.Darkness)
+        {
+            AddBlockerAbility();
+            AddTriggeredAbility(new TriggeredAbilities.WheneverThisCreatureBlocksAbility(new OneShotEffects.DestroyAfterBattleEffect()));
+            AddDoubleBreakerAbility();
+        }
+    }
+}
