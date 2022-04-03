d6ba936dc16ae5a3e0baef33bcc01afe31614cc3
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 14:11:11 +0300

    Spectral Horn Glitalis

---
+++ Cards/Cards/DM12/SpectralHornGlitalis.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace Cards.Cards.DM12
+{
+    class SpectralHornGlitalis : Creature
+    {
+        public SpectralHornGlitalis() : base("Spectral Horn Glitalis", 3, 4000, Subtype.HornedBeast, Subtype.RainbowPhantom, Civilization.Light, Civilization.Nature)
+        {
+        }
+    }
+}
