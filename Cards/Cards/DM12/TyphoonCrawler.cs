bd25415f49423acadbfda21a2abed1569d763976
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 14:17:48 +0300

    Typhoon Crawler

---
+++ Cards/Cards/DM12/TyphoonCrawler.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM12
+{
+    class TyphoonCrawler : Creature
+    {
+        public TyphoonCrawler() : base("Typhoon Crawler", 6, 5000, Subtype.EarthEater, Civilization.Water)
+        {
+            AddStaticAbilities(new ContinuousEffects.ThisCreatureCannotBeAttackedByCivilizationCreaturesEffect(Civilization.Fire, Civilization.Nature));
+        }
+    }
+}
