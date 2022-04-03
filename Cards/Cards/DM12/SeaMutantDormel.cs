1dff591d0495fb63d6807a93e8d7abf4c3b8383a
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 14:09:38 +0300

    Sea Mutant Dormel

---
+++ Cards/Cards/DM12/SeaMutantDormel.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace Cards.Cards.DM12
+{
+    class SeaMutantDormel : Creature
+    {
+        public SeaMutantDormel() : base("Sea Mutant Dormel", 3, 4000, Subtype.Merfolk, Subtype.Hedrian, Civilization.Water, Civilization.Darkness)
+        {
+        }
+    }
+}
