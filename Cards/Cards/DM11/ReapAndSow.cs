20c1864ce4e38f6706bd6bd32fde524ac410b057
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 09:51:25 +0300

    Reap and Sow

---
+++ Cards/Cards/DM11/ReapAndSow.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM11
+{
+    class ReapAndSow : Spell
+    {
+        public ReapAndSow() : base("Reap and Sow", 5, Civilization.Fire, Civilization.Nature)
+        {
+            AddSpellAbilities(new ReapAndSowEffect());
+        }
+    }
+
+    class ReapAndSowEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            new OneShotEffects.ChooseCardInYourOpponentsManaZoneAndPutItIntoHisGraveyardEffect().Apply(game, source);
+            source.GetController(game).PutFromTopOfDeckIntoManaZone(game, 1);
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new ReapAndSowEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Choose a card in your opponent's mana zone and put it into his graveyard. Then put the top card of your deck into your mana zone.";
+        }
+    }
+}
