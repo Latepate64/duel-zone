e91bf35324e1a5d6be33176a2aa04e8e28358363
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 08:02:14 +0300

    Macho Melon

---
+++ Cards/Cards/DM11/MachoMelon.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class MachoMelon : WaveStrikerCreature
+    {
+        public MachoMelon() : base("Macho Melon", 2, 1000, Subtype.WildVeggies, Civilization.Nature)
+        {
+            AddWaveStrikerAbility(new ContinuousEffects.PowerAttackerEffect(3000));
+        }
+    }
+}
