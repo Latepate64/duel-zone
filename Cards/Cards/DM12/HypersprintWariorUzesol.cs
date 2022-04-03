14689146703b7ec2842bc93dde544dd5a6761c7e
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 13:34:44 +0300

    Hypersprint Warior Uzesol

---
+++ Cards/Cards/DM12/HypersprintWariorUzesol.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM12
+{
+    class HypersprintWariorUzesol : Creature
+    {
+        public HypersprintWariorUzesol() : base("Hypersprint Warior Uzesol", 4, 1000, Subtype.Armorloid, Civilization.Fire)
+        {
+            AddSpeedAttackerAbility();
+            AddPowerAttackerAbility(4000);
+        }
+    }
+}
