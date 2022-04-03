64ab3ee253bd1c988f13e8e85d46dbf8a596bf1c
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 13:59:32 +0300

    Phantomach, the Gigatrooper

---
+++ Cards/Cards/DM12/PhantomachTheGigatrooper.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using Engine.ContinuousEffects;
+
+namespace Cards.Cards.DM12
+{
+    class PhantomachTheGigatrooper : EvolutionCreature
+    {
+        public PhantomachTheGigatrooper() : base("Phantomach, the Gigatrooper", 5, 6000, Subtype.Chimera, Subtype.Armorloid, Civilization.Darkness, Civilization.Fire)
+        {
+            AddStaticAbilities(new PhantomachPowerEffect(), new PhantomachDoubleBreakerEffect());
+        }
+    }
+
+    class PhantomachPowerEffect : PowerModifyingEffect
+    {
+        public PhantomachPowerEffect() : base(2000, new CardFilters.OwnersBattleZoneSubtypeCreatureExceptFilter(Subtype.Chimera, Subtype.Armorloid), new Durations.Indefinite())
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new PhantomachPowerEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Each of your other Chimeras and Armorloids in the battle zone gain +2000 power.";
+        }
+    }
+
+    class PhantomachDoubleBreakerEffect : AbilityAddingEffect
+    {
+        public PhantomachDoubleBreakerEffect() : base(new CardFilters.OwnersBattleZoneSubtypeCreatureFilter(Subtype.Chimera, Subtype.Armorloid), new Durations.Indefinite(), new StaticAbilities.DoubleBreakerAbility())
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new PhantomachDoubleBreakerEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Each of your Chimeras and Armorloids in the battle zone has \"double breaker.\"";
+        }
+    }
+}
