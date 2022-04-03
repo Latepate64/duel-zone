06f329f303e7c362337fdf13a82fc4cffad11e8b
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 15:20:08 +0300

    Velyrika Dragon

---
+++ Cards/Cards/Promo/VelyrikaDragon.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.Promo
+{
+    class VelyrikaDragon : Creature
+    {
+        public VelyrikaDragon() : base("Velyrika Dragon", 7, 7000, Subtype.ArmoredDragon, Civilization.Fire)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new OneShotEffects.SearchSubtypeCreatureEffect(Subtype.ArmoredDragon));
+            AddDoubleBreakerAbility();
+        }
+    }
+}
