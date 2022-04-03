1f31be9613e1226df079fd0a2655afafddd1e622
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 07:49:42 +0300

    Hysteria Lizard

---
+++ Cards/Cards/DM11/HysteriaLizard.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Cards.Cards.DM11
+{
+    class HysteriaLizard : Creature
+    {
+        public HysteriaLizard() : base("Hysteria Lizard", 4, 3000, Subtype.MeltWarrior, Civilization.Fire)
+        {
+            AddStaticAbilities(new ContinuousEffects.ThisCreatureAttacksEachTurnIfAbleEffect());
+            AddPowerAttackerAbility(3000);
+        }
+    }
+}
