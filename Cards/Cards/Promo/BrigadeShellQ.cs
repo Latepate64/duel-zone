ba95a8d79ba8a2b9f41aeff59d5ff8b720263c07
Latepate64 <lauri.koriseva@gmail.com>
2022-04-03 15:34:25 +0300

    Brigade Shell Q

---
+++ Cards/Cards/Promo/BrigadeShellQ.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.Promo
+{
+    class BrigadeShellQ : Creature
+    {
+        public BrigadeShellQ() : base("Brigade Shell Q", 3, 1000, Civilization.Nature)
+        {
+            AddSubtypes(Subtype.Survivor, Subtype.ColonyBeetle);
+            AddSurvivorAbility(new TriggeredAbilities.WheneverThisCreatureAttacksAbility(new BrigadeShellQEffect()));
+        }
+    }
+
+    class BrigadeShellQEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            var cards = source.GetController(game).RevealTopCardsOfDeck(1, game).ToArray();
+            if (cards.Length == 1)
+            {
+                if (cards.Single().HasSubtype(Subtype.Survivor))
+                {
+                    game.Move(ZoneType.Deck, ZoneType.Hand, cards);
+                }
+                else
+                {
+                    game.Move(ZoneType.Deck, ZoneType.Graveyard, cards);
+                }
+            }
+            source.GetController(game).Unreveal(cards);
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new BrigadeShellQEffect();
+        }
+
+        public override string ToString()
+        {
+            return "Reveal the top card of your deck. If it's a Survivor, put it into your hand. Otherwise, put it into your graveyard.";
+        }
+    }
+}
