75c824832ee8a114a3e01e9404ef2586213d1907
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-09 16:55:55 +0300

    Olgate, Nightmare Samurai

---
+++ Cards/Cards/Promo/OlgateNightmareSamurai.cs
@@ -0,0 +1,42 @@
+﻿using Cards.TriggeredAbilities;
+using Common;
+using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.Promo
+{
+    class OlgateNightmareSamurai : Creature
+    {
+        public OlgateNightmareSamurai() : base("Olgate, Nightmare Samurai", 7, 6000, Subtype.DemonCommand, Civilization.Darkness)
+        {
+            AddDoubleBreakerAbility();
+            AddTriggeredAbility(new OlgateAbility());
+        }
+    }
+
+    class OlgateAbility : DestroyedAbility
+    {
+        public OlgateAbility() : base(new OneShotEffects.YouMayUntapThisCreatureEffect())
+        {
+        }
+
+        public OlgateAbility(OlgateAbility ability) : base(ability)
+        {
+        }
+
+        public override IAbility Copy()
+        {
+            return new OlgateAbility();
+        }
+
+        public override string ToString()
+        {
+            return "Whenever one of your creatures is destroyed, you may untap this creature.";
+        }
+
+        protected override bool TriggersFrom(Engine.ICard card, IGame game)
+        {
+            return card.Owner == Controller && card.Id != Source;
+        }
+    }
+}
