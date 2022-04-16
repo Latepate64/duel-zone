8fa4088b88684c17759dbf020c79cd2408f04da4
Latepate64 <lauri.koriseva@gmail.com>
2022-04-16 14:27:37 +0300

    Cliffcrush Giant

---
+++ Cards/Cards/DM06/CliffcrushGiant.cs
@@ -0,0 +1,42 @@
+﻿using Cards.ContinuousEffects;
+using Engine;
+using Engine.ContinuousEffects;
+using System.Linq;
+
+namespace Cards.Cards.DM06
+{
+    class CliffcrushGiant : Creature
+    {
+        public CliffcrushGiant() : base("Cliffcrush Giant", 5, 7000, Race.Giant, Civilization.Nature)
+        {
+            AddStaticAbilities(new CliffcrushGiantEffect());
+            AddDoubleBreakerAbility();
+        }
+    }
+
+    class CliffcrushGiantEffect : ContinuousEffect, ICannotAttackEffect
+    {
+        public CliffcrushGiantEffect()
+        {
+        }
+
+        public CliffcrushGiantEffect(CliffcrushGiantEffect effect) : base(effect)
+        {
+        }
+
+        public bool Applies(ICard creature, IGame game)
+        {
+            return IsSourceOfAbility(creature, game) && game.BattleZone.GetOtherUntappedCreatures(GetController(game).Id, GetSourceCard(game).Id).Any();
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new CliffcrushGiantEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "While you have any other untapped creatures in the battle zone, this creature can't attack.";
+        }
+    }
+}
