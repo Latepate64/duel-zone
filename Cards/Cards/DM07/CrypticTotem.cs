8eed3635f17778c0d4e48ac329b9b80bd8309767
Latepate64 <lauri.koriseva@gmail.com>
2022-04-16 12:33:24 +0300

    Cryptic Totem

---
+++ Cards/Cards/DM07/CrypticTotem.cs
@@ -0,0 +1,41 @@
+﻿using Cards.ContinuousEffects;
+using Engine;
+using Engine.ContinuousEffects;
+
+namespace Cards.Cards.DM07
+{
+    class CrypticTotem : Creature
+    {
+        public CrypticTotem() : base("Cryptic Totem", 6, 6000, Race.MysteryTotem, Civilization.Nature)
+        {
+            AddDoubleBreakerAbility();
+            AddStaticAbilities(new CrypticTotemEffect());
+        }
+    }
+
+    class CrypticTotemEffect : ContinuousEffect, ICannotUseShieldTriggerEffect
+    {
+        public CrypticTotemEffect()
+        {
+        }
+
+        public CrypticTotemEffect(CrypticTotemEffect effect) : base(effect)
+        {
+        }
+
+        public bool Applies(ICard shield, IGame game)
+        {
+            return GetSourceCard(game).Tapped && game.GetOpponent(GetController(game)).Id == shield.Id;
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new CrypticTotemEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "While this creature is tapped, your opponent can't use the \"shield trigger\" ability of his shields.";
+        }
+    }
+}
