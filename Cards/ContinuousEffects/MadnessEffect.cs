8b4cc1f56fba42f60e6de91be29d764b6f7d0e3b
Latepate64 <lauri.koriseva@gmail.com>
2022-04-16 11:49:55 +0300

    MadnessEffect

---
+++ Cards/ContinuousEffects/MadnessEffect.cs
@@ -0,0 +1,58 @@
+﻿using Engine;
+using Engine.ContinuousEffects;
+using Engine.GameEvents;
+
+namespace Cards.ContinuousEffects
+{
+    abstract class MadnessEffect : ReplacementEffect
+    {
+        protected MadnessEffect()
+        {
+        }
+
+        protected MadnessEffect(MadnessEffect effect) : base(effect)
+        {
+        }
+
+        public override bool CanBeApplied(IGameEvent gameEvent, IGame game)
+        {
+            return gameEvent is CardMovedEvent e && e.Source == ZoneType.Hand && e.Destination == ZoneType.Graveyard && GetSourceCard(game).Id == e.CardInSourceZone;
+        }
+    }
+
+    class OptionalMadnessEffect : MadnessEffect
+    {
+        public OptionalMadnessEffect()
+        {
+        }
+
+        public OptionalMadnessEffect(OptionalMadnessEffect effect) : base(effect)
+        {
+        }
+
+        public override IGameEvent Apply(IGameEvent gameEvent, IGame game)
+        {
+            if (GetController(game).ChooseToTakeAction(ToString()))
+            {
+                return new CardMovedEvent(gameEvent as ICardMovedEvent)
+                {
+                    Destination = ZoneType.BattleZone
+                };
+            }
+            else
+            {
+                return gameEvent;
+            }
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new OptionalMadnessEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "When this creature would be discarded from your hand during your opponent's turn, you may put it into the battle zone instead.";
+        }
+    }
+}
