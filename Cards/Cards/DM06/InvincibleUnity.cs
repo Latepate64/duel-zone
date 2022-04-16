1570435c38095d398f34aa3b1b6a8389a6eee5a3
Latepate64 <lauri.koriseva@gmail.com>
2022-04-16 17:29:11 +0300

    Invincible Unity

---
+++ Cards/Cards/DM06/InvincibleUnity.cs
@@ -0,0 +1,80 @@
+﻿using Cards.ContinuousEffects;
+using Cards.StaticAbilities;
+using Engine;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cards.Cards.DM06
+{
+    class InvincibleUnity : Spell
+    {
+        public InvincibleUnity() : base("Invincible Unity", 13, Civilization.Nature)
+        {
+            AddSpellAbilities(new InvincibleUnityOneShotEffect());
+        }
+    }
+
+    class InvincibleUnityOneShotEffect : OneShotEffect
+    {
+        public InvincibleUnityOneShotEffect()
+        {
+        }
+
+        public InvincibleUnityOneShotEffect(InvincibleUnityOneShotEffect effect) : base(effect)
+        {
+        }
+
+        public override object Apply(IGame game, IAbility source)
+        {
+            game.AddContinuousEffects(source, new InvincibleUnityContinuousEffect(game.BattleZone.GetCreatures(source.Controller)));
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new InvincibleUnityOneShotEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "Each of your creatures in the battle zone gets +8000 power and \"triple breaker\" until the end of the turn.";
+        }
+    }
+
+    class InvincibleUnityContinuousEffect : UntilEndOfTurnEffect, IAbilityAddingEffect, IPowerModifyingEffect
+    {
+        private readonly List<ICard> _cards;
+
+        public InvincibleUnityContinuousEffect(IEnumerable<ICard> cards)
+        {
+            _cards = cards.ToList();
+        }
+
+        public InvincibleUnityContinuousEffect(InvincibleUnityContinuousEffect effect) : base(effect)
+        {
+            _cards = effect._cards;
+        }
+
+        public void AddAbility(IGame game)
+        {
+            _cards.ForEach(x => x.AddGrantedAbility(new TripleBreakerAbility()));
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new InvincibleUnityContinuousEffect(this);
+        }
+
+        public void ModifyPower(IGame game)
+        {
+            _cards.ForEach(x => x.Power += 8000);
+        }
+
+        public override string ToString()
+        {
+            return $"{_cards} get +8000 power and \"triple breaker\" until the end of the turn.";
+        }
+    }
+}
---
+++ Cards/StaticAbilities/TripleBreakerAbility.cs
@@ -0,0 +1,12 @@
+﻿using Cards.ContinuousEffects;
+using Engine.Abilities;
+
+namespace Cards.StaticAbilities
+{
+    class TripleBreakerAbility : StaticAbility
+    {
+        public TripleBreakerAbility() : base(new TripleBreakerEffect())
+        {
+        }
+    }
+}
