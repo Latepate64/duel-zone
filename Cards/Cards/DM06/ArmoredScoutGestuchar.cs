ee3ae68affaf659e7ab89335c09c63d6d4318896
Latepate64 <lauri.koriseva@gmail.com>
2022-04-16 13:41:28 +0300

    Armored Scout Gestuchar

---
+++ Cards/Cards/DM06/ArmoredScoutGestuchar.cs
@@ -0,0 +1,49 @@
+﻿using Cards.ContinuousEffects;
+using Cards.StaticAbilities;
+using Engine;
+using Engine.Abilities;
+using Engine.ContinuousEffects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cards.Cards.DM06
+{
+    class ArmoredScoutGestuchar : Creature
+    {
+        public ArmoredScoutGestuchar() : base("Armored Scout Gestuchar", 5, 4000, Race.Armorloid, Civilization.Fire)
+        {
+            AddStaticAbilities(new ArmoredScoutGestucharEffect());
+        }
+    }
+
+    class ArmoredScoutGestucharEffect : ContinuousEffect, IAbilityAddingEffect
+    {
+        public ArmoredScoutGestucharEffect()
+        {
+        }
+
+        public ArmoredScoutGestucharEffect(ArmoredScoutGestucharEffect effect) : base(effect)
+        {
+        }
+
+        public void AddAbility(IGame game)
+        {
+            var creature = GetSourceCard(game);
+            if (!game.BattleZone.GetOtherCreatures(GetController(game).Id, creature.Id, Civilization.Fire).Any())
+            {
+                creature.AddGrantedAbility(new PowerAttackerAbility(3000));
+                creature.AddGrantedAbility(new DoubleBreakerAbility());
+            }
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new ArmoredScoutGestucharEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "While you have no other fire creatures in the battle zone, this creature has \"power attacker +3000\" and \"double breaker.\"";
+        }
+    }
+}
