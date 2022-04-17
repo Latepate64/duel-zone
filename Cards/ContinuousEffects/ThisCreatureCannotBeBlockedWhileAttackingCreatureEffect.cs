5afcdf8aa18ec6cd0bee218150392a98119aa409
Latepate64 <lauri.koriseva@gmail.com>
2022-04-17 18:19:43 +0300

    ThisCreatureCannotBeBlockedWhileAttackingCreatureEffect

---
+++ Cards/ContinuousEffects/ThisCreatureCannotBeBlockedWhileAttackingCreatureEffect.cs
@@ -0,0 +1,32 @@
+﻿using Engine;
+using Engine.ContinuousEffects;
+using Engine.Steps;
+
+namespace Cards.ContinuousEffects
+{
+    class ThisCreatureCannotBeBlockedWhileAttackingCreatureEffect : ContinuousEffect, IUnblockableEffect
+    {
+        public ThisCreatureCannotBeBlockedWhileAttackingCreatureEffect()
+        {
+        }
+
+        public ThisCreatureCannotBeBlockedWhileAttackingCreatureEffect(ThisCreatureCannotBeBlockedWhileAttackingCreatureEffect effect) : base(effect)
+        {
+        }
+
+        public bool CannotBeBlocked(ICard attacker, ICard blocker, IGame game)
+        {
+            return IsSourceOfAbility(attacker, game) && game.CurrentTurn.CurrentPhase is AttackPhase a && a.AttackTarget is ICard;
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new ThisCreatureCannotBeBlockedWhileAttackingCreatureEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "While attacking a creature, this creature can't be blocked.";
+        }
+    }
+}
