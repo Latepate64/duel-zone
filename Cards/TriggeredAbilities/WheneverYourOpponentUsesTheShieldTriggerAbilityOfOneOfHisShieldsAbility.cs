814d40611f08aeb5f77071a675b23de8a7a0b9ca
Latepate64 <lauri.koriseva@gmail.com>
2022-04-17 13:57:55 +0300

    Emperor Quazla

---
+++ Cards/TriggeredAbilities/WheneverYourOpponentUsesTheShieldTriggerAbilityOfOneOfHisShieldsAbility.cs
@@ -0,0 +1,32 @@
+﻿using Engine;
+using Engine.Abilities;
+using Engine.GameEvents;
+
+namespace Cards.TriggeredAbilities
+{
+    class WheneverYourOpponentUsesTheShieldTriggerAbilityOfOneOfHisShieldsAbility : TriggeredAbility
+    {
+        public WheneverYourOpponentUsesTheShieldTriggerAbilityOfOneOfHisShieldsAbility(IOneShotEffect effect) : base(effect)
+        {
+        }
+
+        public WheneverYourOpponentUsesTheShieldTriggerAbilityOfOneOfHisShieldsAbility(WheneverYourOpponentUsesTheShieldTriggerAbilityOfOneOfHisShieldsAbility ability) : base(ability)
+        {
+        }
+
+        public override bool CanTrigger(IGameEvent gameEvent, IGame game)
+        {
+            return gameEvent is ShieldTriggerEvent e && e.Player == GetOpponent(game);
+        }
+
+        public override IAbility Copy()
+        {
+            return new WheneverYourOpponentUsesTheShieldTriggerAbilityOfOneOfHisShieldsAbility(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Whenever your opponent uses the \"shield trigger\" ability of one of his shields, {GetEffectText()}";
+        }
+    }
+}
