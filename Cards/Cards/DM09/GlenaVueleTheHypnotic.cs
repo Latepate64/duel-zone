0944c3bdd42d93782bf731cb3abcf439df2b4b06
Latepate64 <lauri.koriseva@gmail.com>
2022-04-17 14:02:32 +0300

    Glena Vuele, the Hypnotic

---
+++ Cards/Cards/DM09/GlenaVueleTheHypnotic.cs
@@ -0,0 +1,36 @@
+﻿using Engine;
+using Engine.Abilities;
+
+namespace Cards.Cards.DM09
+{
+    class GlenaVueleTheHypnotic : EvolutionCreature
+    {
+        public GlenaVueleTheHypnotic() : base("Glena Vuele, the Hypnotic", 5, 8500, Race.Guardian, Civilization.Light)
+        {
+            AddDoubleBreakerAbility();
+            AddTriggeredAbility(new TriggeredAbilities.WheneverYourOpponentUsesTheShieldTriggerAbilityOfOneOfHisShieldsAbility(new GlenaVueleTheHypnoticEffect()));
+        }
+    }
+
+    class GlenaVueleTheHypnoticEffect : OneShotEffect
+    {
+        public override object Apply(IGame game, IAbility source)
+        {
+            if (source.GetController(game).ChooseToTakeAction(ToString()))
+            {
+                source.GetController(game).PutFromTopOfDeckIntoShieldZone(1, game, source);
+            }
+            return null;
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new GlenaVueleTheHypnoticEffect();
+        }
+
+        public override string ToString()
+        {
+            return "You may add the top card of your deck to your shields face down.";
+        }
+    }
+}
