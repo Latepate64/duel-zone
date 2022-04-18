64c36fd1346a8ed41930c0c20c97792b1050f7e0
Latepate64 <lauri.koriseva@gmail.com>
2022-04-18 07:56:16 +0300

    Elixia, Pureblade Elemental

---
+++ Cards/Cards/DM10/ElixiaPurebladeElemental.cs
@@ -0,0 +1,41 @@
+﻿using Cards.ContinuousEffects;
+using Engine;
+using Engine.ContinuousEffects;
+using System.Linq;
+
+namespace Cards.Cards.DM10
+{
+    class ElixiaPurebladeElemental : Creature
+    {
+        public ElixiaPurebladeElemental() : base("Elixia, Pureblade Elemental", 6, 1000, Race.AngelCommand, Civilization.Light)
+        {
+            AddStaticAbilities(new ElixiaEffect(), new PoweredTripleBreaker());
+        }
+    }
+
+    class ElixiaEffect : PowerModifyingMultiplierEffect
+    {
+        public ElixiaEffect() : base(3000)
+        {
+        }
+
+        public ElixiaEffect(ElixiaEffect effect) : base(effect)
+        {
+        }
+
+        public override IContinuousEffect Copy()
+        {
+            return new ElixiaEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "This creature gets +3000 power for each civilization in your mana zone.";
+        }
+
+        protected override int GetMultiplier(IGame game)
+        {
+            return GetController(game).ManaZone.Cards.SelectMany(x => x.Civilizations).Distinct().Count();
+        }
+    }
+}
