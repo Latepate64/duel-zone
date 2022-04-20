723f23f508eefcffcd46266f3983e29979aee0e9
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-20 19:33:32 +0300

    RapidReincarnationEffect refactored

--- Cards/Cards/DM10/RapidReincarnation.cs
+++ Cards/Cards/DM10/RapidReincarnation.cs
@@ -1,6 +1,5 @@
 ﻿using Engine;
 using Engine.Abilities;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Cards.Cards.DM10
@@ -18,11 +17,13 @@ namespace Cards.Cards.DM10
     {
         public override void Apply(IGame game, IAbility source)
         {
-            var creature = source.GetController(game).ChooseControlledCreatureOptionally(game, ToString());
+            var controller = source.GetController(game);
+            var creature = controller.ChooseControlledCreatureOptionally(game, ToString());
             if (creature != null)
             {
                 game.Destroy(source, creature);
-                new RapidReincarnationSecondEffect().Apply(game, source);
+                var card = controller.ChooseCard(controller.Hand.Creatures.Where(x => x.ManaCost <= controller.ManaZone.Cards.Count), ToString());
+                game.Move(source, ZoneType.Hand, ZoneType.BattleZone, card);
             }
         }
 
@@ -34,28 +35,6 @@ namespace Cards.Cards.DM10
         public override string ToString()
         {
             return "You may destroy one of your creatures. If you do, choose a creature in your hand that costs the same as or less than the number of cards in your mana zone and put it into the battle zone.";
-        }
-    }
-
-    class RapidReincarnationSecondEffect : OneShotEffects.CardMovingChoiceEffect
-    {
-        public RapidReincarnationSecondEffect() : base(1, 1, true, ZoneType.Hand, ZoneType.BattleZone)
-        {
-        }
-
-        public override IOneShotEffect Copy()
-        {
-            return new RapidReincarnationSecondEffect();
-        }
-
-        public override string ToString()
-        {
-            return "Choose a creature in your hand that costs the same as or less than the number of cards in your mana zone and put it into the battle zone.";
-        }
-
-        protected override IEnumerable<ICard> GetSelectableCards(IGame game, IAbility source)
-        {
-            return source.GetController(game).Hand.Creatures.Where(x => x.ManaCost <= source.GetController(game).ManaZone.Cards.Count);
         }
     }
 }
