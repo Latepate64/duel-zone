3e87e53228348280ce97d8efa5c5daab4bdbda25
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-20 19:15:06 +0300

    ShockHurricaneEffect refactored

--- Cards/Cards/DM06/ShockHurricane.cs
+++ Cards/Cards/DM06/ShockHurricane.cs
@@ -1,7 +1,5 @@
-﻿using Cards.OneShotEffects;
-using Engine;
+﻿using Engine;
 using Engine.Abilities;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Cards.Cards.DM06
@@ -23,10 +21,10 @@ namespace Cards.Cards.DM06
             var choosableAmount = game.BattleZone.GetChoosableCreaturesControlledByPlayer(game, source.GetOpponent(game).Id).Count();
             if (amount > 0 && amount <= choosableAmount)
             {
-                var effect = new ChooseOpponentsCreaturesAndReturnThemToHisHandEffect(amount);
-                if (source.GetController(game).ChooseToTakeAction($"You may {effect.ToString().ToLower()}"))
+                if (source.GetController(game).ChooseToTakeAction($"You may choose {amount} of your opponent's creatures in the battle zone and return them to your opponent's hand."))
                 {
-                    effect.Apply(game, source);
+                    var creatures = player.ChooseCards(game.BattleZone.GetChoosableCreaturesControlledByPlayer(game, source.GetOpponent(game).Id), amount, amount, ToString());
+                    game.Move(source, ZoneType.BattleZone, ZoneType.Hand, creatures.ToArray());
                 }
             }
         }
@@ -39,36 +37,6 @@ namespace Cards.Cards.DM06
         public override string ToString()
         {
             return "Return any number of your creatures from the battle zone to your hand. Then you may choose that many of your opponent's creatures in the battle zone and return them to your opponent's hand.";
-        }
-    }
-
-    class ChooseOpponentsCreaturesAndReturnThemToHisHandEffect : BounceEffect
-    {
-        private readonly int _amount;
-
-        public ChooseOpponentsCreaturesAndReturnThemToHisHandEffect(ChooseOpponentsCreaturesAndReturnThemToHisHandEffect effect) : base(effect)
-        {
-            _amount = effect._amount;
-        }
-
-        public ChooseOpponentsCreaturesAndReturnThemToHisHandEffect(int amount) : base(amount, amount)
-        {
-            _amount = amount;
-        }
-
-        public override IOneShotEffect Copy()
-        {
-            return new ChooseOpponentsCreaturesAndReturnThemToHisHandEffect(this);
-        }
-
-        public override string ToString()
-        {
-            return $"Choose {_amount} of your opponent's creatures in the battle zone and return them to your opponent's hand.";
-        }
-
-        protected override IEnumerable<ICard> GetSelectableCards(IGame game, IAbility source)
-        {
-            return game.BattleZone.GetChoosableCreaturesControlledByPlayer(game, source.GetOpponent(game).Id);
         }
     }
 }
