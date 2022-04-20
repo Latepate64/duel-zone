07993fada1224e659cdb5b5030da4350e87e7a7a
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-20 20:17:35 +0300

    IntenseEvilEffect refactored

--- Cards/Cards/DM06/IntenseEvil.cs
+++ Cards/Cards/DM06/IntenseEvil.cs
@@ -1,6 +1,5 @@
 ﻿using Engine;
 using Engine.Abilities;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Cards.Cards.DM06
@@ -19,8 +18,9 @@ namespace Cards.Cards.DM06
         public override void Apply(IGame game, IAbility source)
         {
             var player = source.GetController(game);
-            var cards = player.ChooseAnyNumberOfCards(game.BattleZone.GetCreatures(player.Id), ToString());
-            new OneShotEffects.DrawCardsEffect(cards.Count()).Apply(game, source);
+            var cards = player.ChooseAnyNumberOfCards(game.BattleZone.GetCreatures(player.Id), ToString()).ToArray();
+            game.Move(source, ZoneType.BattleZone, ZoneType.Graveyard, cards);
+            player.DrawCards(cards.Length, game, source);
         }
 
         public override IOneShotEffect Copy()
@@ -31,33 +31,6 @@ namespace Cards.Cards.DM06
         public override string ToString()
         {
             return "Destroy any number of your creatures. Then draw that many cards.";
-        }
-    }
-
-    class DestroyAnyNumberOfYourCreatures : OneShotEffects.ChooseAnyNumberOfCardsEffect
-    {
-        public DestroyAnyNumberOfYourCreatures() : base()
-        {
-        }
-
-        public override IOneShotEffect Copy()
-        {
-            return new DestroyAnyNumberOfYourCreatures();
-        }
-
-        public override string ToString()
-        {
-            return "Destroy any number of your creatures.";
-        }
-
-        protected override void Apply(IGame game, IAbility source, params ICard[] cards)
-        {
-            game.Move(source, ZoneType.BattleZone, ZoneType.Graveyard, cards);
-        }
-
-        protected override IEnumerable<ICard> GetAffectedCards(IGame game, IAbility source)
-        {
-            return game.BattleZone.GetCreatures(source.Controller);
         }
     }
 }
