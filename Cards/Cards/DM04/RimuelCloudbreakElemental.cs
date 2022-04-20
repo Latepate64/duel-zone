d535c036a95e4d23095a8f3efbf1a1c5e2f79dbc
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-20 18:26:52 +0300

    RimuelCloudbreakElementalEffect refactored

--- Cards/Cards/DM04/RimuelCloudbreakElemental.cs
+++ Cards/Cards/DM04/RimuelCloudbreakElemental.cs
@@ -1,7 +1,5 @@
-﻿using Cards.OneShotEffects;
-using Engine;
+﻿using Engine;
 using Engine.Abilities;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Cards.Cards.DM04
@@ -19,8 +17,10 @@ namespace Cards.Cards.DM04
     {
         public override void Apply(IGame game, IAbility source)
         {
-            var amount = source.GetController(game).ManaZone.UntappedCards.Count(x => x.HasCivilization(Civilization.Light));
-            new RimuelCloudbreakElementalTapEffect(amount).Apply(game, source);
+            var controller = source.GetController(game);
+            var amount = controller.ManaZone.UntappedCards.Count(x => x.HasCivilization(Civilization.Light));
+            var creatures = controller.ChooseCards(game.BattleZone.GetChoosableCreaturesControlledByPlayer(game, source.GetOpponent(game).Id), amount, amount, ToString());
+            controller.Tap(game, creatures.ToArray());
         }
 
         public override IOneShotEffect Copy()
@@ -31,36 +31,6 @@ namespace Cards.Cards.DM04
         public override string ToString()
         {
             return "Tap one of your opponent's creatures in the battle zone for each untapped light card in your mana zone.";
-        }
-    }
-
-    class RimuelCloudbreakElementalTapEffect : TapChoiceEffect
-    {
-        private readonly int _amount;
-
-        public RimuelCloudbreakElementalTapEffect(int amount) : base(amount, amount, true)
-        {
-            _amount = amount;
-        }
-
-        public RimuelCloudbreakElementalTapEffect(RimuelCloudbreakElementalTapEffect effect) : base(effect)
-        {
-            _amount = effect._amount;
-        }
-
-        public override IOneShotEffect Copy()
-        {
-            return new RimuelCloudbreakElementalTapEffect(this);
-        }
-
-        public override string ToString()
-        {
-            return $"Tap {_amount} of your opponent's creatures.";
-        }
-
-        protected override IEnumerable<ICard> GetSelectableCards(IGame game, IAbility source)
-        {
-            return game.BattleZone.GetChoosableCreaturesControlledByPlayer(game, source.GetOpponent(game).Id);
         }
     }
 }
