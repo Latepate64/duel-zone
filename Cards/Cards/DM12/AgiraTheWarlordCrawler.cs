a0d3b390f8a29b80833148337def9549959ee7c7
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-23 19:53:27 +0300

    Agira, the Warlord Crawler

---
+++ Cards/Cards/DM12/AgiraTheWarlordCrawler.cs
@@ -0,0 +1,42 @@
+﻿using Cards.ContinuousEffects;
+using Engine;
+using Engine.Abilities;
+using Engine.GameEvents;
+
+namespace Cards.Cards.DM12
+{
+    class AgiraTheWarlordCrawler : EvolutionCreature
+    {
+        public AgiraTheWarlordCrawler() : base("Agira, the Warlord Crawler", 4, 5500, Race.Gladiator, Race.EarthEater, Civilization.Light, Civilization.Water)
+        {
+            AddStaticAbilities(new EachOfYourOtherRacesGetsPowerEffect(Race.Gladiator, Race.EarthEater));
+            AddTriggeredAbility(new AgiraAbility());
+        }
+    }
+
+    class AgiraAbility : TriggeredAbility
+    {
+        public AgiraAbility() : base(new OneShotEffects.YouMayDrawCardEffect())
+        {
+        }
+
+        public AgiraAbility(TriggeredAbility ability) : base(ability)
+        {
+        }
+
+        public override bool CanTrigger(IGameEvent gameEvent, IGame game)
+        {
+            return gameEvent is BecomeBlockedEvent e && e.Blocker.Owner == Controller && (e.Blocker.HasRace(Race.Gladiator) || e.Blocker.HasRace(Race.EarthEater));
+        }
+
+        public override IAbility Copy()
+        {
+            return new AgiraAbility(this);
+        }
+
+        public override string ToString()
+        {
+            return "Whenever one of your Gladiators or Earth Eaters blocks, you may draw a card.";
+        }
+    }
+}
