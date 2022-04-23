ec1848f32f99fe0d5d89563c7674783ada756a7b
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-23 17:02:31 +0300

    Ularus, Punishment Elemental

---
+++ Cards/Cards/DM12/UlarusPunishmentElemental.cs
@@ -0,0 +1,43 @@
+﻿using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM12
+{
+    class UlarusPunishmentElemental : Creature
+    {
+        public UlarusPunishmentElemental() : base("Ularus, Punishment Elemental", 5, 4500, Race.AngelCommand, Civilization.Light)
+        {
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new UlarusEffect());
+        }
+    }
+
+    class UlarusEffect : OneShotEffect
+    {
+        public UlarusEffect()
+        {
+        }
+
+        public UlarusEffect(IOneShotEffect effect) : base(effect)
+        {
+        }
+
+        public override void Apply(IGame game)
+        {
+            var maximum = game.BattleZone.GetCreatures(Controller.Id).Count();
+            var shields = Controller.ShieldZone.Cards.Union(GetOpponent(game).ShieldZone.Cards);
+            var chosen = Controller.ChooseCards(shields, 0, maximum, ToString()).ToList();
+            chosen.ForEach(x => x.FaceDown = false);
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new UlarusEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "For each creature you have in the battle zone, you may choose a shield and turn it face up.";
+        }
+    }
+}
