3b15794d75e78c7b0d8d9b0d0331105ebe0cbd3c
Lauri Koriseva <lauri.koriseva@hotmail.fi>
2022-04-23 17:12:07 +0300

    Royal Durian

---
+++ Cards/Cards/DM11/RoyalDurian.cs
@@ -0,0 +1,41 @@
+﻿using Engine;
+using Engine.Abilities;
+using System.Linq;
+
+namespace Cards.Cards.DM11
+{
+    class RoyalDurian : Creature
+    {
+        public RoyalDurian() : base("Royal Durian", 5, 1000, Race.WildVeggies, Civilization.Nature)
+        {
+            AddShieldTrigger();
+            AddWhenYouPutThisCreatureIntoTheBattleZoneAbility(new RoyalDurianEffect());
+        }
+    }
+
+    class RoyalDurianEffect : OneShotEffect
+    {
+        public RoyalDurianEffect()
+        {
+        }
+
+        public RoyalDurianEffect(IOneShotEffect effect) : base(effect)
+        {
+        }
+
+        public override void Apply(IGame game)
+        {
+            game.BattleZone.EvolutionCreatures.ToList().ForEach(x => game.MoveTopCard(x, ZoneType.ManaZone, Ability));
+        }
+
+        public override IOneShotEffect Copy()
+        {
+            return new RoyalDurianEffect(this);
+        }
+
+        public override string ToString()
+        {
+            return "Put the top card of each evolution creature from the battle zone into its owner's mana zone.";
+        }
+    }
+}
