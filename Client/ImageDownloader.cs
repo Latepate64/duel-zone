cd85ceb9f9927b3a39ad6d6f1380433f74cfba98
Lauri Koriseva <lauri.koriseva@atostek.com>
2023-03-24 11:53:07 +0200

    image-downloader: Add ImageDownloader and JsonUtility

---
+++ Client/ImageDownloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TimerConsole
+{
+    internal static class HttpClientUtility
+    {
+        internal static async Task DownloadAsync(string uriString, string fileName)
+        {
+            using HttpClient client = new();
+            using Stream s = await client.GetStreamAsync(new Uri(uriString));
+            using FileStream fs = new(fileName, FileMode.Create);
+            await s.CopyToAsync(fs);
+        }
+
+        internal static async Task<string> GetJson(string url)
+        {
+            using HttpClient httpClient = new();
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}
+
+//// See https://aka.ms/new-console-template for more information
+//using TimerConsole;
+
+//foreach (Card card in JsonUtility.Deserialize<Rootobject>(await HttpClientUtility.GetJson("https://raw.githubusercontent.com/Latepate64/duel-masters-json/master/DuelMastersCards.json")).cards)
+//{
+//    foreach (Printing printing in card.printings.Where(p => !string.IsNullOrEmpty(p.image)))
+//    {
+//        await HttpClientUtility.DownloadAsync(printing.image, $"{card.name}.jpg");
+//    }
+//}
---
+++ Client/JsonUtility.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace TimerConsole
+{
+    internal class JsonUtility
+    {
+        internal static T Deserialize<T>(string jsonString)
+        {
+            return JsonSerializer.Deserialize<T>(jsonString);
+        }
+    }
+}
+public class Rootobject
+{
+    public string schema { get; set; }
+    public Card[] cards { get; set; }
+}
+
+public class Card
+{
+    public string[] civilizations { get; set; }
+    public int cost { get; set; }
+    public string name { get; set; }
+    public Printing[] printings { get; set; }
+    public string text { get; set; }
+    public string type { get; set; }
+    public string power { get; set; }
+    public string[] subtypes { get; set; }
+    public string[] supertypes { get; set; }
+}
+
+public class Printing
+{
+    public string set { get; set; }
+    public string id { get; set; }
+    public string rarity { get; set; }
+    public string illustrator { get; set; }
+    public string flavor { get; set; }
+    public string image { get; set; }
+}
+
