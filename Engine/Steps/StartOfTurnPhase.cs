cbb9c2bdc9b8b7839a64668be8c0b6acb580f7bb
Lauri Koriseva <lauri.koriseva@atostek.com>
2023-03-27 14:36:07 +0300

    IGame.PlayerCannotUntapTheCardsInTheirManaZoneAtTheStartOfEachOfTheirTurns

--- Engine/Steps/StartOfTurnPhase.cs
+++ Engine/Steps/StartOfTurnPhase.cs
@@ -33,20 +33,14 @@ namespace Engine.Steps
         /// <returns></returns>
         public void PerformTurnBasedAction(IGame game)
         {
-            var player = game.CurrentTurn.ActivePlayer;
-            var ownCreaturesWithSummoningSickness = game.BattleZone.GetCreatures(game.CurrentTurn.ActivePlayer.Id).Where(x => x.SummoningSickness).ToList();
-            if (ownCreaturesWithSummoningSickness.Any())
-            {
-                foreach (var creature in ownCreaturesWithSummoningSickness)
-                {
-                    creature.SummoningSickness = false;
-                }
-                //TODO: event
-                //game.Process(new SummoningSicknessEvent { Cards = ownCreaturesWithSummoningSickness.Select(x => x.Convert()).ToList() });
-            }
+            IPlayer player = game.CurrentTurn.ActivePlayer;
             var battleZoneCreatures = game.BattleZone.GetCreatures(game.CurrentTurn.ActivePlayer.Id);
+            foreach (ICard creature in battleZoneCreatures.Where(x => x.SummoningSickness).ToList())
+            {
+                creature.SummoningSickness = false;
+            }
 
-            if (!game.GetContinuousEffects<IPlayerCannotUntapCardsInManaZoneAtStartOfTurn>().Any(x => x.PlayerCannotUntapCardsInManaZoneAtStartOfTurn(player)))
+            if (!game.PlayerCannotUntapTheCardsInTheirManaZoneAtTheStartOfEachOfTheirTurns(player))
             {
                 player.Untap(game, player.ManaZone.Cards.ToArray());
             }
