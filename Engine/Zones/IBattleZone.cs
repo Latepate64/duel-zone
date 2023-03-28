ef7222a977db2e29a2d1fb6ddeb1a6a08779a299
Lauri Koriseva <lauri.koriseva@atostek.com>
2023-03-28 11:17:44 +0300

    IBattleZone.GetCreaturesWithSilentSkill

--- Engine/Zones/IBattleZone.cs
+++ Engine/Zones/IBattleZone.cs
@@ -16,6 +16,7 @@ namespace Engine.Zones
         IEnumerable<ICard> GetCreatures(Guid controller, Race race1, Race race2);
         IEnumerable<ICard> GetCreatures(Guid controller, Civilization civilization);
         IEnumerable<ICard> GetCreatures(Guid controller, Civilization civilization1, Civilization civilization2);
+        IEnumerable<ICard> GetCreaturesWithSilentSkill(IPlayer player);
         IEnumerable<ICard> GetOtherCreatures(Guid controller, Guid creature);
         IEnumerable<ICard> GetOtherCreatures(Guid controller, Guid creature, Civilization civilization);
         IEnumerable<ICard> GetOtherCreatures(Guid creature, Civilization civilization);
@@ -23,6 +24,6 @@ namespace Engine.Zones
         IEnumerable<ICard> GetOtherTappedCreatures(Guid controller, Guid creature);
         IEnumerable<ICard> GetOtherUntappedCreatures(Guid controller, Guid creature);
         IEnumerable<ICard> GetTappedCreatures(Guid controller);
-        void RemoveSummoningSicknesses(IPlayer activePlayer);
+        void RemoveSummoningSicknesses(IPlayer player);
     }
 }
