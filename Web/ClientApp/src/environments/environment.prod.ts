9dce6762a8814bdbaf4c50c4636427bc1d13eb3c
Latepate64 <lauri.koriseva@gmail.com>
2023-07-24 13:09:01 +0300

    Added compose.yaml

--- Web/ClientApp/src/environments/environment.prod.ts
+++ Web/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    domain: 'dev-otd2luzvi36lw2nh.us.auth0.com',
+    clientId: 'vtjasg67th7LJwC6mPcV0BuyLORvbLEP',
+    authorizationParams: {
+      redirect_uri: 'https://localhost:44476',
+    },
+  },
+  api: {
+    serverUrl: 'http://localhost:6060',
+  },
 };
