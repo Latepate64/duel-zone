45921a03968c1c04b9ecc584ae0871adbf2b419a
Latepate64 <lauri.koriseva@gmail.com>
2023-07-24 14:11:47 +0300

    Update auth0.authorizationParams.redirect_uri

--- Web/ClientApp/src/environments/environment.ts
+++ Web/ClientApp/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     domain: 'dev-otd2luzvi36lw2nh.us.auth0.com',
     clientId: 'vtjasg67th7LJwC6mPcV0BuyLORvbLEP',
     authorizationParams: {
-      redirect_uri: 'https://localhost:44476',
+      redirect_uri: 'http://localhost:4200/',
     },
   },
   api: {
